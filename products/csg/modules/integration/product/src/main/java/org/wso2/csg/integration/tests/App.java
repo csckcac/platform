f3256899abf06d511bd1d76a05d2fbaa2085cdc7
rajika <rajika@a5903396-d722-0410-b921-86c7d4935375>
2012-04-17 10:22:14 +0000

    Added new folder structure for intergration tests.
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@125083 a5903396-d722-0410-b921-86c7d4935375

---
+++ products/csg/modules/integration/product/src/main/java/org/wso2/csg/integration/tests/App.java
@@ -0,0 +1,13 @@
+package org.wso2.csg.integration.tests;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

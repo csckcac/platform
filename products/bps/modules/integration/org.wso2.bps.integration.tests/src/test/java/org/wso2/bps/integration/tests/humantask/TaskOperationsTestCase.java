acef3a1a3ee56fccbbdbe0367d41f5ed3218d00f
sanjayav <sanjayav@a5903396-d722-0410-b921-86c7d4935375>
2012-04-18 12:51:26 +0000

    Added more integration test cases to task operations test.
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@125211 a5903396-d722-0410-b921-86c7d4935375

--- products/bps/modules/integration/org.wso2.bps.integration.tests/src/test/java/org/wso2/bps/integration/tests/humantask/TaskOperationsTestCase.java
+++ products/bps/modules/integration/org.wso2.bps.integration.tests/src/test/java/org/wso2/bps/integration/tests/humantask/TaskOperationsTestCase.java
@@ -19,14 +19,13 @@ package org.wso2.bps.integration.tests.humantask;
 import org.apache.axis2.client.Options;
 import org.apache.axis2.client.ServiceClient;
 import org.apache.axis2.databinding.types.URI;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.testng.Assert;
 import org.testng.annotations.BeforeGroups;
 import org.testng.annotations.Test;
 import org.wso2.bps.integration.tests.util.FrameworkSettings;
 import org.wso2.bps.integration.tests.util.HumanTaskTestConstants;
 import org.wso2.carbon.humantask.stub.ui.task.client.api.TaskOperationsStub;
+import org.wso2.carbon.humantask.stub.ui.task.client.api.types.TComment;
 import org.wso2.carbon.humantask.stub.ui.task.client.api.types.TTaskAbstract;
 import org.wso2.carbon.utils.CarbonUtils;
 
@@ -34,8 +33,6 @@ import org.wso2.carbon.utils.CarbonUtils;
  * Test class to check the task operations functionality.
  */
 public class TaskOperationsTestCase {
-
-    private static final Log log = LogFactory.getLog(TaskOperationsTestCase.class);
 
     private TaskOperationsStub taskOperationsStub = null;
 
@@ -97,6 +94,22 @@ public class TaskOperationsTestCase {
     }
 
     @Test(groups = {"wso2.bps"}, description = "Claims approval test case")
+    public void testStopTask()
+            throws Exception {
+
+        taskOperationsStub.stop(taskId);
+        TTaskAbstract loadedTask = taskOperationsStub.loadTask(taskId);
+        Assert.assertEquals(loadedTask.getStatus().toString(), "RESERVED",
+                            "The task status should be RESERVED after stopping the task!");
+
+        // Now start the task again
+        taskOperationsStub.start(taskId);
+        TTaskAbstract loadedTask2 = taskOperationsStub.loadTask(taskId);
+        Assert.assertEquals(loadedTask2.getStatus().toString(), "IN_PROGRESS",
+                            "The task status should be IN_PROGRESS after re-starting the task!");
+    }
+
+    @Test(groups = {"wso2.bps"}, description = "Claims approval test case")
     public void testSuspendAndResume()
             throws Exception {
 
@@ -111,6 +124,38 @@ public class TaskOperationsTestCase {
         TTaskAbstract loadedTaskAfterResume = taskOperationsStub.loadTask(taskId);
         Assert.assertEquals(loadedTaskAfterResume.getStatus().toString(), "IN_PROGRESS",
                             "The task status should be IN_PROGRESS after resuming the suspended task!");
+    }
+
+    @Test(groups = {"wso2.bps"}, description = "Claims approval test case")
+    public void testTaskCommentOperations() throws Exception {
+        String commentText1 = "This is a test comment";
+        URI taskCommentId = taskOperationsStub.addComment(taskId, commentText1);
+
+        Assert.assertNotNull(taskCommentId, "The comment id cannot be null");
+
+        TComment[] taskComments = taskOperationsStub.getComments(taskId);
+        Assert.assertEquals(taskComments.length, 1, "The task comments size should be 1 after adding only 1 comment");
+        Assert.assertEquals(taskComments[0].getId(), taskCommentId, "The task comment id returned should be equal");
+
+        String commentText2 = "This is a test comment 2";
+        URI taskCommentId2 = taskOperationsStub.addComment(taskId, commentText2);
+        Assert.assertNotNull(taskCommentId2, "The comment id cannot be null");
+
+        TComment[] taskComments2 = taskOperationsStub.getComments(taskId);
+        Assert.assertEquals(taskComments2.length, 2, "The task comments size should be 2 after adding 2 comments");
+        Assert.assertEquals(taskComments2[1].getId(), taskCommentId2, "The task comment id returned should be equal");
+
+        //delete the comments
+        taskOperationsStub.deleteComment(taskId, taskCommentId);
+        TComment[] commentsAfterDeletion = taskOperationsStub.getComments(taskId);
+        Assert.assertEquals(commentsAfterDeletion.length, 1, "Only 1 comment should be left");
+        Assert.assertEquals(commentsAfterDeletion[0].getId(), taskCommentId2, "Only comment 2 should be left after deleting comment 1");
+
+        //delete the left over comment as well
+        taskOperationsStub.deleteComment(taskId, taskCommentId2);
+        TComment[] commentsAfterAllDeletions = taskOperationsStub.getComments(taskId);
+        Assert.assertNull(commentsAfterAllDeletions, "There should not be any comments left!");
+
     }
 
 }

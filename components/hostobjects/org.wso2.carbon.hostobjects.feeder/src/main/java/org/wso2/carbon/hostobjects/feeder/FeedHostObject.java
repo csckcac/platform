72684f0ab17f2af6c432b90e970820839c1cd7a8
madhuka <madhuka@a5903396-d722-0410-b921-86c7d4935375>
2012-04-19 12:45:29 +0000

    Added FeedHostObject and EntryHostObject for jaggery
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@125346 a5903396-d722-0410-b921-86c7d4935375

---
+++ components/hostobjects/org.wso2.carbon.hostobjects.feeder/src/main/java/org/wso2/carbon/hostobjects/feeder/FeedHostObject.java
@@ -0,0 +1,391 @@
+package org.wso2.carbon.hostobjects.feeder;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.apache.abdera.Abdera;
+import org.apache.abdera.factory.Factory;
+import org.apache.abdera.i18n.iri.IRISyntaxException;
+import org.apache.abdera.model.Document;
+import org.apache.abdera.model.Entry;
+import org.apache.abdera.model.Feed;
+import org.apache.abdera.parser.ParseException;
+import org.apache.abdera.parser.Parser;
+import org.apache.abdera.parser.stax.util.FOMHelper;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Function;
+import org.mozilla.javascript.NativeObject;
+import org.mozilla.javascript.Scriptable;
+import org.mozilla.javascript.ScriptableObject;
+import org.mozilla.javascript.NativeArray;
+import org.wso2.carbon.scriptengine.exceptions.ScriptException;
+import org.wso2.javascript.xmlimpl.XML;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+
+//import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
+
+public class FeedHostObject extends ScriptableObject {
+
+	private static Feed feed;
+	private static Abdera abdera;
+	private static Entry entry;
+
+	private static Log log = LogFactory.getLog(FeedHostObject.class);
+
+	@Override
+	public String getClassName() {
+		return "Feeder";
+	}
+
+	/**
+	 * Constructor the user will be using inside javaScript
+	 */
+	public Scriptable jsConstructor() {
+		Abdera abdera = new Abdera();
+		Factory factory = abdera.getFactory();
+		feed = factory.newFeed();
+		return this;
+	}
+
+	synchronized public static void jsFunction_setFeed(Context cx,
+			Scriptable thisObj, Object[] arguments, Function funObj)
+			throws ScriptException {
+		if (arguments.length != 1) {
+			throw new ScriptException("Invalid parameter");
+		}
+
+		if (arguments[0] instanceof String) {
+
+			feed = null;
+			try {
+
+				URL url = new URL((String) arguments[0]);
+				feed = (Feed) Abdera.getNewParser().parse(url.openStream())
+						.getRoot();
+
+			} catch (IRISyntaxException e) {
+				throw new ScriptException(e);
+			} catch (MalformedURLException e) {
+				throw new ScriptException(e);
+			} catch (IOException e) {
+				throw new ScriptException(e);
+			}
+
+		}
+	}
+
+	public String jsGet_author() {
+		String author = feed.getAuthor().toString();
+		return author;
+	}
+
+	public String jsGet_title() {
+		String title = feed.getTitle();
+		return title;
+	}
+
+	public String jsGet_language() {
+		String language = feed.getLanguage();
+		return language;
+	}
+
+	public String jsGet_alternateLink() {
+		String alternateLink = feed.getAlternateLink().toString();
+		return alternateLink;
+	}
+
+	public String jsGet_entriesAsString() {
+		String entries = feed.getEntries().toString();
+		return entries;
+	}
+
+	public NativeArray jsGet_entries() {
+		List<Entry> entries = feed.getEntries();
+		EntryHostObject[] retEntries = null;
+
+		// Retrieving the entries from our feed
+		// List tempEntries = feed.getEntries();
+		Iterator tempEntryIterator = entries.iterator();
+
+		// Creating a list to store converted Entries
+		ArrayList<ScriptableObject> convertedEntries = new ArrayList();
+
+		Entry currentEntry;
+		NativeArray nativeArray = new NativeArray(0);
+
+		// Converting the list of Abdera Entries to AtomEntryHostObjects
+		while (tempEntryIterator.hasNext()) {
+			currentEntry = (Entry) tempEntryIterator.next();
+			EntryHostObject newAtomEntry = new EntryHostObject();
+			newAtomEntry.setEntry(currentEntry);
+			convertedEntries.add(newAtomEntry);
+			nativeArray.put(0, nativeArray, newAtomEntry);
+		}
+
+		retEntries = new EntryHostObject[convertedEntries.size()];
+		convertedEntries.toArray(retEntries);
+		return nativeArray;
+
+	}
+
+	public static Entry jsFunction_getEntry(Context cx, Scriptable thisObj,
+			Object[] arguments, Function funObj) throws ScriptException {
+		Entry entryOut = null;
+		if (arguments[0] instanceof String) {
+
+			try {
+								
+				List<Entry> entry = feed.getEntries();
+				Object[] entryArray = entry.toArray();
+				int x = Integer.parseInt((String) arguments[0]);
+				entryOut = (Entry) entryArray[x];
+				
+			
+			} catch (IRISyntaxException e) {
+				throw new ScriptException(e);
+			}
+		} else {
+			throw new ScriptException("Invalid parameter");
+		}
+
+		return entryOut;
+	}
+
+	public static Entry[] jsFunction_getAllEntry(Context cx,
+			Scriptable thisObj, Object[] arguments, Function funObj)
+			throws ScriptException {
+		Entry[] entryOut = null;
+
+		try {
+			
+		
+			List<Entry> entry = feed.getEntries();
+			Object[] entryArray = entry.toArray();
+			entryOut = new Entry[entry.size()];
+			for (int i = 0; i < entry.size(); i++) {
+				entryOut[i] = (Entry) entryArray[i];
+			}
+		} catch (IRISyntaxException e) {
+			throw new ScriptException(e);
+		}
+
+		return entryOut;
+	}
+
+	public static Entry jsFunction_addEntries(Context cx, Scriptable thisObj,
+			Object[] arguments, Function funObj) throws ScriptException {
+
+		NativeArray fields = (NativeArray) arguments[0];
+
+		if (arguments[0] instanceof NativeArray ) {
+			 for (Object o : fields.getIds()) {
+
+                 int index = (Integer) o;                    
+                 Object nativeObject = fields.get(index, null);
+                 Object[] argumentsPass = new Object[1];
+                 argumentsPass[0]=nativeObject;
+                 jsFunction_addEntry(cx, thisObj,argumentsPass, funObj);
+             }
+
+	
+		}else {
+		}
+			throw new ScriptException("Invalid parameter");
+		}
+
+	public static Entry jsFunction_addEntry(Context cx, Scriptable thisObj,
+			Object[] arguments, Function funObj) throws ScriptException {
+
+		abdera = new Abdera();
+		Factory factory = abdera.getFactory();
+		entry = factory.newEntry();
+
+		if (arguments[0] instanceof NativeObject ) {
+
+			try {
+				NativeObject nativeObject = (NativeObject) arguments[0];
+			
+				ScriptableObject so = (ScriptableObject) nativeObject;
+
+				// author and authors processing
+				Object authorProperty = ScriptableObject.getProperty(
+						nativeObject, "author");
+				if (authorProperty instanceof String) {
+				
+					entry.addAuthor((String) (authorProperty));
+				}
+				Object authorsProperty = ScriptableObject.getProperty(
+						nativeObject, "authors");
+				if (authorsProperty instanceof NativeArray) {
+					NativeArray authorsPropertyArray = (NativeArray) authorsProperty;
+					for (Object o1 : authorsPropertyArray.getIds()) {
+
+						int indexx = (Integer) o1;
+						String name = authorsPropertyArray.get(indexx, null)
+								.toString();
+						
+						entry.addAuthor(name);
+					}
+
+				}
+
+				// processing category
+				Object categoryProperty = ScriptableObject.getProperty(
+						nativeObject, "category");
+				if (categoryProperty instanceof String) {
+					
+					entry.addCategory((String) (categoryProperty));
+				}
+				Object categoriesProperty = ScriptableObject.getProperty(
+						nativeObject, "categories");
+				if (categoriesProperty instanceof NativeArray) {
+					NativeArray categoriesPropertyArray = (NativeArray) categoriesProperty;
+					for (Object o1 : categoriesPropertyArray.getIds()) {
+
+						int indexC = (Integer) o1;
+						String name = categoriesPropertyArray.get(indexC, null)
+								.toString();
+						
+						entry.addCategory(name);
+					}
+
+				}
+
+				// process content
+				Object content = ScriptableObject.getProperty(nativeObject,
+						"content");
+				if (content instanceof XML) {
+					entry.setContentAsXhtml(content.toString());
+				} else if (content instanceof String) {
+					entry.setContent(content.toString());
+				} else {
+					throw new ScriptException("Unsupported Content");
+				}
+
+				// process contributor
+				Object contributorProperty = ScriptableObject.getProperty(
+						nativeObject, "contributor");
+				if (contributorProperty instanceof String) {
+					entry.addContributor(contributorProperty.toString());
+				}
+				Object contributorsProperty = ScriptableObject.getProperty(
+						nativeObject, "contributors");
+				if (contributorsProperty instanceof NativeArray) {
+					NativeArray contributorsPropertyArray = (NativeArray) contributorsProperty;
+					for (Object o1 : contributorsPropertyArray.getIds()) {
+
+						int index = (Integer) o1;
+						String name = contributorsPropertyArray
+								.get(index, null).toString();
+						
+						entry.addContributor(name);
+					}
+				}
+
+				// process id
+				Object idProperty = ScriptableObject.getProperty(nativeObject,
+						"id");
+				if (idProperty instanceof String) {
+					entry.setId((String) (idProperty));
+				} else {
+					entry.setId(FOMHelper.generateUuid());
+				}
+
+				// process link
+				// TODO link object
+				Object linkProperty = ScriptableObject.getProperty(
+						nativeObject, "link");
+				if (linkProperty instanceof String) {
+					entry.addLink((String) (linkProperty));
+				}
+				Object linksProperty = ScriptableObject.getProperty(
+						nativeObject, "links");
+				if (linksProperty instanceof NativeArray) {
+					NativeArray linksPropertyArray = (NativeArray) contributorsProperty;
+					for (Object o1 : linksPropertyArray.getIds()) {
+
+						int index = (Integer) o1;
+						String name = linksPropertyArray.get(index, null)
+								.toString();
+						
+						entry.addLink(name);
+					}
+				}
+
+				// process published
+				// TODO handle javascript date
+				Object publishedProperty = ScriptableObject.getProperty(
+						nativeObject, "published");
+				if (publishedProperty instanceof String) {
+					if (publishedProperty.equals("now")) {
+						entry.setPublished(new Date(System.currentTimeMillis()));
+					} else {
+						entry.setPublished(publishedProperty.toString());
+					}
+				}
+
+				// process rights
+				Object rights = ScriptableObject.getProperty(nativeObject,
+						"rights");
+				if (rights instanceof XML) {
+					entry.setRightsAsXhtml(rights.toString());
+				} else if (rights instanceof String) {
+					entry.setRights(rights.toString());
+				}
+
+				// process summary
+				Object summary = ScriptableObject.getProperty(nativeObject,
+						"summary");
+				if (summary instanceof XML) {
+					entry.setSummaryAsXhtml(summary.toString());
+				} else if (summary instanceof String) {
+					entry.setSummary(summary.toString());
+				}
+
+				// process title
+				Object title = ScriptableObject.getProperty(nativeObject,
+						"title");
+				if (title instanceof XML) {
+					entry.setTitleAsXhtml(title.toString());
+				} else if (title instanceof String) {
+					entry.setTitle(title.toString());
+				} else {
+					throw new ScriptException("An Entry MUST have a title.");
+				}
+
+				// process updated
+				Object updated = ScriptableObject.getProperty(nativeObject,
+						"updated");
+				if (updated instanceof String) {
+					if (updated.equals("now")) {
+						entry.setUpdated(new Date(System.currentTimeMillis()));
+					} else {
+						entry.setUpdated((String) updated);
+					}
+				}
+
+			} catch (IRISyntaxException e) {
+				throw new ScriptException(e);
+			}
+		} else {
+			throw new ScriptException("Invalid parameter");
+		}
+		//for testing will be removed
+		log.info("New Added Entry"+entry);
+		return entry;
+	}
+
+
+
+}

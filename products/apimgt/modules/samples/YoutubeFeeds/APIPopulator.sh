156937e438e812dfe988a1000841a803ca3ddc26
hiranya <hiranya@a5903396-d722-0410-b921-86c7d4935375>
2012-04-25 14:05:35 +0000

    Adding new Youtube sample
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@125980 a5903396-d722-0410-b921-86c7d4935375

---
+++ products/apimgt/modules/samples/YoutubeFeeds/APIPopulator.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+START=$1
+COUNT=$2
+SERVER=http://localhost:9763
+curl -X POST -c cookies $SERVER/apiprovider/services/provider.jag -d 'action=login&username=provider1&password=provider1';
+curl -X POST -b cookies $SERVER/apiprovider/services/provider.jag -d 'action=getUser';
+
+curl -X POST -b cookies $SERVER/apiprovider/services/provider.jag -d "action=saveAPI&apiName=YoutubeFeeds&version=1.0.0&description=Youtube Live Feeds&endpoint=http://gdata.youtube.com/feeds/api/standardfeeds&wsdl=&tags=youtube,gdata,multimedia&tier=Silver&thumbUrl=http://www.10bigideas.com.au/www/573/files/pf-thumbnail-youtube_logo.jpg&context=/youtube&resourceCount=0&resourceMethod-0=GET&uriTemplate-0=/*";
+
+curl -X POST -b cookies $SERVER/apiprovider/services/provider.jag -d "action=updateAPI&apiName=YoutubeFeeds&version=1.0.0&description=Youtube Live Feeds&endpoint=http://gdata.youtube.com/feeds/api/standardfeeds&wsdl=&tags=youtube,gdata,multimedia&tier=Silver&thumbUrl=http://www.10bigideas.com.au/www/573/files/pf-thumbnail-youtube_logo.jpg&context=/youtube&status=PUBLISHED&resourceCount=0&resourceMethod-0=GET&uriTemplate-0=/*";
+
+
+

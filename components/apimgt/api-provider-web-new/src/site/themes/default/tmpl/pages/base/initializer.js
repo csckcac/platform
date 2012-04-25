2bfe04c9d3cdc2ce136f025327ac9dc0767ea01b
lalaji <lalaji@a5903396-d722-0410-b921-86c7d4935375>
2012-04-25 12:37:24 +0000

    Updated the templates
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@125965 a5903396-d722-0410-b921-86c7d4935375

---
+++ components/apimgt/api-provider-web-new/src/site/themes/default/tmpl/pages/base/initializer.js
@@ -0,0 +1,30 @@
+var getTemplateFile = function () {
+    return "tmpl/pages/base/template.jag";
+};
+
+var initialize = function (jagg) {
+    //addHeaderCSS(global, "layouts/layout0", "layout0", "tmpl/layouts/layout0/css/layout0.css");
+};
+
+var getData = function (params) {
+    return {
+        title:params.title,
+        body:params.body
+    };
+};
+
+var getParams = function () {
+    //return { top:null, left:null, middle:null, right:null };
+    return {
+        title:null,
+        body:null
+    };
+};
+
+var getTemplates = function () {
+
+};
+
+var getTemplateParams = function () {
+    return ["body"];
+};

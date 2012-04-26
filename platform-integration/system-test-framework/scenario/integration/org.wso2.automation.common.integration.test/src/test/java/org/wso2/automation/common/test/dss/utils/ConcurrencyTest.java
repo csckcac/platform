af7debe694d9c3a8d87ff36bed626611b914f71f
nuwanw <nuwanw@a5903396-d722-0410-b921-86c7d4935375>
2012-04-26 13:40:37 +0000

    added concurrency test for load testing
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@126067 a5903396-d722-0410-b921-86c7d4935375

---
+++ platform-integration/system-test-framework/scenario/integration/org.wso2.automation.common.integration.test/src/test/java/org/wso2/automation/common/test/dss/utils/ConcurrencyTest.java
@@ -0,0 +1,68 @@
+/*
+*Copyright (c) 2005-2010, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+*WSO2 Inc. licenses this file to you under the Apache License,
+*Version 2.0 (the "License"); you may not use this file except
+*in compliance with the License.
+*You may obtain a copy of the License at
+*
+*http://www.apache.org/licenses/LICENSE-2.0
+*
+*Unless required by applicable law or agreed to in writing,
+*software distributed under the License is distributed on an
+*"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+*KIND, either express or implied.  See the License for the
+*specific language governing permissions and limitations
+*under the License.
+*/
+package org.wso2.automation.common.test.dss.utils;
+
+import org.apache.axiom.om.OMElement;
+import org.apache.axis2.AxisFault;
+import org.testng.Assert;
+import org.wso2.automation.common.test.dss.utils.exception.ExceptionHandler;
+import org.wso2.platform.test.core.utils.axis2client.AxisServiceClient;
+
+public class ConcurrencyTest {
+    private int concurrencyNumber;
+    private int numberOfIterations;
+
+    public ConcurrencyTest(int threadGroup, int loopCount) {
+        concurrencyNumber = threadGroup;
+        numberOfIterations = loopCount;
+    }
+
+    public boolean run(final String serviceEndPoint, final OMElement payload, final String operation)
+            throws Exception {
+
+        ExceptionHandler handler = new ExceptionHandler();
+        Thread[] clientThread = new Thread[concurrencyNumber];
+        final AxisServiceClient serviceClient = new AxisServiceClient();
+        for (int i = 0; i < concurrencyNumber; i++) {
+            clientThread[i] = new Thread(new Runnable() {
+                public void run() {
+                    for (int j = 0; j < numberOfIterations; j++) {
+                        try {
+                            serviceClient.sendReceive(payload, serviceEndPoint,operation);
+                        } catch (AxisFault axisFault) {
+                            Assert.fail("AxisFault " + axisFault.getMessage());
+                        }
+                    }
+                }
+            });
+            clientThread[i].setUncaughtExceptionHandler(handler);
+
+        }
+
+        for (int i = 0; i < concurrencyNumber; i++) {
+            clientThread[i].start();
+        }
+
+        for (int i = 0; i < concurrencyNumber; i++) {
+            clientThread[i].join();
+        }
+
+        return handler.isTestPass();
+
+    }
+}

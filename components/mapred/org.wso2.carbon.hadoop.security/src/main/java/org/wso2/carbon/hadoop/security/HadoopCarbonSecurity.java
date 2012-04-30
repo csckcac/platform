8a8434e06cfc2466a32b8da07d5af8789465cd91
wathsala <wathsala@a5903396-d722-0410-b921-86c7d4935375>
2012-04-30 05:05:23 +0000

    Security related improvements to Hadoop MapRed subsystem
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@126163 a5903396-d722-0410-b921-86c7d4935375

---
+++ components/mapred/org.wso2.carbon.hadoop.security/src/main/java/org/wso2/carbon/hadoop/security/HadoopCarbonSecurity.java
@@ -0,0 +1,9 @@
+package org.wso2.carbon.hadoop.security;
+
+import org.apache.hadoop.security.UserGroupInformationThreadLocal;
+
+public class HadoopCarbonSecurity {
+	public static void clean() {
+		UserGroupInformationThreadLocal.remove();
+	}
+}
---
+++ components/mapred/org.wso2.carbon.mapred.mgt/src/main/java/org/wso2/carbon/mapred/mgt/HadoopJobRunnerActivator.java
@@ -0,0 +1,28 @@
+package org.wso2.carbon.mapred.mgt;
+
+import java.util.Hashtable;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceFactory;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class HadoopJobRunnerActivator implements BundleActivator {
+
+	private Log log = LogFactory.getLog(HadoopJobRunnerActivator.class);
+	@Override
+	public void start(BundleContext bc) throws Exception {
+		log.info("Starting HadoopJobRunner bundle.");
+		ServiceFactory serviceFactory = new HadoopJobRunnerFactory();
+		bc.registerService(HadoopJobRunnerFactory.class.getName(), serviceFactory, new Hashtable());
+		log.info("Registered HadoopJobRunner service.");
+	}
+
+	@Override
+	public void stop(BundleContext bc) throws Exception {
+		log.info("Stopping HadoopJobRunner bundle");
+	}
+
+}

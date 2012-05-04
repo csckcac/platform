c5d0758d3452e1323ac38d993466e62f1f0aa707
chanaka <chanaka@a5903396-d722-0410-b921-86c7d4935375>
2012-05-04 12:33:34 +0000

    Fixing a issue in tabs
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@126512 a5903396-d722-0410-b921-86c7d4935375

--- components/apimgt/api-store-new/src/site/themes/fancy/templates/page/base/js/base.js
+++ components/apimgt/api-store-new/src/site/themes/fancy/templates/page/base/js/base.js
@@ -4,4 +4,8 @@ $(function() {
         $('.clouds ul.tabs li a').corner("bevel  tl");
         $('#left div.bevel-from-left').corner("bevel  bl");
         $('.bevel-from-right').corner("bevel  br");
+		
+		//Hiding the tabs to fix a bug in kikstart
+		$('.tab-data div.tab-content').hide();
+        $('.tab-data div.tab-content:first-child').show();
 });

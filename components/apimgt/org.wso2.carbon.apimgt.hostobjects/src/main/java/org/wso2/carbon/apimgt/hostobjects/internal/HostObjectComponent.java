756a9a21a064bd91abef8763224c18b1a267be65
hiranya <hiranya@a5903396-d722-0410-b921-86c7d4935375>
2012-05-08 11:30:07 +0000

    Removing obsolete class
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@126773 a5903396-d722-0410-b921-86c7d4935375

--- components/apimgt/org.wso2.carbon.apimgt.hostobjects/src/main/java/org/wso2/carbon/apimgt/hostobjects/internal/HostObjectComponent.java
+++ components/apimgt/org.wso2.carbon.apimgt.hostobjects/src/main/java/org/wso2/carbon/apimgt/hostobjects/internal/HostObjectComponent.java
@@ -18,27 +18,23 @@ package org.wso2.carbon.apimgt.hostobjects.internal;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.osgi.service.component.ComponentContext;
-import org.wso2.carbon.apimgt.hostobjects.utils.APIHostObjectUtil;
 
 /**
  * @scr.component name="org.wso2.apimgt.hostobjects" immediate="true"
  */
 public class HostObjectComponent {
 
-    static Log log = LogFactory.getLog(HostObjectComponent.class);
-    private APIHostObjectUtil hostObjectUtil;
+    private static final Log log = LogFactory.getLog(HostObjectComponent.class);
 
     protected void activate(ComponentContext componentContext) {
-       if(log.isDebugEnabled()){
+       if (log.isDebugEnabled()){
            log.debug("HostObjectComponent activated");
        }
-        hostObjectUtil = APIHostObjectUtil.getApiHostObjectUtils();
     }
 
-    protected void dedactivate(ComponentContext componentContext) {
-       if(log.isDebugEnabled()){
+    protected void deactivate(ComponentContext componentContext) {
+       if (log.isDebugEnabled()){
            log.debug("HostObjectComponent deactivated");
        }
-       hostObjectUtil.cleanup();
     }
 }

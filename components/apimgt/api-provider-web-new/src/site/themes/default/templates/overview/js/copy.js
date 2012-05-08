361e612870d03f56761e8df3f074f41bc57f63ff
lalaji <lalaji@a5903396-d722-0410-b921-86c7d4935375>
2012-05-08 08:17:11 +0000

    Updated overview page
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@126747 a5903396-d722-0410-b921-86c7d4935375

--- components/apimgt/api-provider-web-new/src/site/themes/default/templates/overview/js/copy.js
+++ components/apimgt/api-provider-web-new/src/site/themes/default/templates/overview/js/copy.js
@@ -1,6 +1,6 @@
 var copyAPIToNewVersion = function () {
-    var apiName=$("td#api-name").text();
-    var version=$("td#api-version").text() ;
+    var apiName=$("#item-info h2")[0].innerHTML.split("-v")[0];
+    var version=$("#item-info h2")[0].innerHTML.split("-v")[1];
     var newVersion = $("#copy-api #new-version").val();
     jagg.post("/site/blocks/overview/ajax/overview.jag", { action:"createNewAPI", apiName:apiName, version:version, newVersion:newVersion },
               function (result) {

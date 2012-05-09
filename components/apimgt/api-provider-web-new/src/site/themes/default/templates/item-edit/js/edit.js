7a732b88dd5ecec67376e0a2a6baea7460a7a97d
lalaji <lalaji@a5903396-d722-0410-b921-86c7d4935375>
2012-05-09 09:27:59 +0000

    Updated add-api,edit-api functionalities and did some corrections
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@126887 a5903396-d722-0410-b921-86c7d4935375

--- components/apimgt/api-provider-web-new/src/site/themes/default/templates/item-edit/js/edit.js
+++ components/apimgt/api-provider-web-new/src/site/themes/default/templates/item-edit/js/edit.js
@@ -1,109 +1,3 @@
-$(document).ready(function () {
-    var options = {
-        success:function () {
-            location.href = "index.jag?place=api-details&name=" + apiProviderApp.currentAPIName + "&version=" + apiProviderApp.currentVersion;
-        }
-    };
-    $('#editAPIForm').submit(function () {
-        $(this).ajaxSubmit(options);
-        return false;
-    });
-
-    $('#editAPIForm #apiName').val(apiProviderApp.currentAPIName);
-    $('#editAPIForm #version').val(apiProviderApp.currentVersion);
-
-
-});
-
-var getAPI=function(api){
-    if (typeof api.name != "string") {
-           location.href = "login.jag";
-       }
-       $("#editDescription").text(api.description);
-
-       $("#context").attr("value", api.context);
-       $("#editEndpoint").attr("value", api.endpoint);
-       $("#editWsdl").attr("value", api.wsdl);
-       $("#editTags").attr("value", api.tags);
-       if (typeof api.thumb) {
-           $("img#apiEditThumb").attr("src", "images/api-default.png");
-
-       } else {
-           $("img#apiEditThumb").attr("src", api.thumb);
-
-       }
-       var availTier = api.availableTiers;
-       if (availTier == 'Gold') {
-           document.getElementById('editTier').selectedIndex = 0;
-       }
-       else {
-           document.getElementById('editTier').selectedIndex = 1;
-       }
-       var status = api.status;
-       if (status == 'CREATED') {
-           $("#editStatus option[value='CREATED']").attr('selected', 'selected');
-       }
-       else if (status == 'PUBLISHED') {
-           $("#editStatus option[value='CREATED']").remove();
-           $("#editStatus option[value='PUBLISHED']").attr('selected', 'selected');
-
-       } else if (status == 'DEPRECATED') {
-           $("#editStatus option[value='CREATED']").remove();
-           $("#editStatus option[value='PUBLISHED']").remove();
-           $("#editStatus option[value='DEPRECATED']").attr('selected', 'selected')
-       }
-       else if (status == 'RETIRED') {
-           $("#editStatus option[value='CREATED']").remove();
-           $("#editStatus option[value='PUBLISHED']").remove();
-           $("#editStatus option[value='RETIRED']").attr('selected', 'selected')
-       }
-       else if (status == 'BLOCKED') {
-           $("#editStatus option[value='CREATED']").remove();
-           $("#editStatus option[value='PUBLISHED']").remove();
-           $("#editStatus option[value='BLOCKED']").attr('selected', 'selected')
-       }
-       createResourceRows(api);
-
-};
-
-
-var createResourceRows = function (api) {
-    if (api.templates.length > 0) {
-        for (var i = 0; i < api.templates.length; i++) {
-            $('#resourceRow').clone(true).attr('id', 'item-' + resourcesCount).insertAfter($('#resourceRow'));
-            var thisItem = '#item-' + resourcesCount;
-            $('#item-' + resourcesCount + ' #resourceMethod').val(api.templates[i][1]);
-            $('#item-' + resourcesCount + ' #uriTemplate').val(api.templates[i][0]);
-            $('#item-' + resourcesCount + ' #uriTemplate').attr('disabled', 'disabled');
-            $('#item-' + resourcesCount + ' #resourceMethod').attr('disabled', 'disabled');
-            $('#item-' + resourcesCount + ' td#buttons a#resourceAddBtn').remove();
-            $('#item-' + resourcesCount + ' td#buttons').append("<a class=\"btn btn-danger\" onclick=\"deleteResource(" + resourcesCount + ")\"><i class=\"icon-trash icon-white\"></i> Delete</a>");
-
-
-            $('<input>').attr('type', 'hidden')
-                .attr('name', 'resourceMethod-' + resourcesCount).attr('value', api.templates[i][1])
-                .appendTo('#editAPIForm');
-
-            $('<input>').attr('type', 'hidden')
-                .attr('name', 'uriTemplate-' + resourcesCount).attr('value', api.templates[i][0])
-                .appendTo('#editAPIForm');
-
-            rowNums.push(resourcesCount);
-            resourcesCount++;
-
-            if ($('#resourceCount').length == 0) {
-                $('<input>').attr('type', 'hidden')
-                    .attr('name', 'resourceCount')
-                    .attr('id', 'resourceCount')
-                    .attr('value', rowNums)
-                    .appendTo('#editAPIForm');
-            } else {
-                $('#resourceCount').attr('value', rowNums);
-            }
-        }
-    }
-};
-
 var resourcesCount = 1;
 var rowNums = new Array();
 
@@ -140,6 +34,22 @@ var updateResourcesToApi = function () {
 
     $('#uriTemplate').val('');
     $('#resourceMethod').val('GET');
+
+};
+
+var deleteResource = function (id) {
+    var count=$('#resource-table tr').length;
+    //Check whether only one defined resource remains before delete operation
+    if(count==3){
+        $('#resourceTableError').show('fast');
+        return;
+    }
+    $('#resourceTableError').hide('fast');
+    $('#item-' + id).remove();
+    $('[name=resourceMethod-' + id + ']').remove();
+    $('[name=uriTemplate-' + id + ']').remove();
+    rowNums.splice(rowNums.indexOf(id),1);
+    $('#resourceCount').val(rowNums);
 
 };
 

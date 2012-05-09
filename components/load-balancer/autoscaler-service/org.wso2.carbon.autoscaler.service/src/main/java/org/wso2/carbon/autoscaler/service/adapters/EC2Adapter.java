5c5ad1f593b1e70c3b19dbabe2526186a0a06785
ranga <ranga@a5903396-d722-0410-b921-86c7d4935375>
2012-05-09 06:27:56 +0000

    Improving EC2 Adapter to work with pending instance count
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@126850 a5903396-d722-0410-b921-86c7d4935375

--- components/load-balancer/autoscaler-service/org.wso2.carbon.autoscaler.service/src/main/java/org/wso2/carbon/autoscaler/service/adapters/EC2Adapter.java
+++ components/load-balancer/autoscaler-service/org.wso2.carbon.autoscaler.service/src/main/java/org/wso2/carbon/autoscaler/service/adapters/EC2Adapter.java
@@ -3,6 +3,8 @@ package org.wso2.carbon.autoscaler.service.adapters;
 import com.amazonaws.services.ec2.model.*;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.wso2.carbon.autoscaler.service.IAutoscalerService;
+import org.wso2.carbon.autoscaler.service.internal.AutoscalerServiceDSHolder;
 import org.wso2.carbon.lb.common.ec2.EC2InstanceManager;
 import org.wso2.carbon.lb.common.ec2.EC2Util;
 import org.wso2.carbon.lb.common.conf.EC2Configuration;
@@ -12,35 +14,44 @@ import java.io.File;
 import java.sql.SQLException;
 import java.util.Arrays;
 
+/**
+ * EC2 specific Adapter, handles spawning / terminating instances in EC2
+ *
+ */
 public class EC2Adapter extends Adapter {
 
     private static final Log log = LogFactory.getLog(EC2Adapter.class);
 
+    // Name of the Adapter
     private static final String ADAPTER_NAME = "ec2";
 
     // EC2Instance Manager will handle all the EC2 API level invocations
     private EC2InstanceManager ec2InstanceManager;
 
-    //private EC2Config;
+    // EC2Configuration which holds ec2.conf configuration;
     private EC2Configuration ec2Configuration = new EC2Configuration();
 
+    private IAutoscalerService autoscalerService = AutoscalerServiceDSHolder.getInstance().getAutoscalerService();
 
+    /**
+     * Default constructor which creates EC2 Configuration and EC2 Instance Manager
+     */
     public EC2Adapter(){
         log.info("Constructing EC2 Adapter...");
 
-        //Need to create ec2Configuration here
+        // Create ec2Configuration from given file
         String ec2ConfigFile =
                 CarbonUtils.getCarbonConfigDirPath() + File.separator + "ec2.conf";
         ec2Configuration.init(ec2ConfigFile);
 
-        ec2InstanceManager = EC2Util.createEC2InstanceManager(ec2Configuration.getEc2AccessKey(),
-                ec2Configuration.getEc2PrivateKey(),
-                ec2Configuration.getInstanceMgtEPR());
+        // Create EC2 Instance Manager
+        ec2InstanceManager = EC2Util.createEC2InstanceManager(ec2Configuration.getEc2_access_key(),
+                ec2Configuration.getEc2_private_key(),
+                ec2Configuration.getInstance_mgt_epr());
 
-        log.info("Initialized LoadBalancerConfiguration for EC2 configuration...");
-
-
-        //  ec2InstanceManager =
+        if (log.isDebugEnabled()) {
+            log.debug("EC2 Adaptor constructed with EC2 configuration");
+        }
     }
 
     @Override
@@ -50,15 +61,26 @@ public class EC2Adapter extends Adapter {
 
     @Override
     public boolean spawnInstance(String domainName, String instanceId) throws ClassNotFoundException, SQLException {
-        log.info("Spawning EC2 Instance...");
+
+        log.info("Trying to spawn instance in EC2 Adapter");
 
         EC2Configuration.ServiceConfiguration serviceConfig = ec2Configuration.getServiceConfig(domainName);
-       // int instancesPerScaleUp = serviceConfig.getInstancesPerScaleUp();
-        log.info("Domain: " + domainName + " Going to start instance " + 1 +
-                ". Running instances:" + "runningInstances ??? ");
+
         runInstances(serviceConfig, 1);
-        log.info("Started " + 1 + " new app instances in domain " +
+
+        log.info("Started " + 1 + " new instances in domain " +
                 domainName);
+
+        // At this point the the server will starts after some time (Sleep for 2 minutes)
+        try {
+            Thread.sleep(120000);
+        } catch (InterruptedException e) {
+            log.error("InterruptedException while waiting for new instance startup" , e);
+        }
+
+        // Reducing the pending instance count by one in AutoscalerService
+        // Here we assumes that the server instance is successfully started
+        autoscalerService.addPendingInstanceCount(domainName, -1);
 
         return true;
     }
@@ -66,69 +88,57 @@ public class EC2Adapter extends Adapter {
     @Override
     public boolean terminateInstance(String instanceId) {
 
+        log.info("Trying to terminate EC2 instance " + instanceId + ".");
+
         // Here we have decided to terminate the given instance
         // This decision should make in a intelligent way for better use of ec2 resources.
-            if (ec2Configuration.getDisableApiTermination()) {
-                ec2InstanceManager.enableApiTermination(instanceId);
-            }
-            log.info("Going to terminate instance " + instanceId + ".");
-            ec2InstanceManager.terminateInstances(Arrays.asList(instanceId));
-            log.info("Terminated instance: " + instanceId + ".");
+        if (ec2Configuration.getDisable_api_termination()) {
+            ec2InstanceManager.enableApiTermination(instanceId);
+        }
+
+        ec2InstanceManager.terminateInstances(Arrays.asList(instanceId));
+
         return true;
     }
 
     @Override
     public int getRunningInstanceCount() {
-        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+        return 0;
     }
 
     @Override
     public int getRunningInstanceCount(String domainName) {
-        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+        return 0;
     }
 
     @Override
     public int getPendingInstanceCount(String domainName) {
-        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+        return 0;
     }
 
     @Override
     public boolean sanityCheck() {
-        return false;  //To change body of implemented methods use File | Settings | File Templates.
+        return false;
     }
 
-
+    /**
+     * method which calls EC2 Instance Manager when starting new EC2 Instances
+     * @param configuration : EC2 Configuration
+     * @param diff : Minimum and maximum number of instances to spawn
+     */
     private void runInstances(EC2Configuration.Configuration configuration, int diff) {
         RunInstancesRequest request = new RunInstancesRequest(configuration.getImageId(),
                                                               diff, diff);
         request.setInstanceType(configuration.getInstanceType());
-        request.setKeyName(ec2Configuration.getSshKey());
+        request.setKeyName(ec2Configuration.getEc2_ssh_key());
         request.setSecurityGroups(Arrays.asList(configuration.getSecurityGroups()));
         request.setAdditionalInfo(configuration.getAdditionalInfo());
         request.setUserData(configuration.getUserData());
         request.setPlacement(new Placement(configuration.getAvailability_zone()));
-        request.setDisableApiTermination(ec2Configuration.getDisableApiTermination());
+        request.setDisableApiTermination(ec2Configuration.getDisable_api_termination());
 
+        log.info("Calling EC2InstanceManager to spawn new instance");
         ec2InstanceManager.runInstances(request);
     }
-
-//    public static void main(String args[]) {
-//        System.out.println("Inside EC2 Adaptor");
-//
-//        EC2Adapter ec2Adapter = new EC2Adapter();
-//        String domainName = "wso2.as.domain";
-//        String instanceId = "1";
-//
-//        // Need to read the policy  file
-//
-//        try {
-//            ec2Adapter.spawnInstance(domainName, instanceId);
-//        } catch (ClassNotFoundException e) {
-//            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-//        } catch (SQLException e) {
-//            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-//        }
-//
-//    }
 
 }

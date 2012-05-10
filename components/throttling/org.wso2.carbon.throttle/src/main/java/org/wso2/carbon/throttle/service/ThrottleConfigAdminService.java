bcbb53bfdd9fecbdf135b0bfca8327541f6e9ff0
supunm <supunm@a5903396-d722-0410-b921-86c7d4935375>
2012-05-10 09:20:26 +0000

    changes related to WSAS-838
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@127012 a5903396-d722-0410-b921-86c7d4935375

--- components/throttling/org.wso2.carbon.throttle/src/main/java/org/wso2/carbon/throttle/service/ThrottleConfigAdminService.java
+++ components/throttling/org.wso2.carbon.throttle/src/main/java/org/wso2/carbon/throttle/service/ThrottleConfigAdminService.java
@@ -292,6 +292,7 @@ public class ThrottleConfigAdminService extends AbstractAdmin {
                     if (!Boolean.parseBoolean(element
                             .getAttributeValue(new QName(GLOBALLY_ENGAGED_CUSTOM)))) {
                         element.addAttribute(GLOBALLY_ENGAGED_CUSTOM, Boolean.TRUE.toString(), null);
+                        mfpm.setMetaFileModification(module.getName());
                     }
                 } else {
                     OMFactory omFactory = OMAbstractFactory.getOMFactory();
@@ -714,6 +715,7 @@ public class ThrottleConfigAdminService extends AbstractAdmin {
                 if (!Boolean.parseBoolean(element
                         .getAttributeValue(new QName(GLOBALLY_ENGAGED_CUSTOM)))) {
                     element.addAttribute(GLOBALLY_ENGAGED_CUSTOM, Boolean.FALSE.toString(), null);
+                    mfpm.setMetaFileModification(module.getName());
                 }
             }
 

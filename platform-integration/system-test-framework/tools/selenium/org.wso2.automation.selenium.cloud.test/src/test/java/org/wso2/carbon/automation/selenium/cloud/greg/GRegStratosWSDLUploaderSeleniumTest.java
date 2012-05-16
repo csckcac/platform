59a1f29f150aec939a7455a394d55984e94101a9
krishantha <krishantha@a5903396-d722-0410-b921-86c7d4935375>
2012-05-16 05:20:36 +0000

    Fixing G-Reg UI tests
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@127369 a5903396-d722-0410-b921-86c7d4935375

--- platform-integration/system-test-framework/tools/selenium/org.wso2.automation.selenium.cloud.test/src/test/java/org/wso2/carbon/automation/selenium/cloud/greg/GRegStratosWSDLUploaderSeleniumTest.java
+++ platform-integration/system-test-framework/tools/selenium/org.wso2.automation.selenium.cloud.test/src/test/java/org/wso2/carbon/automation/selenium/cloud/greg/GRegStratosWSDLUploaderSeleniumTest.java
@@ -36,7 +36,6 @@ import org.wso2.platform.test.core.utils.seleniumutils.StratosUserLogin;
 
 import java.io.File;
 import java.net.MalformedURLException;
-import java.util.Calendar;
 
 import static org.testng.Assert.assertTrue;
 
@@ -48,11 +47,11 @@ public class GRegStratosWSDLUploaderSeleniumTest {
     String productName = "greg";
     String userName;
     String password;
-    long sleeptime = 3000;
+    long sleepTime = 3000;
 
     @BeforeClass(alwaysRun = true)
     public void init() throws MalformedURLException, InterruptedException {
-        UserInfo userDetails = UserListCsvReader.getUserInfo(3);
+        UserInfo userDetails = UserListCsvReader.getUserInfo(6);
         userName = userDetails.getUserName();
         password = userDetails.getPassword();
         String baseUrl = new ProductUrlGeneratorUtil().getServiceHomeURL(ProductConstant.
@@ -64,13 +63,13 @@ public class GRegStratosWSDLUploaderSeleniumTest {
     }
 
     @Test(groups = {"wso2.stratos.greg"}, description = "add wsdl from url", priority = 1)
-    public void testaddWSDLfromURL() throws Exception {
+    public void testAddWSDLFromURL() throws Exception {
         String wsdl_url = "http://people.wso2.com/~evanthika/wsdls/echo.wsdl";
 
         try {
-            new StratosUserLogin().userLogin(driver, selenium, userName, password, productName);
+            StratosUserLogin.userLogin(driver, selenium, userName, password, productName);
             gotoAddWSDLPage();
-            addWsdlfromURL(wsdl_url);
+            addWsdlFromURL(wsdl_url);
             deleteWSDL();
             deleteService();
             userLogout();
@@ -92,16 +91,16 @@ public class GRegStratosWSDLUploaderSeleniumTest {
 
 
     @Test(groups = {"wso2.stratos.greg"}, description = "add wsdl from file", priority = 2)
-    public void testaddWSDLfromFile() throws Exception {
+    public void testAddWSDLFromFile() throws Exception {
         String resourcePath = ProductConstant.SYSTEM_TEST_RESOURCE_LOCATION;
         String file_path = resourcePath + File.separator + "artifacts" + File.separator +
-                           "Selenium" + File.separator + "GREG" + File.separator + "wsdl" +
+                           File.separator + "GREG" + File.separator + "wsdl" +
                            File.separator + "echo.wsdl";
 
         try {
-            new StratosUserLogin().userLogin(driver, selenium, userName, password, productName);
+            StratosUserLogin.userLogin(driver, selenium, userName, password, productName);
             gotoAddWSDLPage();
-            addWSDLfromFile(file_path);
+            addWSDLFromFile(file_path);
             deleteWSDL();
             deleteService();
             userLogout();
@@ -127,36 +126,38 @@ public class GRegStratosWSDLUploaderSeleniumTest {
         driver.quit();
     }
 
-    private void addWSDLfromFile(String file_path) throws InterruptedException {
+    private void addWSDLFromFile(String file_path) throws InterruptedException {
         Select select = new Select(driver.findElement(By.id("addMethodSelector")));
         select.selectByVisibleText("Upload WSDL from a file");
-        waitTimeforElement("//p/input");
         selenium.focus("id=uResourceFile");
-        Thread.sleep(sleeptime);
+        Thread.sleep(sleepTime);
         driver.findElement(By.id("uResourceFile")).sendKeys(file_path);
-        Thread.sleep(sleeptime);
+        Thread.sleep(sleepTime);
         driver.findElement(By.xpath("//div/table/tbody/tr[2]/td/input")).click();
-        waitTimeforElement("//form/table/tbody/tr/td/a");
-        assertTrue(driver.getPageSource().contains("echo.wsdl"), "Failed to add echo.wsdl :");
-        assertTrue(driver.getPageSource().contains("http://echo.services.core.carbon.wso2.org"),
-                   "Failed to display tagnameSpace :");
+        Thread.sleep(40000);
+        assertTrue(driver.findElement(By.xpath("//tr[2]/td/div/div/form/table/tbody/tr/td/a"))
+                           .getText().contains("echo.wsdl"), "echo wsdl not found in the list");
+        assertTrue(driver.findElement(By.xpath("//td/div/div/form/table/tbody/tr/td[2]"))
+                           .getText().contains("http://echo.services.core.carbon.wso2.org"),
+                   "List doesn't contain target namespace");
     }
 
 
     private void userLogout() throws InterruptedException {
         driver.findElement(By.linkText("Sign-out")).click();
-        waitTimeforElement("//a[2]/img");
     }
 
 
-    private void addWsdlfromURL(String wsdl_url) throws InterruptedException {
+    private void addWsdlFromURL(String wsdl_url) throws InterruptedException {
         driver.findElement(By.id("irFetchURL")).sendKeys(wsdl_url);
         driver.findElement(By.id("irResourceName")).click();
         driver.findElement(By.xpath("//div/table/tbody/tr[2]/td/input")).click();
-        waitTimeforElement("//form/table/tbody/tr/td/a");
-        assertTrue(driver.getPageSource().contains("echo.wsdl"), "Failed to add echo.wsdl :");
-        assertTrue(driver.getPageSource().contains("http://echo.services.core.carbon.wso2.org"),
-                   "Failed to display tagnameSpace :");
+        Thread.sleep(40000);
+        assertTrue(driver.findElement(By.xpath("//tr[2]/td/div/div/form/table/tbody/tr/td/a"))
+                .getText().contains("echo.wsdl"), "echo wsdl not found in the list");
+        assertTrue(driver.findElement(By.xpath("//td/div/div/form/table/tbody/tr/td[2]"))
+                .getText().contains("http://echo.services.core.carbon.wso2.org"),
+                   "List doesn't contain target namespace");
     }
 
 
@@ -164,57 +165,33 @@ public class GRegStratosWSDLUploaderSeleniumTest {
         driver.findElement(By.id("uLocationBar")).clear();
         driver.findElement(By.id("uLocationBar")).sendKeys("/_system/governance/trunk/services/org");
         driver.findElement(By.xpath("//input[2]")).click();
-        waitTimeforElement("//div[9]/table/tbody/tr/td/table/tbody/tr/td/a");
         driver.findElement(By.id("actionLink1")).click();
-        waitTimeforElement("//div/a[3]");
         driver.findElement(By.linkText("Delete")).click();
-        waitTimeforElement("//body/div[3]/div/div");
         assertTrue(selenium.isTextPresent("exact:Are you sure you want to delete '/_system/" +
                                           "governance/trunk/services/org/wso2' permanently?"),
                    "Failed to delete service");
         selenium.click("//button");
-        Thread.sleep(sleeptime);
+        Thread.sleep(sleepTime);
     }
 
     private void deleteWSDL() throws InterruptedException {
         driver.findElement(By.linkText("echo.wsdl")).click();
-        waitTimeforElement("//input");
         driver.findElement(By.id("uLocationBar")).clear();
         driver.findElement(By.id("uLocationBar")).sendKeys("/_system/governance/trunk/wsdls/org/");
         driver.findElement(By.xpath("//input[2]")).click();
-        waitTimeforElement("//div[9]/table/tbody/tr/td/table/tbody/tr/td/a");
         driver.findElement(By.id("actionLink1")).click();
-        waitTimeforElement("//div/a[3]");
         driver.findElement(By.linkText("Delete")).click();
-        waitTimeforElement("//body/div[3]/div/div");
         assertTrue(selenium.isTextPresent("exact:Are you sure you want to delete '/_" +
                                           "system/governance/trunk/wsdls/org/wso2' permanently?"),
                    "Failed to delete wsdl :");
         selenium.click("//button");
-        Thread.sleep(sleeptime);
+        Thread.sleep(sleepTime);
     }
 
     private void gotoAddWSDLPage() throws InterruptedException {
         driver.findElement(By.linkText("WSDL")).click();
-        waitTimeforElement("//td[2]/input");
         assertTrue(driver.getPageSource().contains("Add WSDL"), "Failed to display Add WSDL Page :");
-        Thread.sleep(sleeptime);
-    }
-
-    private void waitTimeforElement(String elementName) throws InterruptedException {
-        Calendar startTime = Calendar.getInstance();
-        long time;
-        boolean element = false;
-        while ((time = (Calendar.getInstance().getTimeInMillis() - startTime.getTimeInMillis()))
-               < 120 * 1000) {
-            if (selenium.isElementPresent(elementName)) {
-                element = true;
-                break;
-            }
-            Thread.sleep(1000);
-            log.info("waiting for element :" + elementName);
-        }
-        assertTrue(element, "Element Not Found within 2 minutes :");
+        Thread.sleep(sleepTime);
     }
 
 

25598de507dd25daae267f92a3de1cc500d932c2
lalaji <lalaji@a5903396-d722-0410-b921-86c7d4935375>
2012-05-16 14:38:19 +0000

    Updated the script
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@127521 a5903396-d722-0410-b921-86c7d4935375

--- products/apimgt/modules/samples/YahooPlaceFinder/APIPopulator.sh
+++ products/apimgt/modules/samples/YahooPlaceFinder/APIPopulator.sh
@@ -2,12 +2,11 @@
 START=$1
 COUNT=$2
 SERVER=http://localhost:9763
-curl -X POST -c cookies $SERVER/apiprovider/services/provider.jag -d 'action=login&username=provider1&password=provider1';
-curl -X POST -b cookies $SERVER/apiprovider/services/provider.jag -d 'action=getUser';
+curl -X POST -c cookies $SERVER/apiprovider/site/blocks/user/login/ajax/login.jag -d 'action=login&username=provider1&password=provider1';
 
-curl -X POST -b cookies $SERVER/apiprovider/services/provider.jag -d "action=saveAPI&apiName=PlaceFinder&version=1.0.0&description=Place Finder&endpoint=http://where.yahooapis.com/geocode&wsdl=&tags=finder,open,social&tier=Silver&thumbUrl=http://images.ientrymail.com/webpronews/article_pics/yahoo-placefinder.jpg&context=/placeFinder&resourceCount=0&resourceMethod-0=POST&uriTemplate-0=/*";
+curl -X POST -b cookies $SERVER/apiprovider/site/blocks/item-add/ajax/add.jag -d "action=addAPI&name=PlaceFinder&version=1.0.0&description=Place Finder&endpoint=http://where.yahooapis.com/geocode&wsdl=&tags=finder,open,social&tier=Silver&thumbUrl=http://images.ientrymail.com/webpronews/article_pics/yahoo-placefinder.jpg&context=/placeFinder&resourceCount=0&resourceMethod-0=POST&uriTemplate-0=/*";
 
-curl -X POST -b cookies $SERVER/apiprovider/services/provider.jag -d "action=updateAPI&apiName=PlaceFinder&version=1.0.0&description=Place Finder&endpoint=http://where.yahooapis.com/geocode&wsdl=&tags=finder,open,social&tier=Silver&thumbUrl=http://images.ientrymail.com/webpronews/article_pics/yahoo-placefinder.jpg&context=/placeFinder&status=PUBLISHED&resourceCount=0&resourceMethod-0=GET&uriTemplate-0=/*";
+curl -X POST -b cookies $SERVER/apiprovider/site/blocks/item-add/ajax/add.jag -d "action=updateAPI&name=PlaceFinder&version=1.0.0&description=Place Finder&endpoint=http://where.yahooapis.com/geocode&wsdl=&tags=finder,open,social&tier=Silver&thumbUrl=http://images.ientrymail.com/webpronews/article_pics/yahoo-placefinder.jpg&context=/placeFinder&status=PUBLISHED&resourceCount=0&resourceMethod-0=GET&uriTemplate-0=/*";
 
 
 

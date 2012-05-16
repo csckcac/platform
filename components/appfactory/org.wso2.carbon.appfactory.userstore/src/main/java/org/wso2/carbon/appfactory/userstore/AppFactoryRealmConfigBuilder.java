c0ad621face0606abdfe503462e9698e92237c6a
dimuthul <dimuthul@a5903396-d722-0410-b921-86c7d4935375>
2012-05-16 03:53:50 +0000

    Adding the appfactory user store.
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@127353 a5903396-d722-0410-b921-86c7d4935375

---
+++ components/appfactory/org.wso2.carbon.appfactory.userstore/src/main/java/org/wso2/carbon/appfactory/userstore/AppFactoryRealmConfigBuilder.java
@@ -0,0 +1,95 @@
+package org.wso2.carbon.appfactory.userstore;
+
+import java.util.Map;
+
+import org.apache.axiom.util.UIDGenerator;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.wso2.carbon.CarbonConstants;
+import org.wso2.carbon.user.api.RealmConfiguration;
+import org.wso2.carbon.user.api.TenantMgtConfiguration;
+import org.wso2.carbon.user.core.UserCoreConstants;
+import org.wso2.carbon.user.core.UserStoreException;
+import org.wso2.carbon.user.core.config.multitenancy.MultiTenantRealmConfigBuilder;
+import org.wso2.carbon.user.core.ldap.LDAPConstants;
+import org.wso2.carbon.user.core.tenant.Tenant;
+
+public class AppFactoryRealmConfigBuilder implements MultiTenantRealmConfigBuilder  {
+
+    private static Log logger = LogFactory.getLog(AppFactoryRealmConfigBuilder.class);
+    
+    @Override
+    public RealmConfiguration getRealmConfigForTenantToCreateRealm(RealmConfiguration bootStrapConfig, 
+                                                                   RealmConfiguration persistedConfig, 
+                                                                   int tenantId) throws UserStoreException {
+        return persistedConfig;
+    }
+
+    @Override
+    public RealmConfiguration getRealmConfigForTenantToPersist(RealmConfiguration bootStrapConfig,
+                                                               TenantMgtConfiguration tenantMgtConfig, Tenant tenantInfo,
+                                                               int tenantId) throws UserStoreException {
+
+        try {
+            RealmConfiguration ldapRealmConfig = bootStrapConfig
+                    .cloneRealmConfiguration();
+            ldapRealmConfig.setAdminPassword(UIDGenerator.generateUID());
+            ldapRealmConfig.setAdminUserName(tenantInfo.getAdminName());
+            ldapRealmConfig.setTenantId(tenantId);
+
+            Map<String, String> authz = ldapRealmConfig.getAuthzProperties();
+            authz.put(
+                    UserCoreConstants.RealmConfig.PROPERTY_ADMINROLE_AUTHORIZATION,
+                    CarbonConstants.UI_ADMIN_PERMISSION_COLLECTION);
+
+            Map<String, String> userStoreProperties = ldapRealmConfig
+                    .getUserStoreProperties();
+
+            String partitionDN = tenantMgtConfig
+                    .getTenantStoreProperties()
+                    .get(UserCoreConstants.TenantMgtConfig.PROPERTY_ROOT_PARTITION);
+            String organizationName = tenantInfo.getDomain();
+            // eg: o=cse.rog
+            String organizationRDN = tenantMgtConfig
+                    .getTenantStoreProperties()
+                    .get(UserCoreConstants.TenantMgtConfig.PROPERTY_ORGANIZATIONAL_ATTRIBUTE)
+                    + "=" + organizationName;
+            // eg: ou=users
+            String orgSubContextAttribute = tenantMgtConfig
+                    .getTenantStoreProperties()
+                    .get(UserCoreConstants.TenantMgtConfig.PROPERTY_ORG_SUB_CONTEXT_ATTRIBUTE);
+            
+            // if read ldap group is enabled, set the tenant specific group
+            // search base
+            if (("true").equals(bootStrapConfig
+                    .getUserStoreProperty(LDAPConstants.READ_EXTERNAL_ROLES))) {
+                // eg: ou=groups
+                String groupContextRDN = orgSubContextAttribute + "="
+                        + LDAPConstants.GROUP_CONTEXT_NAME;
+                // eg: ou=users,o=cse.org, dc=cloud, dc=com
+                String groupSearchBase = groupContextRDN + ","
+                        + organizationRDN + "," + partitionDN;
+
+                userStoreProperties.put(LDAPConstants.GROUP_SEARCH_BASE,
+                        groupSearchBase);
+            }
+
+            return ldapRealmConfig;
+
+        } catch (Exception e) {
+            String errorMessage = "Error while building tenant specific Realm Configuration.";
+            logger.error(errorMessage, e);
+            throw new UserStoreException(errorMessage, e);
+        }
+    }
+
+    @Override
+    public RealmConfiguration getRealmConfigForTenantToCreateRealmOnTenantCreation(
+                              RealmConfiguration bootStrapConfig, 
+                              RealmConfiguration persistedConfig, int tenantId) throws UserStoreException {
+        return persistedConfig;
+    }
+    
+    
+
+}
---
+++ components/appfactory/org.wso2.carbon.appfactory.userstore/src/main/java/org/wso2/carbon/appfactory/userstore/AppFactoryUserStore.java
@@ -0,0 +1,53 @@
+package org.wso2.carbon.appfactory.userstore;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import org.wso2.carbon.user.api.RealmConfiguration;
+import org.wso2.carbon.user.core.UserRealm;
+import org.wso2.carbon.user.core.UserStoreException;
+import org.wso2.carbon.user.core.claim.ClaimManager;
+import org.wso2.carbon.user.core.ldap.ApacheDSUserStoreManager;
+import org.wso2.carbon.user.core.profile.ProfileConfigurationManager;
+
+public class AppFactoryUserStore extends ApacheDSUserStoreManager {
+
+    public AppFactoryUserStore(RealmConfiguration realmConfig,
+                               Map<String, Object> properties, ClaimManager claimManager,
+                               ProfileConfigurationManager profileManager, UserRealm realm,
+                               Integer tenantId) throws UserStoreException {
+        super(realmConfig, properties, claimManager, profileManager, realm,
+                tenantId);
+    }
+
+    public AppFactoryUserStore(RealmConfiguration realmConfig,
+                               ClaimManager claimManager,
+                               ProfileConfigurationManager profileManager) throws UserStoreException {
+        super(realmConfig, claimManager, profileManager);
+    }
+
+    public boolean authenticate(String username, Object credential) throws UserStoreException {
+        boolean isAuthenticated = false;
+        isAuthenticated = super.authenticate(username, credential);
+        if (isAuthenticated) {
+            String[] roles = getRoleListOfUser(username);
+            if (roles.length > 0) {
+                return true;
+            }
+        }
+        return isAuthenticated;
+    }
+
+    public String[] listUsers(String arg0, int maxItemLimit) throws UserStoreException {
+        String[] roles = getRoleNames();
+        List<String> users = new ArrayList<String>();
+        for (String role : roles) {
+            String[] usersInRole = getUserListOfRole(role);
+            users.addAll(Arrays.asList(usersInRole));
+        }
+        return users.toArray(new String[users.size()]);
+    }
+
+}

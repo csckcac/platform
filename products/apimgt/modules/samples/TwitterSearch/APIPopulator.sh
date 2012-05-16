fc8e7bab93f312f5191a4a0926d1306520e25cc0
lalaji <lalaji@a5903396-d722-0410-b921-86c7d4935375>
2012-05-16 14:57:16 +0000

    Corrected script
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@127523 a5903396-d722-0410-b921-86c7d4935375

--- products/apimgt/modules/samples/TwitterSearch/APIPopulator.sh
+++ products/apimgt/modules/samples/TwitterSearch/APIPopulator.sh
@@ -4,9 +4,9 @@ COUNT=$2
 SERVER=http://localhost:9763
 curl -X POST -c cookies $SERVER/apiprovider/site/blocks/user/login/ajax/login.jag -d 'action=login&username=admin&password=admin';
 
-curl -X POST -b cookies $SERVER/apiprovider/site/blocks/item-add/ajax/add.jag -d "action=addAPI&apiName=TwitterSearch&version=1.0.0&description=Twitter Search&endpoint=http://search.twitter.com&wsdl=&tags=twitter,open,social&tier=Silver&thumbUrl=https://lh6.ggpht.com/RNc8dD2hXG_rWGzlj09ZwAe1sXVvLWkeYT3ePx7zePCy4ZVV2XMGIxAzup4cKM85NFtL=w124&context=/twitter&resourceCount=0&resourceMethod-0=POST&uriTemplate-0=/*";
+curl -X POST -b cookies $SERVER/apiprovider/site/blocks/item-add/ajax/add.jag -d "action=addAPI&name=TwitterSearch&version=1.0.0&description=Twitter Search&endpoint=http://search.twitter.com&wsdl=&tags=twitter,open,social&tier=Silver&thumbUrl=https://lh6.ggpht.com/RNc8dD2hXG_rWGzlj09ZwAe1sXVvLWkeYT3ePx7zePCy4ZVV2XMGIxAzup4cKM85NFtL=w124&context=/twitter&resourceCount=0&resourceMethod-0=POST&uriTemplate-0=/*";
 
-curl -X POST -b cookies $SERVER/apiprovider/site/blocks/item-add/ajax/add.jag -d "action=updateAPI&apiName=TwitterSearch&version=1.0.0&description=Twitter Search&endpoint=http://search.twitter.com&wsdl=&tags=twitter,open,social&tier=Silver&thumbUrl=https://lh6.ggpht.com/RNc8dD2hXG_rWGzlj09ZwAe1sXVvLWkeYT3ePx7zePCy4ZVV2XMGIxAzup4cKM85NFtL=w124&context=/twitter&status=PUBLISHED&resourceCount=0&resourceMethod-0=POST&uriTemplate-0=/*";
+curl -X POST -b cookies $SERVER/apiprovider/site/blocks/item-add/ajax/add.jag -d "action=updateAPI&name=TwitterSearch&version=1.0.0&description=Twitter Search&endpoint=http://search.twitter.com&wsdl=&tags=twitter,open,social&tier=Silver&thumbUrl=https://lh6.ggpht.com/RNc8dD2hXG_rWGzlj09ZwAe1sXVvLWkeYT3ePx7zePCy4ZVV2XMGIxAzup4cKM85NFtL=w124&context=/twitter&status=PUBLISHED&resourceCount=0&resourceMethod-0=POST&uriTemplate-0=/*";
 
 
 

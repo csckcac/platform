a43b38c8691a329f039e058ab87aee0e4a3cb782
dimuthul <dimuthul@a5903396-d722-0410-b921-86c7d4935375>
2012-05-16 04:11:33 +0000

    Inheriting from common ldap thing
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@127358 a5903396-d722-0410-b921-86c7d4935375

--- components/appfactory/org.wso2.carbon.appfactory.userstore/src/main/java/org/wso2/carbon/appfactory/userstore/AppFactoryTenantManager.java
+++ components/appfactory/org.wso2.carbon.appfactory.userstore/src/main/java/org/wso2/carbon/appfactory/userstore/AppFactoryTenantManager.java
@@ -17,32 +17,29 @@
 */
 package org.wso2.carbon.appfactory.userstore;
 
+import java.util.Map;
+
+import javax.naming.NamingException;
+import javax.naming.directory.DirContext;
+import javax.sql.DataSource;
+
 import org.apache.axiom.om.OMElement;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.wso2.carbon.user.api.RealmConfiguration;
 import org.wso2.carbon.user.api.Tenant;
 import org.wso2.carbon.user.api.TenantMgtConfiguration;
 import org.wso2.carbon.user.core.UserCoreConstants;
 import org.wso2.carbon.user.core.UserStoreException;
-import org.wso2.carbon.user.api.RealmConfiguration;
 import org.wso2.carbon.user.core.ldap.LDAPConnectionContext;
 import org.wso2.carbon.user.core.ldap.LDAPConstants;
-import org.wso2.carbon.user.core.tenant.JDBCTenantManager;
-
-import javax.naming.NamingException;
-import javax.naming.directory.Attribute;
-import javax.naming.directory.Attributes;
-import javax.naming.directory.BasicAttribute;
-import javax.naming.directory.BasicAttributes;
-import javax.naming.directory.DirContext;
-import javax.sql.DataSource;
-import java.util.Map;
+import org.wso2.carbon.user.core.tenant.CommonHybridLDAPTenantManager;
 
 /**
  * This class is the tenant manager for any external LDAP and based on the "ou" partitioning
  * per tenant under one DIT.
  */
-public class AppFactoryTenantManager extends JDBCTenantManager {
+public class AppFactoryTenantManager extends CommonHybridLDAPTenantManager {
     private static Log logger = LogFactory.getLog(AppFactoryTenantManager.class);
     private LDAPConnectionContext ldapConnectionSource;
 
@@ -72,31 +69,6 @@ public class AppFactoryTenantManager extends JDBCTenantManager {
     }
 
     /**
-     * Do necessary things in LDAP when adding a tenant.
-     *
-     * @param tenant Tenant to be added.
-     * @return The tenant id.
-     * @throws UserStoreException If an error occurred while creating the tenant.
-     */
-    @Override
-    public int addTenant(org.wso2.carbon.user.api.Tenant tenant) throws UserStoreException {
-        int tenantID = super.addTenant(tenant);
-        tenant.setId(tenantID);
-
-        DirContext initialDirContext = null;
-        try {
-            initialDirContext = this.ldapConnectionSource.getContext();
-            //create per tenant context and its user store and group store with admin related entries.
-            createOrganizationalUnit(tenant.getDomain(), (Tenant) tenant, initialDirContext);
-        } finally {
-            closeContext(initialDirContext);
-        }
-
-
-        return tenantID;
-    }
-
-    /**
      * Create a space for tenant in LDAP.
      *
      * @param orgName  Organization name.
@@ -104,7 +76,7 @@ public class AppFactoryTenantManager extends JDBCTenantManager {
      * @param initialDirContext The directory connection.
      * @throws UserStoreException If an error occurred while creating.
      */
-    private void createOrganizationalUnit(String orgName, Tenant tenant, DirContext initialDirContext)
+    protected void createOrganizationalUnit(String orgName, Tenant tenant, DirContext initialDirContext)
             throws UserStoreException {
         //e.g: ou=wso2.com
         String partitionDN = tenantMgtConfig.getTenantStoreProperties().get(
@@ -140,134 +112,11 @@ public class AppFactoryTenantManager extends JDBCTenantManager {
                     LDAPConstants.GROUP_CONTEXT_NAME + "," +
                     dnOfOrganizationalContext;
             
-            System.out.println(" sssssssssss  Membership attribute is " + dnOfUserEntry);
             createAdminGroup(dnOfGroupContext, dnOfUserEntry, initialDirContext);
         }
     }
 
-    /**
-     * Create main context corresponding to tenant.
-     *
-     * @param rootDN   Root domain name.
-     * @param orgName Organization name
-     * @param initialDirContext The directory connection.
-     * @throws UserStoreException If an error occurred while creating context.
-     */
-    private void createOrganizationalContext(String rootDN, String orgName, DirContext initialDirContext)
-            throws UserStoreException {
 
-        DirContext subContext = null;
-        DirContext organizationalContext = null;
-        try {
-
-            //get the connection context for rootDN
-            subContext = (DirContext) initialDirContext.lookup(rootDN);
-
-            Attributes contextAttributes = new BasicAttributes(true);
-            //create organizational object class attribute
-            Attribute objectClass = new BasicAttribute(LDAPConstants.OBJECT_CLASS_NAME);
-            objectClass.add(
-                    tenantMgtConfig.getTenantStoreProperties().get(
-                            UserCoreConstants.TenantMgtConfig.PROPERTY_ORGANIZATIONAL_OBJECT_CLASS));
-            contextAttributes.put(objectClass);
-            //create organizational name attribute
-            String organizationalNameAttribute = tenantMgtConfig.getTenantStoreProperties().get(
-                    UserCoreConstants.TenantMgtConfig.PROPERTY_ORGANIZATIONAL_ATTRIBUTE);
-            Attribute organization =
-                    new BasicAttribute(organizationalNameAttribute);
-            organization.add(orgName);
-            contextAttributes.put(organization);
-            //construct organization rdn.
-            String rdnOfOrganizationalContext = organizationalNameAttribute + "=" + orgName;
-            if (logger.isDebugEnabled()) {
-                logger.debug("Adding sub context: " + rdnOfOrganizationalContext + " under " +
-                        rootDN + " ...");
-            }
-            //create organization sub context
-            organizationalContext = subContext.createSubcontext(rdnOfOrganizationalContext, contextAttributes);
-            if (logger.isDebugEnabled()) {
-                logger.debug("Sub context: " + rdnOfOrganizationalContext + " was added under "
-                        + rootDN + " successfully.");
-            }
-
-        } catch (NamingException e) {
-            String errorMsg = "Error occurred while adding the organizational unit " +
-                    "sub context.";
-            logger.error(errorMsg, e);
-            throw new UserStoreException(errorMsg, e);
-        } finally {
-            closeContext(organizationalContext);
-            closeContext(subContext);
-        }
-    }
-
-    private void closeContext(DirContext ldapContext) {
-        if (ldapContext != null) {
-            try {
-                ldapContext.close();
-            } catch (NamingException e) {
-                logger.error("Error closing sub context.", e);
-            }
-        }
-    }
-
-    /**
-     * Create sub contexts under the tenant's main context.
-     *
-     * @param dnOfParentContext domain name of the parent context.
-     * @param nameOfCurrentContext name of the current context.
-     * @param initialDirContext The directory connection.
-     * @throws UserStoreException if an error occurs while creating context.
-     */
-    private void createOrganizationalSubContext(String dnOfParentContext,
-                                                String nameOfCurrentContext, DirContext initialDirContext)
-            throws UserStoreException {
-
-        DirContext subContext = null;
-        DirContext organizationalContext = null;
-
-        try {
-            //get the connection for tenant's main context
-            subContext = (DirContext) initialDirContext.lookup(dnOfParentContext);
-
-            Attributes contextAttributes = new BasicAttributes(true);
-            //create sub unit object class attribute
-            Attribute objectClass = new BasicAttribute(LDAPConstants.OBJECT_CLASS_NAME);
-            objectClass.add(tenantMgtConfig.getTenantStoreProperties().get(
-                    UserCoreConstants.TenantMgtConfig.PROPERTY_ORG_SUB_CONTEXT_OBJ_CLASS));
-            contextAttributes.put(objectClass);
-
-            //create org sub unit name attribute
-            String orgSubUnitAttributeName = tenantMgtConfig.getTenantStoreProperties().get(
-                    UserCoreConstants.TenantMgtConfig.PROPERTY_ORG_SUB_CONTEXT_ATTRIBUTE);
-            Attribute organizationSubUnit = new BasicAttribute(orgSubUnitAttributeName);
-            organizationSubUnit.add(nameOfCurrentContext);
-            contextAttributes.put(organizationSubUnit);
-
-            //construct the rdn of org sub context
-            String rdnOfOrganizationalContext = orgSubUnitAttributeName + "=" +
-                    nameOfCurrentContext;
-            if (logger.isDebugEnabled()) {
-                logger.debug("Adding sub context: " + rdnOfOrganizationalContext + " under " +
-                        dnOfParentContext + " ...");
-            }
-            //create sub context
-            organizationalContext = subContext.createSubcontext(rdnOfOrganizationalContext, contextAttributes);
-            if (logger.isDebugEnabled()) {
-                logger.debug("Sub context: " + rdnOfOrganizationalContext + " was added under "
-                        + dnOfParentContext + " successfully.");
-            }
-
-        } catch (NamingException e) {
-            String errorMsg = "Error occurred while adding the organizational unit " +
-                    "sub context.";
-            logger.error(errorMsg, e);
-            throw new UserStoreException(errorMsg, e);
-        } finally {
-            closeContext(organizationalContext);
-            closeContext(subContext);
-        }
-    }
 
     private String getAdminEntryDN(String dnOfUserContext, Tenant tenant, DirContext initialDirContext)
             throws UserStoreException {
@@ -284,7 +133,6 @@ public class AppFactoryTenantManager extends JDBCTenantManager {
             String userRDN = userNameAttribute + "=" + tenant.getAdminName();
             //organizationalUsersContext.bind(userRDN, null, userAttributes);
             userDN = userRDN + "," + dnOfUserContext;
-            System.out.println("******* Setting the userDN " + userDN);
             //return (userRDN + dnOfUserContext);
         } catch (NamingException e) {
             String errorMsg = "Error occurred while creating Admin entry";
@@ -297,44 +145,6 @@ public class AppFactoryTenantManager extends JDBCTenantManager {
         return userDN;
     }
 
-    private void createAdminGroup(String dnOfGroupContext, String adminUserDN, DirContext initialDirContext)
-            throws UserStoreException {
-        //create set of attributes required to create admin group
-        Attributes adminGroupAttributes = new BasicAttributes(true);
-        //admin entry object class
-        Attribute objectClassAttribute = new BasicAttribute(LDAPConstants.OBJECT_CLASS_NAME);
-        objectClassAttribute.add(realmConfig.getUserStoreProperty(
-                LDAPConstants.GROUP_ENTRY_OBJECT_CLASS));
-        adminGroupAttributes.put(objectClassAttribute);
 
-        //group name attribute
-        String groupNameAttributeName = realmConfig.getUserStoreProperty(
-                LDAPConstants.ROLE_NAME_ATTRIBUTE_NAME);
-        Attribute groupNameAttribute = new BasicAttribute(groupNameAttributeName);
-        String adminRoleName = realmConfig.getAdminRoleName();
-        groupNameAttribute.add(adminRoleName);
-        adminGroupAttributes.put(groupNameAttribute);
-
-        //membership attribute
-        Attribute membershipAttribute = new BasicAttribute(realmConfig.getUserStoreProperty(
-                LDAPConstants.MEMBERSHIP_ATTRIBUTE));
-        membershipAttribute.add(adminUserDN);
-        System.out.println("*************   Membership attribute is " + adminUserDN);
-        adminGroupAttributes.put(membershipAttribute);
-
-        DirContext groupContext = null;
-        try {
-            groupContext = (DirContext) initialDirContext.lookup(dnOfGroupContext);
-            String rdnOfAdminGroup = groupNameAttributeName + "=" + adminRoleName;
-            groupContext.bind(rdnOfAdminGroup, null, adminGroupAttributes);
-            
-        } catch (NamingException e) {
-            String errorMessage = "Error occurred while creating the admin group.";
-            logger.error(errorMessage);
-            throw new UserStoreException(errorMessage, e);
-        } finally {
-            closeContext(groupContext);
-        }
-    }
 
 }

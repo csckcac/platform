cdeefcc0875154726a8cb1eb54dade946405f7d3
chanaka <chanaka@a5903396-d722-0410-b921-86c7d4935375>
2012-05-17 09:01:51 +0000

    Fixing image upload issue in provider from UI side and validation issue with the API add form.
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@127567 a5903396-d722-0410-b921-86c7d4935375

--- components/apimgt/api-provider-web/src/site/themes/default/templates/item-add/js/load.js
+++ components/apimgt/api-provider-web/src/site/themes/default/templates/item-add/js/load.js
@@ -1,36 +1,23 @@
-$(function() {
+ $(document).ready(function() {
+    var v = $("#addAPIForm").validate({
+			submitHandler: function(form) {
+				$(form).ajaxSubmit({
+                    success:function(responseText, statusText, xhr, $form) {
+                        $('#apiModal h3.modal-title').html('API Provider');
+                        $('#apiModal div.modal-body').html('\n\nSuccessfully added new API.\n Do you want to leave this page?');
+                        $('#apiModal a.btn-primary').html('Yes');
+                        $('#apiModal a.btn-other').html('No. Stay and reset form.');
+                        $('#apiModal div.modal-footer').append($('<a class="btn btn-other-noreset" data-dismiss="modal">No. Stay</a>'));
+                        $('#apiModal a.btn-other').click(function(){
+                            v.resetForm();
+                        });
+                        $('#apiModal a.btn-primary').click(function(){
+                            location.href = "site/pages/index.jag";
+                        });
+                        $('#apiModal').modal();
+                    }
+                });
+			}
+		});
 
-    jagg.post("/site/blocks/tag/ajax/tag.jag", {
-        action:"getAllTags"
-
-
-    }, function (result) {
-        if (!result.error) {
-            var tags = new Array();
-            var jsonTagCloud = result.tags;
-            for (var i = 0; i < jsonTagCloud.length; i++) {
-                tags.push(jsonTagCloud[i].name);
-            }
-            $('.typeahead').typeahead({
-                                          source:tags
-                                      });
-        } else {
-
-            jagg.message(result.message);
-        }
-    }, "json");
-
-
-});
-var addAPIForm = $("#addAPIForm");
-addAPIForm.validation();
-
-$('#addAPIForm').ajaxForm({
-                              complete: function() {
-                                  $("#addAPIForm")[0].reset();
-                                  location.href = "index.jag";
-                              }
-
-                          });
-
-
+ });

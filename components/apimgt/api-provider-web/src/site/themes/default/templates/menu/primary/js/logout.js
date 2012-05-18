1b314fb552399f8245e6e21617697b165f3cd032
lalaji <lalaji@a5903396-d722-0410-b921-86c7d4935375>
2012-05-18 11:07:52 +0000

    Corrected logout redirection url
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@127681 a5903396-d722-0410-b921-86c7d4935375

--- components/apimgt/api-provider-web/src/site/themes/default/templates/menu/primary/js/logout.js
+++ components/apimgt/api-provider-web/src/site/themes/default/templates/menu/primary/js/logout.js
@@ -1,7 +1,12 @@
- var logout = function () {
+var logout = function () {
     jagg.post("/site/blocks/user/login/ajax/login.jag", {action:"logout"}, function (result) {
         if (!result.error) {
-            location.href = 'login.jag';
+            var current = window.location.pathname;
+            if (current.indexOf(".jag") >= 0) {
+                location.href = "login.jag";
+            } else {
+                location.href = 'site/pages/login.jag';
+            }
         } else {
             jagg.message(result.message);
         }

9357c2d36280d1db6df3ed94ec377b2e72c7e0a7
lalaji <lalaji@a5903396-d722-0410-b921-86c7d4935375>
2012-05-18 11:09:01 +0000

    Corrected logout redirection url
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@127682 a5903396-d722-0410-b921-86c7d4935375

--- components/apimgt/api-provider-web/src/site/themes/default/templates/overview/js/copy.js
+++ components/apimgt/api-provider-web/src/site/themes/default/templates/overview/js/copy.js
@@ -1,12 +1,18 @@
 var copyAPIToNewVersion = function () {
-    var apiName=$("#item-info h2")[0].innerHTML.split("-v")[0];
-    var version=$("#item-info h2")[0].innerHTML.split("-v")[1];
+    var apiName = $("#item-info h2")[0].innerHTML.split("-v")[0];
+    var version = $("#item-info h2")[0].innerHTML.split("-v")[1];
     var newVersion = $("#copy-api #new-version").val();
     jagg.post("/site/blocks/overview/ajax/overview.jag", { action:"createNewAPI", apiName:apiName, version:version, newVersion:newVersion },
               function (result) {
                   if (!result.error) {
                       $("#copy-api #new-version").val('');
-                      location.href ='site/pages/index.jag';
+                      var current = window.location.pathname;
+                      if (current.indexOf(".jag") >= 0) {
+                          location.href = "index.jag";
+                      } else {
+                          location.href = 'site/pages/index.jag';
+                      }
+
                   } else {
                       jagg.message(result.message);
                   }

07b4495a039065fea54f129319fd1a9a1cda9381
lalaji <lalaji@a5903396-d722-0410-b921-86c7d4935375>
2012-05-18 05:44:28 +0000

    Added context validation
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@127645 a5903396-d722-0410-b921-86c7d4935375

--- components/apimgt/api-provider-web/src/site/themes/default/templates/item-add/js/load.js
+++ components/apimgt/api-provider-web/src/site/themes/default/templates/item-add/js/load.js
@@ -20,4 +20,19 @@
 			}
 		});
 
+     $.validator.addMethod('contextExists', function(value, element) {
+         if (value.charAt(0) != "/") {
+             value = "/" + value;
+         }
+         var contextExist = false;
+         jagg.syncPost("/site/blocks/item-add/ajax/add.jag", { action:"isContextExist", context:value },
+                       function (result) {
+                           if (!result.error) {
+                               contextExist = result.exist;
+                           }
+                       });
+         return this.optional(element) || contextExist != "true";
+     }, 'Duplicate context value.');
+
+
  });
--- components/apimgt/api-provider-web/src/site/themes/default/templates/page/base/js/jagg.js
+++ components/apimgt/api-provider-web/src/site/themes/default/templates/page/base/js/jagg.js
@@ -7,13 +7,25 @@ var jagg = jagg || {};
         $.post.apply(this, args);
     };
 
+    jagg.syncPost = function(url, data, callback, type) {
+        url = this.site.context + url;
+        return jQuery.ajax({
+                               type: "POST",
+                               url: url,
+                               data: data,
+                               async:false,
+                               success: callback,
+                               dataType:"json"
+                           });
+    },
+
     jagg.message = function (content) {
         $("#message-box:ui-dialog").dialog("destroy");
         var dialog = $("#message-box");
         $(".message", dialog).text(content);
         dialog.dialog({
-            modal:true,
-            dialogClass: "alert"
-        });
+                   modal:true,
+                   dialogClass: "alert"
+                  });
     };
 }());

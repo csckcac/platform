6e06796a3c9e6dd6a3b48b0b311489b63d5368fc
lalaji <lalaji@a5903396-d722-0410-b921-86c7d4935375>
2012-05-23 11:11:10 +0000

    Added multiple resource method support per uri template
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@128030 a5903396-d722-0410-b921-86c7d4935375

--- components/apimgt/api-provider-web/src/site/themes/default/templates/item-add/js/resource-template.js
+++ components/apimgt/api-provider-web/src/site/themes/default/templates/item-add/js/resource-template.js
@@ -6,18 +6,52 @@ var addResourcesToApi = function () {
     var countLength=$('#resourceCount').length;
     $('#resourceTableError').hide('fast');
     $('#resourceRow').clone(true).attr('id', 'item-' + resourcesCount).insertAfter($('#resourceRow'));
-    $('#item-' + resourcesCount + ' #resourceMethod').val($('#resourceMethod').val());
+    var resourceGet,resourcePut,resourcePost,resourceDelete;
+    if($('#resource-get').is(":checked")){
+    resourceGet=$('#resource-get').val();
+    $('#item-' + resourcesCount + ' #resource-get').val(resourceGet);
+    }if($('#resource-put').is(":checked")){
+    resourcePut=$('#resource-put').val();
+    $('#item-' + resourcesCount + ' #resource-put').val(resourcePut);
+    }if($('#resource-post').is(":checked")){
+    resourcePost=$('#resource-post').val();
+    $('#item-' + resourcesCount + ' #resource-post').val(resourcePost);
+    }if($('#resource-delete').is(":checked")){
+    resourceDelete=$('#resource-delete').val();
+    $('#item-' + resourcesCount + ' #resource-delete').val(resourceDelete);
+    }
     $('#item-' + resourcesCount + ' #uriTemplate').attr('disabled', 'disabled');
-    $('#item-' + resourcesCount + ' #resourceMethod').attr('disabled', 'disabled');
+    $('#item-' + resourcesCount + ' #resource-get').attr('disabled', 'disabled');
+    $('#item-' + resourcesCount + ' #resource-put').attr('disabled', 'disabled');
+    $('#item-' + resourcesCount + ' #resource-post').attr('disabled', 'disabled');
+    $('#item-' + resourcesCount + ' #resource-delete').attr('disabled', 'disabled');
+
+
+    $('#resource-get').attr('checked', false);
+    $('#resource-put').attr('checked', false);
+    $('#resource-post').attr('checked', false);
+    $('#resource-delete').attr('checked', false);
+
+
     $('#item-' + resourcesCount + ' td#buttons a#resourceAddBtn').remove();
     if($('#item-' + resourcesCount + ' td#buttons a#resourceDelBtn').length>0){
     $('#item-' + resourcesCount + ' td#buttons a#resourceDelBtn').remove();
     }
     $('#item-' + resourcesCount + ' td#buttons').append("<a class=\"btn btn-danger\" onclick=\"deleteResource(" + resourcesCount + ")\"><i class=\"icon-trash icon-white\"></i> Delete</a>");
 
+    var resourceMethodValues=new Array();
+    if(resourceGet!=null){
+    resourceMethodValues.push(resourceGet);
+    }if(resourcePut!=null){
+    resourceMethodValues.push(resourcePut);
+    }if(resourcePost!=null){
+    resourceMethodValues.push(resourcePost);
+    }if(resourceDelete!=null){
+    resourceMethodValues.push(resourceDelete);
+    }
 
     $('<input>').attr('type', 'hidden')
-        .attr('name', 'resourceMethod-' + resourcesCount).attr('value', $('#resourceMethod').val())
+        .attr('name', 'resourceMethod-' + resourcesCount).attr('value', resourceMethodValues)
         .appendTo('#addAPIForm');
 
     $('<input>').attr('type', 'hidden')
@@ -37,7 +71,6 @@ var addResourcesToApi = function () {
     }
 
     $('#uriTemplate').val('');
-    $('#resourceMethod').val('GET');
 
 };
 

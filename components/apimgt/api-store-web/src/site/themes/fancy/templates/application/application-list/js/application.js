8094406c1f08affeb5f8473ba35f6f5deae1694b
chanaka <chanaka@a5903396-d722-0410-b921-86c7d4935375>
2012-05-23 10:41:05 +0000

    Fixing some of the issues in api store
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@128028 a5903396-d722-0410-b921-86c7d4935375

--- components/apimgt/api-store-web/src/site/themes/fancy/templates/application/application-list/js/application.js
+++ components/apimgt/api-store-web/src/site/themes/fancy/templates/application/application-list/js/application.js
@@ -1,14 +1,16 @@
 function changeAppNameMode(linkObj){
-    var theTr = $(linkObj).parent().parent().attr('data-value');
-    $('td:first',appName).html('<div class="row-fluid"><div class="span6"> <input class="app_name_new" value="'+theTr.attr('data-value')+'" type="text" /> </div><div class="span6"><button class="btn btn-primary" onclick="updateApplication(this)">Save</button> <button class="btn" onclick="updateApplication_reset(this)">Cancel</button></div></div> ');
+    var theTr = $(linkObj).parent().parent();
+    var appName = $(theTr).attr('data-value');
+    $('td:first',theTr).html('<div class="row-fluid"><div class="span6"> <input class="app_name_new" value="'+theTr.attr('data-value')+'" type="text" /> </div><div class="span6"><button class="btn btn-primary" onclick="updateApplication(this)">Save</button> <button class="btn" onclick="updateApplication_reset(this)">Cancel</button></div></div> ');
 }
 function updateApplication_reset(linkObj){
       var appNameCell = $(linkObj).parent();
         appNameCell.html(appNameCell.val());
 }
-function updateApplication(saveButton){
-    var applicationOld = $(saveButton).parent().parent().attr('data-value');
-    var applicationNew = $(saveButton).prev().val();
+function updateApplication(linkObj){
+    var theTr = $(linkObj).parent().parent().parent().parent();
+    var applicationOld = $(theTr).attr('data-value');
+    var applicationNew = $('input.app_name_new',theTr).val();
         jagg.post("/site/blocks/application/application-update/ajax/application-update.jag", {
             action:"updateApplication",
             applicationOld:applicationOld,

74898695f39233226815be4caf90b40a1699b58f
chanaka <chanaka@a5903396-d722-0410-b921-86c7d4935375>
2012-05-23 12:13:05 +0000

    Removing the confirmation dialog for API adding
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@128042 a5903396-d722-0410-b921-86c7d4935375

--- components/apimgt/api-provider-web/src/site/themes/default/templates/item-add/js/load.js
+++ components/apimgt/api-provider-web/src/site/themes/default/templates/item-add/js/load.js
@@ -3,23 +3,12 @@
 			submitHandler: function(form) {
 				$(form).ajaxSubmit({
                     success:function(responseText, statusText, xhr, $form) {
-                        $('#apiModal h3.modal-title').html('API Provider');
-                        $('#apiModal div.modal-body').html('\n\nSuccessfully added new API.\n Do you want to leave this page?');
-                        $('#apiModal a.btn-primary').html('Yes');
-                        $('#apiModal a.btn-other').html('No. Stay and reset form.');
-                        $('#apiModal div.modal-footer').append($('<a class="btn btn-other-noreset" data-dismiss="modal">No. Stay</a>'));
-                        $('#apiModal a.btn-other').click(function(){
-                            v.resetForm();
-                        });
-                        $('#apiModal a.btn-primary').click(function() {
                             var current = window.location.pathname;
                             if (current.indexOf(".jag") >= 0) {
                                 location.href = "index.jag";
                             } else {
                                 location.href = 'site/pages/index.jag';
                             }
-                        });
-                        $('#apiModal').modal();
                     }
                 });
 			}

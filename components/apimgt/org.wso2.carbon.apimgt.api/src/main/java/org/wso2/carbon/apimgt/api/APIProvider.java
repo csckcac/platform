d3c91cea4cfdce2fcdb741ad7602bb68fed5088c
hiranya <hiranya@a5903396-d722-0410-b921-86c7d4935375>
2012-05-24 07:40:41 +0000

    Updated javadocs
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@128108 a5903396-d722-0410-b921-86c7d4935375

--- components/apimgt/org.wso2.carbon.apimgt.api/src/main/java/org/wso2/carbon/apimgt/api/APIProvider.java
+++ components/apimgt/org.wso2.carbon.apimgt.api/src/main/java/org/wso2/carbon/apimgt/api/APIProvider.java
@@ -138,7 +138,9 @@ public interface APIProvider extends APIManager {
     public void addAPI(API api) throws APIManagementException;
 
     /**
-     * Updates an existing API
+     * Updates an existing API. This method must not be used to change API status. Implementations
+     * should throw an exceptions when such attempts are made. All life cycle state changes
+     * should be carried out using the changeAPIStatus method of this interface.
      *
      * @param api API
      * @throws APIManagementException if failed to update API

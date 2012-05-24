5604a3b1aaeef79df3eddd389a7eb976a0cf9dbe
chanaka <chanaka@a5903396-d722-0410-b921-86c7d4935375>
2012-05-24 11:56:18 +0000

    Fixing the subscribe button popup button issue
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@128162 a5903396-d722-0410-b921-86c7d4935375

--- components/apimgt/api-store-web/src/site/themes/modern/templates/api/api-info/js/api-info.js
+++ components/apimgt/api-store-web/src/site/themes/modern/templates/api/api-info/js/api-info.js
@@ -23,32 +23,43 @@ $(document).ready(function () {
             $("#subscribe-button").html('Subscribe');
             $("#subscribe-button").removeAttr('disabled');
             if (result.error == false) {
-                window.location.reload();
+                $('#messageModal').html($('#confirmation-data').html());
+                $('#messageModal h3.modal-title').html('API Provider');
+                $('#messageModal div.modal-body').html('\n\nSuccessfully subscribed to the API.\n Do you want to go to the subscription page?');
+                $('#messageModal a.btn-primary').html('Yes');
+                $('#messageModal a.btn-other').html('No');
+                $('#messageModal a.btn-other').click(function() {
+                    window.location.reload();
+                });
+                $('#messageModal a.btn-primary').click(function() {
+                    location.href = "../site/pages/subscriptions.jag";
+                });
+                $('#messageModal').modal();
+
+
             } else {
                 jagg.message(result.message);
 
 
-                 //$('#messageModal').html($('#confirmation-data').html());
-                 /*$('#messageModal h3.modal-title').html('API Provider');
-                        $('#messageModal div.modal-body').html('\n\nSuccessfully subscribed to the API.\n Do you want to go to the subscription page?');
-                        $('#messageModal a.btn-primary').html('Yes');
-                        $('#messageModal a.btn-other').html('No');
-                        *//*$('#messageModal a.btn-other').click(function(){
-                            v.resetForm();
-                        });*//*
-                        $('#messageModal a.btn-primary').click(function() {
-                            var current = window.location.pathname;
-                            if (current.indexOf(".jag") >= 0) {
-                                location.href = "index.jag";
-                            } else {
-                                location.href = 'site/pages/index.jag';
-                            }
-                        });*/
+                //$('#messageModal').html($('#confirmation-data').html());
+                /*$('#messageModal h3.modal-title').html('API Provider');
+                 $('#messageModal div.modal-body').html('\n\nSuccessfully subscribed to the API.\n Do you want to go to the subscription page?');
+                 $('#messageModal a.btn-primary').html('Yes');
+                 $('#messageModal a.btn-other').html('No');
+                 */
+                /*$('#messageModal a.btn-other').click(function(){
+                 v.resetForm();
+                 });*/
+                /*
+                 $('#messageModal a.btn-primary').click(function() {
+                 var current = window.location.pathname;
+                 if (current.indexOf(".jag") >= 0) {
+                 location.href = "index.jag";
+                 } else {
+                 location.href = 'site/pages/index.jag';
+                 }
+                 });*/
 //                        $('#messageModal').modal();
-
-
-
-
 
 
             }

533f78881c41950456f7e96d7a45f092a21e67c0
chamara <chamara@a5903396-d722-0410-b921-86c7d4935375>
2012-05-25 07:04:52 +0000

    Adding governance API test
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@128241 a5903396-d722-0410-b921-86c7d4935375

--- platform-integration/system-test-framework/scenario/integration/org.wso2.automation.common.integration.test/src/test/java/org/wso2/automation/common/test/greg/governance/ServiceAPITests.java
+++ platform-integration/system-test-framework/scenario/integration/org.wso2.automation.common.integration.test/src/test/java/org/wso2/automation/common/test/greg/governance/ServiceAPITests.java
@@ -23,6 +23,8 @@ import org.apache.axis2.AxisFault;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import org.wso2.carbon.governance.api.endpoints.EndpointManager;
+import org.wso2.carbon.governance.api.endpoints.dataobjects.Endpoint;
 import org.wso2.carbon.governance.api.exception.GovernanceException;
 import org.wso2.carbon.governance.api.services.ServiceFilter;
 import org.wso2.carbon.governance.api.services.ServiceManager;
@@ -33,16 +35,22 @@ import org.wso2.carbon.registry.ws.client.registry.WSRegistryServiceClient;
 import org.wso2.platform.test.core.ProductConstant;
 import org.wso2.platform.test.core.utils.gregutils.GregUserIDEvaluator;
 import org.wso2.platform.test.core.utils.gregutils.RegistryProvider;
+
 import javax.xml.namespace.QName;
 import javax.xml.stream.XMLStreamException;
+
 import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
 
+/**
+ * Class will test Service manager specific governance API methods
+ */
 public class ServiceAPITests {
 
     String service_namespace = "http://example.com/demo/services";
     String service_name = "GovernanceAPIAutomatedTestService";
     public static ServiceManager serviceManager;
+    public static EndpointManager endpointManager;
 
     @BeforeClass(alwaysRun = true)
     public void initializeAPIObject() throws RegistryException, AxisFault {
@@ -50,36 +58,39 @@ public class ServiceAPITests {
         WSRegistryServiceClient registryWS = new RegistryProvider().getRegistry(userId, ProductConstant.GREG_SERVER_NAME);
         Registry governance = new RegistryProvider().getGovernance(registryWS, userId);
         serviceManager = new ServiceManager(governance);
+        endpointManager = new EndpointManager(governance);
         cleanService();
     }
 
-    //ToDo need remove specified services
     private void cleanService() throws GovernanceException {
         Service[] service = serviceManager.getAllServices();
         for (int i = 0; i <= service.length - 1; i++) {
             if (service[i].getQName().getLocalPart().contains(service_name)) {
-                System.out.println(service[i].getId());
                 serviceManager.removeService(service[i].getId());
             }
         }
     }
 
-    @Test(groups = {"wso2.governance.api"}, description = "Testing addService API method", priority = 1)
+    @Test(groups = {"wso2.greg.api"}, description = "Testing addService API method", priority = 1)
     public void testAddService() throws GovernanceException {
         boolean isServiceFound = false;
-        Service service = serviceManager.newService(new QName(service_namespace, service_name));
-        serviceManager.addService(service);
-        Service[] availableServices = serviceManager.getAllServices();
-        for (int i = 0; i <= availableServices.length - 1; i++) {
-            if (availableServices[i].getQName().getLocalPart().equalsIgnoreCase(service_name)) {
-                isServiceFound = true;
+        try {
+            Service service = serviceManager.newService(new QName(service_namespace, service_name));
+            serviceManager.addService(service);
+            Service[] availableServices = serviceManager.getAllServices();
+            for (int i = 0; i <= availableServices.length - 1; i++) {
+                if (availableServices[i].getQName().getLocalPart().equalsIgnoreCase(service_name)) {
+                    isServiceFound = true;
+                }
             }
+            assertTrue(isServiceFound, "Error occured while adding new service from governance API : " +
+                    "please check newService/addService and getAllService methods");
+        } catch (GovernanceException e) {
+            throw new GovernanceException("Error occured while executing addService and newService API methods : " + e);
         }
-        assertTrue(isServiceFound, "Error occured while adding new service from governance API : " +
-                "please check newService/addService and getAllService methods");
     }
 
-    @Test(groups = {"wso2.governance.api"}, description = "Testing getAllServicePath API method", priority = 2)
+    @Test(groups = {"wso2.greg.api"}, description = "Testing getAllServicePath API method", priority = 2)
     public void testGetAllServicePaths() throws GovernanceException {
         boolean isServicePathFound = false;
         String[] servicePath = serviceManager.getAllServicePaths();
@@ -92,7 +103,7 @@ public class ServiceAPITests {
     }
 
 
-    @Test(enabled = false, groups = {"wso2.governance.api"}, description = "Testing addNewService with inline service content", priority = 3)
+    @Test(enabled = false, groups = {"wso2.greg.api"}, description = "Testing addNewService with inline service content", priority = 3)
     public void testNewServiceWithOMElement() throws XMLStreamException, GovernanceException {
         boolean isServiceFound = false;
         String content = "<serviceMetaData xmlns=\"http://www.wso2.org/governance/metadata\">\n" +
@@ -119,7 +130,7 @@ public class ServiceAPITests {
 
 
     //https://wso2.org/jira/browse/CARBON-13194
-    @Test(groups = {"wso2.governance.api"}, description = "Testing updateService", priority = 4)
+    @Test(groups = {"wso2.greg.api"}, description = "Testing updateService", priority = 4)
     public void testUpdateService() throws GovernanceException {
         Service[] service = serviceManager.getAllServices();
         for (int serviceID = 0; serviceID <= service.length - 1; serviceID++) {
@@ -138,7 +149,7 @@ public class ServiceAPITests {
         }
     }
 
-    @Test(groups = {"wso2.governance.api"}, description = "Testing getService", priority = 5)
+    @Test(groups = {"wso2.greg.api"}, description = "Testing getService", priority = 5)
     public void testGetService() throws GovernanceException {
         Service service;
         boolean isServiceFound = false;
@@ -152,7 +163,7 @@ public class ServiceAPITests {
         assertTrue(isServiceFound, "getService governance API method does not work.");
     }
 
-    @Test(groups = {"wso2.governance.api"}, description = "Testing removeService", priority = 6)
+    @Test(groups = {"wso2.greg.api"}, description = "Testing removeService", priority = 6)
     public void testFindService() throws GovernanceException, XMLStreamException {
         serviceManager.findServices(new ServiceFilter() {
             public boolean matches(Service service) throws GovernanceException {
@@ -164,11 +175,12 @@ public class ServiceAPITests {
         );
     }
 
-    @Test(groups = {"wso2.governance.api"}, description = "Testing lifecycle methods of a service", priority = 7)
+    @Test(groups = {"wso2.greg.api"}, description = "Testing lifecycle methods of a service", priority = 7)
     public void testLifeCycleMethods() throws GovernanceException {
+        String defaultLC = "ServiceLifeCycle";
         Service[] service = serviceManager.getAllServices();
         for (int serviceID = 0; serviceID <= service.length - 1; serviceID++) {
-            String defaultLC = "ServiceLifeCycle";
+
             if (service[serviceID].getQName().getLocalPart().contains(service_name)) {
                 try {
                     service[serviceID].attachLifecycle(defaultLC);
@@ -178,21 +190,22 @@ public class ServiceAPITests {
                 }
                 serviceManager.updateService(service[serviceID]);
             }
-            service = serviceManager.getAllServices();
-            for (int id = 0; id <= service.length - 1; id++) {
-                if (service[id].getQName().getLocalPart().contains(service_name)) {
-                    Service objService = serviceManager.getService(service[id].getId());
-                    assertTrue(objService.getLifecycleName().equalsIgnoreCase(defaultLC),
-                            "Error in getLifeCycleName API method");
-                    assertTrue(objService.getLifecycleState().contains("Development"),
-                            "Error in getLifeCycleState API method");
-                }
+        }
+        service = serviceManager.getAllServices();
+        for (int id = 0; id <= service.length - 1; id++) {
+            if (service[id].getQName().getLocalPart().contains(service_name)) {
+                Service objService = serviceManager.getService(service[id].getId());
+                assertTrue(objService.getLifecycleName().equalsIgnoreCase(defaultLC),
+                        "Error in getLifeCycleName API method");
+                assertTrue(objService.getLifecycleState().contains("Development"),
+                        "Error in getLifeCycleState API method");
             }
+
         }
     }
 
 
-    @Test(groups = {"wso2.governance.api"}, description = "Testing removeService", priority = 8)
+    @Test(groups = {"wso2.greg.api"}, description = "Testing removeService", priority = 8)
     public void testRemoveService() throws GovernanceException {
         Service[] service = serviceManager.getAllServices();
         for (int i = 0; i <= service.length - 1; i++) {
@@ -213,8 +226,8 @@ public class ServiceAPITests {
         }
     }
 
-    @Test(groups = {"wso2.governance.api"}, description = "Testing setAttribute/getAttribute methods of a service", priority = 9)
-    public void testSetAttributeMethod() throws GovernanceException {
+    @Test(groups = {"wso2.greg.api"}, description = "Testing setAttribute/getAttribute methods of a service", priority = 9)
+    public void testServiceSetAttributeMethod() throws GovernanceException {
         Service service = serviceManager.newService(new QName(service_namespace, service_name));
         try {
             service.setAttribute("overview_description", "Hello");
@@ -234,13 +247,78 @@ public class ServiceAPITests {
         }
     }
 
+
+    @Test(groups = {"wso2.greg.api"}, description = "Testing addEndpoint method of a service", priority = 9)
+    public void testServiceEndPoint() throws GovernanceException {
+        Service[] allServices = serviceManager.getAllServices();
+        Endpoint endPoint;
+        try {
+            for (int i = 0; i <= allServices.length - 1; i++) {
+                if (allServices[i].getQName().getLocalPart().contains(service_name)) {
+                    endPoint = endpointManager.newEndpoint("http://localhost:9763/services/Axis2Service3");
+                    endpointManager.addEndpoint(endPoint);
+                    allServices[i].attachEndpoint(endPoint);
+                }
+            }
+        } catch (GovernanceException e) {
+            throw new GovernanceException("Error occurred while executing newEndpoint/addEndpoint and " +
+                    "attachEndpoint API methods : " + e);
+        }
+
+        allServices = serviceManager.getAllServices();
+        try {
+            for (int i = 0; i <= allServices.length - 1; i++) {
+                if (allServices[i].getQName().getLocalPart().contains(service_name)) {
+                    Endpoint[] endpoints = allServices[i].getAttachedEndpoints();
+                    assertTrue(endpoints[0].getUrl().equalsIgnoreCase("http://localhost:9763/services/" +
+                            "Axis2Service3"), "Error occurred while executing getAttachedEndpoints API method ");
+                }
+            }
+        } catch (GovernanceException e) {
+            throw new GovernanceException("Error occurred while executing getAttachedEndpoints API method" + e);
+        }
+
+        allServices = serviceManager.getAllServices();
+        try {
+            for (int i = 0; i <= allServices.length - 1; i++) {
+                if (allServices[i].getQName().getLocalPart().contains(service_name)) {
+                    Endpoint[] endpoints = allServices[i].getAttachedEndpoints();
+                    allServices[i].detachEndpoint(endpoints[0].getId());
+                }
+            }
+        } catch (GovernanceException e) {
+            throw new GovernanceException("Error occurred while  executing detachEndpoint API method :  " + e);
+        }
+    }
+
     //https://wso2.org/jira/browse/CARBON-13202
-    //ToDo need to assert with expected behabiour
-    @Test(enabled = false ,groups = {"wso2.governance.api"}, description = "Testing addAttribute methods of a service", priority = 9)
-    public void testAddAttribute() throws GovernanceException {
+    @Test(groups = {"wso2.greg.api"}, description = "Testing addAttribute methods of a service", priority = 10)
+    public void testServiceAddAttribute() throws GovernanceException {
         cleanService();
         Service service = serviceManager.newService(new QName(service_namespace, service_name));
-        service.addAttribute("overview_TEST", "SAMPLE_ATTRIBUTE");
+        service.setAttribute("endpoints_entry", "Dev:http://localhost:9763/services/SAMPLESERVICE1");
+        try {
+            service.addAttribute("endpoints_entry", "QA:http://localhost:9763/services/SAMPLESERVICE2");
+        } catch (GovernanceException e) {
+            throw new GovernanceException("Error occured while executing addAttribute API method for a Service" + e);
+        }
         serviceManager.addService(service);
+
+        Service[] serviceList = serviceManager.getAllServices();
+        Endpoint[] endPoint;
+        boolean isEndpointFound = false;
+
+        for (int i = 0; i <= serviceList.length - 1; i++) {
+            if (serviceList[i].getQName().getLocalPart().contains(service_name)) {
+                endPoint = serviceList[i].getAttachedEndpoints();
+                for(Endpoint e:endPoint){
+                    if(e.getUrl().contains("http://localhost:9763/services/SAMPLESERVICE2")){
+                        isEndpointFound = true;
+                    }
+                }
+            }
+        }
+        assertTrue(isEndpointFound,"addAttribute API not execute with Service types");
     }
+
 }

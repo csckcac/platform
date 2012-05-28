e67139edaf53d64163cda8ab312e769a6d42da74
lalaji <lalaji@a5903396-d722-0410-b921-86c7d4935375>
2012-05-28 11:28:52 +0000

    Added tier description to show in add-api page
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@128524 a5903396-d722-0410-b921-86c7d4935375

--- components/apimgt/api-provider-web/src/site/themes/default/templates/item-add/js/load.js
+++ components/apimgt/api-provider-web/src/site/themes/default/templates/item-add/js/load.js
@@ -10,8 +10,10 @@ function loadTiers() {
                       for (var j = 0; j < arr.length; j++) {
                           option = new Option(arr[j], arr[j]);
                           target.options[j] = option;
+                          target.options[j].title = result.tiers[j].tierDescription;
                           if (j == 0) {
                               target.options[j].selected = 'selected';
+                              $("#tiersHelp").html(result.tiers[0].tierDescription);
                               var tierArr = [];
                               tierArr.push(target.options[j].value);
                               $('<input>').attr('type', 'hidden')
@@ -25,11 +27,16 @@ function loadTiers() {
               }, "json");
 }
 
+var setTierDescription= function(option) {
+    $("#tiersHelp").html(option);
+};
+
 
 $(document).ready(function() {
     $("select[name='tier']").change(function() {
         // multipleValues will be an array
         var multipleValues = $(this).val() || [];
+        setTierDescription($("select option:selected").attr("title"));
         var countLength = $('#tiersCollection').length;
         if (countLength == 0) {
 

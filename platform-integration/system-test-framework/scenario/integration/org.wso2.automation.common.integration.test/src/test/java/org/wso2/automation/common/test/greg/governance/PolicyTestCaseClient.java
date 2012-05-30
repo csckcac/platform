ca31be6dd758c8a98e8ee5936fbcd0c4b3b70941
krishantha <krishantha@a5903396-d722-0410-b921-86c7d4935375>
2012-05-30 14:52:06 +0000

    More test cases for policyManager
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@128882 a5903396-d722-0410-b921-86c7d4935375

--- platform-integration/system-test-framework/scenario/integration/org.wso2.automation.common.integration.test/src/test/java/org/wso2/automation/common/test/greg/governance/PolicyTestCaseClient.java
+++ platform-integration/system-test-framework/scenario/integration/org.wso2.automation.common.integration.test/src/test/java/org/wso2/automation/common/test/greg/governance/PolicyTestCaseClient.java
@@ -31,6 +31,17 @@ import org.wso2.platform.test.core.ProductConstant;
 import org.wso2.platform.test.core.utils.gregutils.GregUserIDEvaluator;
 import org.wso2.platform.test.core.utils.gregutils.RegistryProvider;
 
+import javax.xml.namespace.QName;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+import static org.testng.Assert.assertNull;
+import static org.testng.Assert.assertTrue;
+
 public class PolicyTestCaseClient {
 
     private Registry governance;
@@ -94,5 +105,114 @@ public class PolicyTestCaseClient {
         Assert.assertNull(deletedPolicy);
     }
 
+    @Test(groups = {"wso2.greg"}, description = "Add 1000 policies")
+    public void testAddLargeNumberOfPolicies() throws GovernanceException {
+        int policyCount = 20;
+        PolicyManager policyManager = new PolicyManager(governance);
 
+        Policy policy;
+        int policyCountBeforeTest = policyManager.getAllPolicies().length;
+
+        for (int i = 1; i <= policyCount; i++) {
+            policy = policyManager.newPolicy("http://svn.wso2.org/repos/wso2/carbon/platform/" +
+                                             "trunk/platform-integration/system-test-framework" +
+                                             "/core/org.wso2.automation.platform.core/src/main/" +
+                                             "resources/artifacts/GREG/policy/UTPolicy.xml");
+            policy.setName("WSO2AutomationUTPolicy" + i + ".xml");
+            policyManager.addPolicy(policy);
+        }
+        Policy[] policies = policyManager.getAllPolicies();
+        int policyCountAfterTest = policies.length;
+        assertTrue(((policyCountAfterTest - policyCountBeforeTest) == policyCount),
+                   "All " + policyCount + "policies were not added");
+
+        //delete policies
+        for (Policy policyEntry : policies) {
+            if (policyEntry.getQName().toString().contains("WSO2AutomationUTPolicy")) {
+                policyManager.removePolicy(policyEntry.getId());
+                assertNull(policyManager.getPolicy(policyEntry.getId()));
+            }
+        }
+    }
+
+    @Test(groups = {"wso2.greg"}, description = "Adding duplicate policy")
+    public void testAddDuplicatePolicy() throws GovernanceException {
+        PolicyManager policyManager = new PolicyManager(governance);
+
+        Policy policy = policyManager.newPolicy("http://svn.wso2.org/repos/wso2/carbon/platform/" +
+                                                "trunk/platform-integration/system-test-framework" +
+                                                "/core/org.wso2.automation.platform.core/src/main/" +
+                                                "resources/artifacts/GREG/policy/UTPolicy.xml");
+        policy.setName("WSO2AutomationUTPolicyDuplicate.xml");
+        policyManager.addPolicy(policy);
+        assertTrue(policy.getQName().toString().contains("WSO2AutomationUTPolicyDuplicate.xml"));
+
+        Policy policyDuplicate = policyManager.newPolicy("http://svn.wso2" +
+                                                         ".org/repos/wso2/carbon/platform/" +
+                                                         "trunk/platform-integration/system-test-framework" +
+                                                         "/core/org.wso2.automation.platform.core/src/main/" +
+                                                         "resources/artifacts/GREG/policy/UTPolicy.xml");
+        policyDuplicate.setName("WSO2AutomationUTPolicyDuplicate.xml");
+        policyManager.addPolicy(policyDuplicate);
+
+        assertTrue(policyDuplicate.getQName().toString().contains("WSO2AutomationUTPolicyDuplicate.xml"));
+        policyManager.removePolicy(policy.getId());
+        assertNull(policyManager.getPolicy(policy.getId()));
+    }
+
+    @Test(groups = {"wso2.greg"}, description = "Adding invalid policy")
+    public void testAddInvalidPolicy() throws GovernanceException {
+        PolicyManager policyManager = new PolicyManager(governance);
+
+        Policy policy = policyManager.newPolicy("https://svn.wso2.org/repos/wso2/carbon/platform/" +
+                                                "trunk/platform-integration/system-test-framework/" +
+                                                "core/org.wso2.automation.platform.core/src/main/" +
+                                                "resources/artifacts/GREG/policy/invlidPolicySyntax.xml");
+
+        policy.setName("invalidPolicy.xml");
+        policyManager.addPolicy(policy);
+        assertTrue(policy.getQName().toString().contains("invalidPolicy.xml"));
+        policyManager.removePolicy(policy.getId());
+        assertNull(policyManager.getPolicy(policy.getId()));
+    }
+
+    @Test(groups = {"wso2.greg"}, description = "Update policy content")
+    public void testUpdatePolicy() throws GovernanceException, IOException {
+        PolicyManager policyManager = new PolicyManager(governance);
+        String updatedPolicyPath = ProductConstant.SYSTEM_TEST_RESOURCE_LOCATION + File.separator
+                                   + "artifacts" + File.separator + "GREG" + File.separator +
+                                   "policy" + File.separator + "UTPolicy-updated.xml";
+
+        Policy policy = policyManager.newPolicy("http://svn.wso2.org/repos/wso2/carbon/platform/" +
+                                                "trunk/platform-integration/system-test-framework" +
+                                                "/core/org.wso2.automation.platform.core/src/main/" +
+                                                "resources/artifacts/GREG/policy/UTPolicy.xml");
+
+        policy.setName("NewPolicyAutomation.xml");
+        policyManager.addPolicy(policy);
+        assertTrue(policy.getQName().toString().contains("NewPolicyAutomation.xml"));
+
+
+        File newPolicyFile = new File(updatedPolicyPath);
+        StringBuilder strContent = new StringBuilder("");
+
+        if (newPolicyFile.exists()) {
+            FileInputStream inputStream = new FileInputStream(newPolicyFile);
+
+            int character;
+            while ((character = inputStream.read()) != -1) {
+                strContent.append((char) character);
+            }
+            inputStream.close();
+        }
+        policy.setPolicyContent(strContent.toString());
+        policyManager.updatePolicy(policy);
+
+        assertTrue(policy.getQName().toString().contains("NewPolicyAutomation.xml"));
+        assertTrue(policyManager.getPolicy(policy.getId()).getPolicyContent().contains("PolicyUpdatedForAutomationTest"),
+                   "Updated content not found");
+
+        policyManager.removePolicy(policy.getId());
+        assertNull(policyManager.getPolicy(policy.getId()));
+    }
 }

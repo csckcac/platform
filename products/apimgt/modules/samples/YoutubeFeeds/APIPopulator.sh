a420c2ae11dc355af2ce129f4f085363119dfe26
hiranya <hiranya@a5903396-d722-0410-b921-86c7d4935375>
2012-05-30 14:02:29 +0000

    Updating release note and samples
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@128879 a5903396-d722-0410-b921-86c7d4935375

--- products/apimgt/modules/samples/YoutubeFeeds/APIPopulator.sh
+++ products/apimgt/modules/samples/YoutubeFeeds/APIPopulator.sh
@@ -2,11 +2,11 @@
 START=$1
 COUNT=$2
 SERVER=http://localhost:9763
-curl -X POST -c cookies $SERVER/apiprovider/site/blocks/user/login/ajax/login.jag -d 'action=login&username=provider1&password=provider1';
+curl -X POST -c cookies $SERVER/apiprovider/site/blocks/user/login/ajax/login.jag -d 'action=login&username=admin&password=admin';
 
-curl -X POST -b cookies $SERVER/apiprovider/site/blocks/item-add/ajax/add.jag -d "action=addAPI&name=YoutubeFeeds&version=1.0.0&description=Youtube Live Feeds&endpoint=http://gdata.youtube.com/feeds/api/standardfeeds&wsdl=&tags=youtube,gdata,multimedia&tier=Silver&thumbUrl=http://www.10bigideas.com.au/www/573/files/pf-thumbnail-youtube_logo.jpg&context=/youtube&resourceCount=0&resourceMethod-0=GET&uriTemplate-0=/*";
+curl -X POST -b cookies $SERVER/apiprovider/site/blocks/item-add/ajax/add.jag -d "action=addAPI&name=YoutubeFeeds&version=1.0.0&description=Youtube Live Feeds&endpoint=http://gdata.youtube.com/feeds/api/standardfeeds&wsdl=&tags=youtube,gdata,multimedia&tier=Silver&thumbUrl=http://www.10bigideas.com.au/www/573/files/pf-thumbnail-youtube_logo.jpg&context=/youtube&tiersCollection=Gold&resourceCount=0&resourceMethod-0=GET&uriTemplate-0=/*";
 
-curl -X POST -b cookies $SERVER/apiprovider/site/blocks/item-add/ajax/add.jag -d "action=updateAPI&name=YoutubeFeeds&version=1.0.0&description=Youtube Live Feeds&endpoint=http://gdata.youtube.com/feeds/api/standardfeeds&wsdl=&tags=youtube,gdata,multimedia&tier=Silver&thumbUrl=http://www.10bigideas.com.au/www/573/files/pf-thumbnail-youtube_logo.jpg&context=/youtube&status=PUBLISHED&resourceCount=0&resourceMethod-0=GET&uriTemplate-0=/*";
+curl -X POST -b cookies $SERVER/apiprovider/site/blocks/life-cycles/ajax/update-status.jag -d "name=YoutubeFeeds&version=1.0.0&provider=admin&status=PUBLISHED&publishToGateway=true&action=updateStatus";
 
 
 

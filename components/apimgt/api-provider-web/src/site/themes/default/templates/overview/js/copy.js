45d7c14489820d0ffd2adec2d93b782ff12a8995
lalaji <lalaji@a5903396-d722-0410-b921-86c7d4935375>
2012-05-31 11:59:58 +0000

    Fixed two bugs found in copyAPI and editAPI methods
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@128959 a5903396-d722-0410-b921-86c7d4935375

--- components/apimgt/api-provider-web/src/site/themes/default/templates/overview/js/copy.js
+++ components/apimgt/api-provider-web/src/site/themes/default/templates/overview/js/copy.js
@@ -1,8 +1,8 @@
-var copyAPIToNewVersion = function () {
+var copyAPIToNewVersion = function (provider) {
     var apiName = $("#item-info h2")[0].innerHTML.split("-v")[0];
     var version = $("#item-info h2")[0].innerHTML.split("-v")[1];
     var newVersion = $("#copy-api #new-version").val();
-    jagg.post("/site/blocks/overview/ajax/overview.jag", { action:"createNewAPI", apiName:apiName, version:version, newVersion:newVersion },
+    jagg.post("/site/blocks/overview/ajax/overview.jag", { action:"createNewAPI", provider:provider,apiName:apiName, version:version, newVersion:newVersion },
               function (result) {
                   if (!result.error) {
                       $("#copy-api #new-version").val('');

fac8e25d65d4b050d233bd0bb7c8715ee1d0cf0d
chanaka <chanaka@a5903396-d722-0410-b921-86c7d4935375>
2012-06-05 06:27:06 +0000

    Sign up page redirecting when the user is already log-in
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@129170 a5903396-d722-0410-b921-86c7d4935375

--- components/apimgt/api-store-web/src/site/themes/fancy/templates/user/sign-up/js/sign-up.js
+++ components/apimgt/api-store-web/src/site/themes/fancy/templates/user/sign-up/js/sign-up.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $.validator.addMethod("matchPasswords", function(value) {
 		return value == $("#password").val();
-	}, "The passwords you entered do not match. Please try again.");
+	}, "The passwords you entered do not match.");
 
     $("#sign-up").validate({
      submitHandler: function(form) {

3ef095fcda4d6573913af8a82c525e9632598bd0
dharshanaw <dharshanaw@a5903396-d722-0410-b921-86c7d4935375>
2012-06-06 11:35:06 +0000

    Modifications for codeCoverage - emma
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@129358 a5903396-d722-0410-b921-86c7d4935375

--- platform-integration/system-test-framework/core/org.wso2.automation.platform.core/src/main/java/org/wso2/platform/test/core/PlatformSuiteManager.java
+++ platform-integration/system-test-framework/core/org.wso2.automation.platform.core/src/main/java/org/wso2/platform/test/core/PlatformSuiteManager.java
@@ -5,43 +5,63 @@ import org.apache.commons.logging.LogFactory;
 import org.testng.Assert;
 import org.testng.ISuite;
 import org.testng.ISuiteListener;
+import org.wso2.platform.test.core.utils.emmautils.CodeCoverageUtils;
 import org.wso2.platform.test.core.utils.environmentutils.EnvironmentBuilder;
+import org.wso2.platform.test.core.utils.serverutils.ServerManager;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class PlatformSuiteManager implements ISuiteListener {
 
     private static final Log log = LogFactory.getLog(PlatformSuiteManager.class);
+    ServerManager serverManager = null;
+    List<ServerManager> serverList = new ArrayList<ServerManager>();
+    EnvironmentBuilder environmentBuilder;
 
     /**
      * This method is invoked before the SuiteRunner starts.
      */
-    public void onStart(ISuite suite) {
+    public synchronized void onStart(ISuite suite) {
         setKeyStoreProperties();
+        environmentBuilder = new EnvironmentBuilder();
         try {
-            EnvironmentBuilder environmentBuilder = new EnvironmentBuilder();
+
             boolean deploymentEnabled =
                     environmentBuilder.getFrameworkSettings().getEnvironmentSettings().isEnableDipFramework();
             boolean startosEnabled =
                     environmentBuilder.getFrameworkSettings().getEnvironmentSettings().is_runningOnStratos();
+            List<String> defaultProductList = environmentBuilder.getFrameworkSettings().getEnvironmentVariables().getProductList();
+
             if (startosEnabled) {
                 //stratos user populate on manager. there for product lis set to null
-            //    new UserPopulator().populateUsers(null);
+
+                new UserPopulator().populateUsers(null);
             } else {
+                if (environmentBuilder.getFrameworkSettings().getCoverageSettings().getCoverageEnable()) {
+                    for (Object carbonHomePath : environmentBuilder.getFrameworkSettings().getCoverageSettings().getCarbonHome().values()) {
+                        CodeCoverageUtils.instrument(carbonHomePath.toString());
+                        serverManager = new ServerManager(carbonHomePath.toString());
+                        serverManager.start();
+                        serverList.add(serverManager);
+                    }
+                    CodeCoverageUtils.init();
+                }
+
                 if (suite.getParameter("server.list") != null) {
                     List<String> productList = Arrays.asList(suite.getParameter("server.list").split(","));
                     if (deploymentEnabled) {
                         log.info("Starting all servers");
                         ServerGroupManager.startServers(productList);
                     }
-                   // new UserPopulator().populateUsers(productList);
+                    new UserPopulator().populateUsers(productList);
+                } else {
+                    new UserPopulator().populateUsers(defaultProductList);
                 }
             }
-
         } catch (Exception e) {  /*cannot throw the exception */
             log.error(e);
-            Assert.fail("Fail start servers " + e);
         }
     }
 
@@ -51,12 +71,28 @@ public class PlatformSuiteManager implements ISuiteListener {
      */
     public void onFinish(ISuite suite) {
         try {
+            if (serverList.size() != 0) {
+                for (ServerManager server : serverList) {
+                    server.shutdown();
+                }
+            }
+            for(ServerManager servers:serverList)
+            {
+                servers.shutdown();
+            }
+            if (environmentBuilder.getFrameworkSettings().getCoverageSettings().getCoverageEnable()) {
+                for (Object carbonHomePath : environmentBuilder.getFrameworkSettings().getCoverageSettings().getCarbonHome().values()) {
+                    CodeCoverageUtils.generateReports(carbonHomePath.toString());
+                }
+            }
+            stopMultipleServers(suite.getParameter("server.list"));
             if (suite.getParameter("server.list") != null) {
-                stopMultipleServers(suite.getParameter("server.list"));
+
             }
         } catch (Exception e) { /*cannot throw the exception */
             log.error(e);
-            Assert.fail("Fail to stop servers " + e);
+
+            Assert.fail("Fail to stop servers " + e.getMessage());
         }
     }
 

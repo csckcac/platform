61d48fff4095ca4a341bb450234712e9d5c74807
hiranya <hiranya@a5903396-d722-0410-b921-86c7d4935375>
2012-06-06 11:55:29 +0000

    Updating populator
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@129361 a5903396-d722-0410-b921-86c7d4935375

--- components/apimgt/perf-testing/org.wso2.carbon.apimgt.perf.populator/src/main/java/org/wso2/carbon/apimgt/perf/populator/SubscriptionPopulator.java
+++ components/apimgt/perf-testing/org.wso2.carbon.apimgt.perf.populator/src/main/java/org/wso2/carbon/apimgt/perf/populator/SubscriptionPopulator.java
@@ -16,10 +16,10 @@ public class SubscriptionPopulator {
 		String USER = "root";
 		String PASS = "root123";*/
 
-        String JDBC_DRIVER = "org.h2.Driver";
-        String DB_URL = "jdbc:h2:tcp://localhost/~/am1";
-        String USER = "sa";
-        String PASS = "sa";
+        String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+        String DB_URL = "jdbc:mysql://localhost:3306/newam";
+        String USER = "root";
+        String PASS = "root123";
 
 		Connection conn = null;
 		try {
@@ -27,12 +27,12 @@ public class SubscriptionPopulator {
 			conn = DriverManager.getConnection(DB_URL, USER, PASS);
 			conn.setAutoCommit(false);
 
-			String INSERT_APPLICATIONS_SQL = "INSERT INTO AM_APPLICATION (NAME,SUBSCRIBER_ID ) VALUES (?,?)";
+			String INSERT_APPLICATIONS_SQL = "INSERT INTO AM_APPLICATION (NAME,SUBSCRIBER_ID) VALUES (?,?)";
+            String INSERT_API_SQL = "INSERT INTO AM_API (API_PROVIDER, API_NAME, API_VERSION, CONTEXT) VALUES (?,?,?,?)";
 			String INSERT_SUBSCRIPTIONS_SQL = "INSERT INTO AM_SUBSCRIPTION ( TIER_ID , API_ID , APPLICATION_ID) VALUES (?,?,?)";
-			String INSERT_KEY_CONTEXT_MAPPING_SQL = "INSERT INTO AM_KEY_CONTEXT_MAPPING ( CONTEXT , VERSION , ACCESS_TOKEN) VALUES (?,?,?)";
 			String INSERT_OAUTH2_ACCESS_TOKEN_SQL = "INSERT INTO IDENTITY_OAUTH2_ACCESS_TOKEN ( ACCESS_TOKEN ,AUTHZ_USER , CONSUMER_KEY , REFRESH_TOKEN ,TIME_CREATED , TOKEN_SCOPE , TOKEN_STATE ,VALIDITY_PERIOD ) "
 					+ "VALUES (?,NULL,?,NULL,NULL,'PRODUCTION','ACTIVE',-1)";
-			String INSERT_SUBSCRIPTION_KEY_MAPPING_SQL = "INSERT INTO AM_SUBSCRIPTION_KEY_MAPPING (SUBSCRIPTION_ID, KEY_CONTEXT_MAPPING_ID, KEY_TYPE) VALUES (?,?,?)";
+			String INSERT_SUBSCRIPTION_KEY_MAPPING_SQL = "INSERT INTO AM_SUBSCRIPTION_KEY_MAPPING (SUBSCRIPTION_ID, ACCESS_TOKEN, KEY_TYPE) VALUES (?,?,?)";
 			String INSERT_CONSUMER_APPLICATION_SQL = "INSERT INTO IDENTITY_OAUTH_CONSUMER_APPLICATIONS ( APP_NAME , CALLBACK_URL , CONSUMER_KEY , CONSUMER_SECRET ,OAUTH_VERSION"
 					+ " , TENANT_ID , USERNAME ) VALUES (NULL,NULL,?,?,'2.0',0,'admin')";
 			String INSERT_SUBSCRIBER_SQL = "INSERT INTO AM_SUBSCRIBER  (USER_ID ,TENANT_ID,DATE_SUBSCRIBED ) VALUES(?,?,NOW())";
@@ -43,10 +43,10 @@ public class SubscriptionPopulator {
 			PreparedStatement psSubscriptions = conn.prepareStatement(
 					INSERT_SUBSCRIPTIONS_SQL,
 					PreparedStatement.RETURN_GENERATED_KEYS);
-			PreparedStatement psKeyContextMappings = conn.prepareStatement(
-					INSERT_KEY_CONTEXT_MAPPING_SQL,
-					PreparedStatement.RETURN_GENERATED_KEYS);
-			PreparedStatement psOAuth2AccessTokenSQL = conn
+			PreparedStatement psAPIs = conn.prepareStatement(
+                    INSERT_API_SQL,
+                    PreparedStatement.RETURN_GENERATED_KEYS);
+            PreparedStatement psOAuth2AccessTokenSQL = conn
 					.prepareStatement(INSERT_OAUTH2_ACCESS_TOKEN_SQL);
 			PreparedStatement psSubscriptionKeyMappingSQL = conn
 					.prepareStatement(INSERT_SUBSCRIPTION_KEY_MAPPING_SQL);
@@ -61,7 +61,7 @@ public class SubscriptionPopulator {
 			psSubscriberSQL.setString(1, "perf1");
 			psSubscriberSQL.setInt(2, 0);
 			psSubscriberSQL.executeUpdate();
-			ResultSet rs = psApplications.getGeneratedKeys();
+			ResultSet rs = psSubscriberSQL.getGeneratedKeys();
 			int subscriberId = 0;
             int id;
 			if (rs.next()) {
@@ -69,6 +69,18 @@ public class SubscriptionPopulator {
 
 			}
 			rs.close();
+
+            psAPIs.setString(1, "admin");
+            psAPIs.setString(2, "StockTicker");
+            psAPIs.setString(3, "1.0.0");
+            psAPIs.setString(4, "/ticker");
+            psAPIs.executeUpdate();
+            rs = psAPIs.getGeneratedKeys();
+            int apiId = -1;
+            if (rs.next()) {
+                apiId = rs.getInt(1);
+            }
+            rs.close();
 
 			int loopCount = 100000;
 			String applicationName;
@@ -88,7 +100,7 @@ public class SubscriptionPopulator {
 
 				// Add Subscription
 				psSubscriptions.setString(1, "Unlimited");
-				psSubscriptions.setString(2, "admin_StockTicker_1.0.0");
+				psSubscriptions.setInt(2, apiId);
 				psSubscriptions.setInt(3, Integer.valueOf(applicationId));
 				psSubscriptions.executeUpdate();
 				rs = psSubscriptions.getGeneratedKeys();
@@ -100,23 +112,11 @@ public class SubscriptionPopulator {
 
 				// Add Key Context Mapping
 				String accessKey = "9nEQnijLZ0Gi0gZ6a3pZIC" + i;
-				psKeyContextMappings.setString(1, "/ticker");
-				psKeyContextMappings.setString(2, "1.0.0");
-				// TODO : add padding to make the key length constant
-				psKeyContextMappings.setString(3, accessKey);
-				psKeyContextMappings.executeUpdate();
-				rs = psKeyContextMappings.getGeneratedKeys();
-				int keyContexMappingId = 0;
-				if (rs.next()) {
-					keyContexMappingId = rs.getInt(1);
-				}
-				rs.close();
 
 				// Add Subscription Key Mapping
 				psSubscriptionKeyMappingSQL.setInt(1,
 						Integer.valueOf(subscriptionId));
-				psSubscriptionKeyMappingSQL.setInt(2,
-						Integer.valueOf(keyContexMappingId));
+				psSubscriptionKeyMappingSQL.setString(2, accessKey);
 				psSubscriptionKeyMappingSQL.setString(3, "PRODUCTION");
 				psSubscriptionKeyMappingSQL.executeUpdate();
 
@@ -132,11 +132,11 @@ public class SubscriptionPopulator {
 
 				psApplications.clearParameters();
 				psSubscriptions.clearParameters();
-				psKeyContextMappings.clearParameters();
 				psOAuth2AccessTokenSQL.clearParameters();
 				psSubscriptionKeyMappingSQL.clearParameters();
 				psConsumerApplicationSQL.clearParameters();
 				conn.commit();
+                System.out.println("Iteration: " + i + " completed");
 			}
 
 		} catch (ClassNotFoundException e) {

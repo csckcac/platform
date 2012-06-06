013d98c06fe18beaf038a50aac6ea14ee56e119d
lalaji <lalaji@a5903396-d722-0410-b921-86c7d4935375>
2012-06-06 07:57:29 +0000

    Added method to get lifecycle change events as a list
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@129320 a5903396-d722-0410-b921-86c7d4935375

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/APIProviderImpl.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/APIProviderImpl.java
@@ -720,4 +720,16 @@ class APIProviderImpl extends AbstractAPIManager implements APIProvider {
             handleException("Failed to add documentation", e);
         }
     }
+
+    /**
+     * Returns the details of all the life-cycle changes done per api
+     *
+     * @param apiId API Identifier
+     * @return List of lifecycle events per given api
+     * @throws org.wso2.carbon.apimgt.api.APIManagementException
+     *          If failed to get Lifecycle Events
+     */
+    public List<LifeCycleEvent> getLifeCycleEvents(APIIdentifier apiId) throws APIManagementException {
+        return apiMgtDAO.getLifeCycleEvents(apiId);
+    }
 }

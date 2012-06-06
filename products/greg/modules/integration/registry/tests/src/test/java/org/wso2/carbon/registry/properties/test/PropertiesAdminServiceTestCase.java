c30b9b5175a30a4aee9c545ff567a6465c9ee28f
fazlan <fazlan@a5903396-d722-0410-b921-86c7d4935375>
2012-06-06 10:08:49 +0000

    Fixing REGISTRY-876
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@129347 a5903396-d722-0410-b921-86c7d4935375

--- products/greg/modules/integration/registry/tests/src/test/java/org/wso2/carbon/registry/properties/test/PropertiesAdminServiceTestCase.java
+++ products/greg/modules/integration/registry/tests/src/test/java/org/wso2/carbon/registry/properties/test/PropertiesAdminServiceTestCase.java
@@ -21,6 +21,7 @@ package org.wso2.carbon.registry.properties.test;
 import org.apache.axis2.AxisFault;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import org.wso2.carbon.integration.framework.LoginLogoutUtil;
@@ -28,11 +29,11 @@ import org.wso2.carbon.integration.framework.utils.FrameworkSettings;
 import org.wso2.carbon.registry.metadata.test.util.TestUtils;
 import org.wso2.carbon.registry.properties.stub.PropertiesAdminServiceRegistryExceptionException;
 import org.wso2.carbon.registry.properties.stub.PropertiesAdminServiceStub;
+import org.wso2.carbon.registry.properties.stub.beans.xsd.PropertiesBean;
+import org.wso2.carbon.registry.properties.stub.utils.xsd.Property;
 
 import java.rmi.RemoteException;
-
-import static org.testng.Assert.assertTrue;
-import static org.testng.Assert.fail;
+import java.util.Properties;
 
 /**
  *Test class to test the <code>PropertiesAdminService</code>
@@ -53,26 +54,54 @@ public class PropertiesAdminServiceTestCase {
         frameworkPath = FrameworkSettings.getFrameworkPath();
         log.debug("Running SuccessCase");
         propertiesAdminServiceStub = TestUtils.getPropertiesAdminServiceStub(loggedInSessionCookie);
-
     }
 
     @Test(groups = {"wso2.greg.prop.admin.service"}, expectedExceptions = AxisFault.class,
             expectedExceptionsMessageRegExp = "Property cannot start with the \"registry.\" prefix.*")
-    public void addHiddenPropertiesToResource() throws PropertiesAdminServiceRegistryExceptionException, RemoteException {
+    public void addHiddenPropertiesToResourceThrowException() throws PropertiesAdminServiceRegistryExceptionException, RemoteException {
 
-
-        propertiesAdminServiceStub.setProperty("/_system", "registry.somehidded.property_name",
+        propertiesAdminServiceStub.setProperty("/", "registry.somehidden.property_name",
                          "somehidden.property_value");
-
     }
 
     @Test(groups = {"wso2.greg.prop.admin.service"}, expectedExceptions = AxisFault.class,
             expectedExceptionsMessageRegExp = "Property cannot start with the \"registry.\" prefix.*")
-    public void updateHiddenPropertiesToResource() throws PropertiesAdminServiceRegistryExceptionException, RemoteException {
+    public void updateHiddenPropertiesToResourceThrowException() throws PropertiesAdminServiceRegistryExceptionException, RemoteException {
+
+        propertiesAdminServiceStub.updateProperty("/", "registry.somehidden.property_name",
+                "somehidden.property_value", "some.old.prop.name");
+    }
+
+    @Test(groups = {"wso2.greg.prop.admin.service"}, expectedExceptions = AxisFault.class,
+            expectedExceptionsMessageRegExp = "Cannot duplicate property name.*")
+    public void addDuplicatePropertiesToResourceThrowException() throws PropertiesAdminServiceRegistryExceptionException, RemoteException {
+
+        propertiesAdminServiceStub.setProperty("/", "add_property_name", "some.property_value");
+        //trying to add an existing property name,thus, this should fail.
+        propertiesAdminServiceStub.setProperty("/", "add_property_name", "some.property_value1");
+    }
 
 
-        propertiesAdminServiceStub.updateProperty("/_system", "registry.somehidded.property_name",
-                         "somehidden.property_value", "some.old.prop.name");
+    @Test(groups = {"wso2.greg.prop.admin.service"}, expectedExceptions = AxisFault.class,
+            expectedExceptionsMessageRegExp = "Cannot duplicate property name.*")
+    public void updateDuplicatePropertiesToResourceThrowException() throws PropertiesAdminServiceRegistryExceptionException, RemoteException {
 
+        propertiesAdminServiceStub.setProperty("/", "update_property_name_1", "some.property_value1");
+
+        propertiesAdminServiceStub.setProperty("/", "update_property_name_2", "some.property_value2");
+        //trying to rename existing property(update_property_name_1) to another existing property(update_property_name_2)
+        // and thus, this should fail.
+        propertiesAdminServiceStub.updateProperty("/", "update_property_name_2",
+                         "some.property_value", "update_property_name_3");
+    }
+
+    @AfterMethod
+    public void removeProperties() throws PropertiesAdminServiceRegistryExceptionException, RemoteException {
+        System.out.println("x");
+        PropertiesBean propertiesBean = propertiesAdminServiceStub.getProperties("/", "yes");
+        if(propertiesBean.getProperties() != null)
+           for (Property prop: propertiesBean.getProperties()) {
+              propertiesAdminServiceStub.removeProperty("/", prop.getKey());
+           }
     }
 }

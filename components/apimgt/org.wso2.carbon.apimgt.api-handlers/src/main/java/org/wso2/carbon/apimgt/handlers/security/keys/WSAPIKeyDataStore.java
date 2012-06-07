4671891bde5f2cf6b0206db983893730071824da
hiranya <hiranya@a5903396-d722-0410-b921-86c7d4935375>
2012-06-07 07:57:44 +0000

    Handling session timeouts when validating api keys
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@129440 a5903396-d722-0410-b921-86c7d4935375

--- components/apimgt/org.wso2.carbon.apimgt.api-handlers/src/main/java/org/wso2/carbon/apimgt/handlers/security/keys/WSAPIKeyDataStore.java
+++ components/apimgt/org.wso2.carbon.apimgt.api-handlers/src/main/java/org/wso2/carbon/apimgt/handlers/security/keys/WSAPIKeyDataStore.java
@@ -16,21 +16,12 @@
 
 package org.wso2.carbon.apimgt.handlers.security.keys;
 
-import org.apache.axis2.client.Options;
-import org.apache.axis2.client.ServiceClient;
-import org.apache.axis2.context.ServiceContext;
-import org.apache.axis2.transport.http.HTTPConstants;
 import org.apache.commons.pool.BasePoolableObjectFactory;
 import org.apache.commons.pool.ObjectPool;
 import org.apache.commons.pool.impl.StackObjectPool;
-import org.wso2.carbon.apimgt.handlers.internal.ServiceReferenceHolder;
 import org.wso2.carbon.apimgt.handlers.security.APISecurityConstants;
 import org.wso2.carbon.apimgt.handlers.security.APISecurityException;
-import org.wso2.carbon.apimgt.impl.APIConstants;
-import org.wso2.carbon.apimgt.impl.APIManagerConfiguration;
 import org.wso2.carbon.apimgt.impl.dto.APIKeyValidationInfoDTO;
-import org.wso2.carbon.apimgt.keymgt.stub.validator.APIKeyValidationServiceStub;
-import org.wso2.carbon.utils.CarbonUtils;
 
 /**
  * Provides a web service interface for the API key data store. This implementation
@@ -41,81 +32,31 @@ import org.wso2.carbon.utils.CarbonUtils;
  */
 public class WSAPIKeyDataStore implements APIKeyDataStore {
 
-    private static final int TIMEOUT_IN_MILLIS = 15 * 60 * 1000;
-
-    private volatile String cookie;
     private ObjectPool clientPool;
 
-    private String username;
-    private String password;
-
     public WSAPIKeyDataStore() throws APISecurityException {
-        final APIManagerConfiguration config = ServiceReferenceHolder.getInstance().
-                getAPIManagerConfiguration();
-        username = config.getFirstProperty(APIConstants.API_KEY_MANAGER_USERNAME);
-        password = config.getFirstProperty(APIConstants.API_KEY_MANAGER_PASSWORD);
-        String serviceURL = config.getFirstProperty(APIConstants.API_KEY_MANAGER_URL);
-        // Just check for the service URL parameter at this point - Don't hold on to it.
-        // Chances are the listener manager is not properly initialized yet and therefore
-        // we should defer reading the service URL for later.
-        if (serviceURL == null || username == null || password == null) {
-            throw new APISecurityException(APISecurityConstants.API_AUTH_GENERAL_ERROR,
-                    "Required connection details for the key management server not provided");
-        }
-
         clientPool = new StackObjectPool(new BasePoolableObjectFactory() {
             @Override
             public Object makeObject() throws Exception {
-                String serviceURL = config.getFirstProperty(APIConstants.API_KEY_MANAGER_URL);
-                APIKeyValidationServiceStub clientStub = new APIKeyValidationServiceStub(null,
-                        serviceURL + "APIKeyValidationService");
-
-                ServiceClient client = clientStub._getServiceClient();
-                Options options = client.getOptions();
-                options.setTimeOutInMilliSeconds(TIMEOUT_IN_MILLIS);
-                options.setProperty(HTTPConstants.SO_TIMEOUT, TIMEOUT_IN_MILLIS);
-                options.setProperty(HTTPConstants.CONNECTION_TIMEOUT, TIMEOUT_IN_MILLIS);
-                options.setCallTransportCleanup(true);
-                options.setManageSession(true);
-                return clientStub;
+                return new APIKeyValidatorClient();
             }
         });
     }
 
     public APIKeyValidationInfoDTO getAPIKeyData(String context, String apiVersion,
                                                  String apiKey) throws APISecurityException {
-        APIKeyValidationServiceStub stub = null;
+        APIKeyValidatorClient client = null;
         try {
-            stub = (APIKeyValidationServiceStub) clientPool.borrowObject();
-            // Add the Authorization header to all requests
-            // If the cookie we send is invalid, this will renew the cookie automatically
-            CarbonUtils.setBasicAccessSecurityHeaders(username, password, true, stub._getServiceClient());
-
-            if (cookie == null) {
-                synchronized (this) {
-                    if (cookie == null) {
-                        // We are using this validator for the first time
-                        // Need to invoke the service and obtain the cookie
-                        org.wso2.carbon.apimgt.impl.dto.xsd.APIKeyValidationInfoDTO dto =
-                                stub.validateKey(context, apiVersion, apiKey);
-                        ServiceContext serviceContext = stub.
-                                _getServiceClient().getLastOperationContext().getServiceContext();
-                        cookie = (String) serviceContext.getProperty(HTTPConstants.COOKIE_STRING);
-                        return toDTO(dto);
-                    }
-                }
-            }
-
-            stub._getServiceClient().getOptions().setProperty(HTTPConstants.COOKIE_STRING, cookie);
-            return toDTO(stub.validateKey(context, apiVersion, apiKey));
+            client = (APIKeyValidatorClient) clientPool.borrowObject();
+            return client.getAPIKeyData(context, apiVersion, apiKey);
 
         } catch (Exception e) {
             throw new APISecurityException(APISecurityConstants.API_AUTH_GENERAL_ERROR,
                     "Error while accessing backend services for API key validation", e);
         } finally {
             try {
-                if (stub != null) {
-                    clientPool.returnObject(stub);
+                if (client != null) {
+                    clientPool.returnObject(client);
                 }
             } catch (Exception ignored) {
             }

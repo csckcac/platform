46164d00aa11b79c82cb2bbf4ddb28a44744ba53
ruchira <ruchira@a5903396-d722-0410-b921-86c7d4935375>
2012-06-13 12:28:55 +0000

    Fixing JAGGERY-204
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@129941 a5903396-d722-0410-b921-86c7d4935375

--- components/script-engine/org.wso2.carbon.scriptengine/src/main/java/org/wso2/carbon/scriptengine/util/HostObjectUtil.java
+++ components/script-engine/org.wso2.carbon.scriptengine/src/main/java/org/wso2/carbon/scriptengine/util/HostObjectUtil.java
@@ -1,27 +1,46 @@
 package org.wso2.carbon.scriptengine.util;
 
 
-import org.apache.commons.httpclient.*;
+import org.apache.commons.httpclient.Credentials;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpMethod;
+import org.apache.commons.httpclient.MultiThreadedHttpConnectionManager;
+import org.apache.commons.httpclient.UsernamePasswordCredentials;
 import org.apache.commons.httpclient.auth.AuthScope;
 import org.apache.commons.httpclient.cookie.CookiePolicy;
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.mozilla.javascript.*;
-import org.wso2.carbon.scriptengine.engine.RhinoEngine;
+import org.mozilla.javascript.NativeArray;
+import org.mozilla.javascript.NativeObject;
+import org.mozilla.javascript.ScriptableObject;
+import org.mozilla.javascript.Wrapper;
 import org.wso2.carbon.scriptengine.exceptions.ScriptException;
 import org.wso2.javascript.xmlimpl.XML;
 import org.wso2.javascript.xmlimpl.XMLList;
 
-import java.io.*;
-import java.lang.reflect.InvocationTargetException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+import java.io.StringWriter;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class HostObjectUtil {
 
     private static final Log log = LogFactory.getLog(HostObjectUtil.class);
+
+    private static final String FORMAT_DATE_ISO="yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
+    private static final DateFormat dateFormat = new SimpleDateFormat(FORMAT_DATE_ISO);
+
+    static {
+        dateFormat.setTimeZone(TimeZone.getDefault());
+    }
 
     public static void invalidArgsError(String hostObject, String function, String index, String required, Object provided,
                                         boolean constructor) throws ScriptException {
@@ -60,65 +79,32 @@ public class HostObjectUtil {
         }
 
         if (obj instanceof NativeObject) {
-            json.append("{");
-            NativeObject o = (NativeObject) obj;
-            Object[] ids = o.getIds();
-            boolean first = true;
-            for (Object id : ids) {
-                String key = (String) id;
-                Object value = o.get((String) id, o);
-                if (!first) {
-                    json.append(", ");
-                } else {
-                    first = false;
-                }
-                json.append("\"").append(key).append("\" : ").append(serializeJSON(value));
-            }
-            json.append("}");
+            serializeNativeObject((NativeObject) obj, json);
         } else if (obj instanceof NativeArray) {
-            json.append("[");
-            NativeArray o = (NativeArray) obj;
-            Object[] ids = o.getIds();
-            boolean first = true;
-            for (Object id : ids) {
-                Object value = o.get((Integer) id, o);
-                if (!first) {
-                    json.append(", ");
-                } else {
-                    first = false;
-                }
-                json.append(serializeJSON(value));
-            }
-            json.append("]");
+            serializeNativeArray((NativeArray) obj, json);
         } else if (obj instanceof Object[]) {
-            json.append("[");
-            boolean first = true;
-            for (Object value : (Object[]) obj) {
-                if (!first) {
-                    json.append(", ");
-                } else {
-                    first = false;
-                }
-                json.append(serializeJSON(value));
-            }
-            json.append("]");
+            serializeObjectArray((Object[]) obj, json);
         } else if (obj instanceof XML || obj instanceof XMLList) {
-            String xml = (String) ScriptableObject.callMethod((ScriptableObject) obj, "toXMLString", new Object[0]);
-            xml = xml.replaceAll("\\n", "\\\\n").replaceAll("\"", "\\\\\"");
-            json.append("\"").append(xml).append("\"");
+            serializeXML((ScriptableObject) obj, json);
         } else if (obj instanceof Integer ||
                 obj instanceof Long ||
                 obj instanceof Float ||
                 obj instanceof Double ||
                 obj instanceof Short ||
                 obj instanceof BigInteger ||
-                obj instanceof BigDecimal ||
-                obj instanceof Boolean) {
-            json.append(obj.toString());
+                obj instanceof BigDecimal) {
+            serializeNumber(obj, json);
+        } else if(obj instanceof Boolean) {
+            serializeBoolean((Boolean) obj, json);
         } else if (obj instanceof String) {
-            json.append("\"").append(obj.toString().replaceAll("\\n", "\\\\n").replaceAll("\"", "\\\\\"")).append("\"");
+            serializeString((String) obj, json);
         } else {
-            json.append("{}");
+            String className = obj.getClass().getName();
+            if(className.equals("org.mozilla.javascript.NativeDate")) {
+                serializeNativeDate((ScriptableObject) obj, json);
+            } else {
+                json.append("{}");
+            }
         }
         return json.toString();
     }
@@ -214,5 +200,76 @@ public class HostObjectUtil {
                     .setCredentials(new AuthScope(url.getHost(), port), creds);
         }
         return httpClient.executeMethod(method);
+    }
+
+    private static void serializeObjectArray(Object[] obj, StringWriter json) {
+        json.append("[");
+        boolean first = true;
+        for (Object value : (Object[]) obj) {
+            if (!first) {
+                json.append(", ");
+            } else {
+                first = false;
+            }
+            json.append(serializeJSON(value));
+        }
+        json.append("]");
+    }
+
+    private static void serializeNumber(Object obj, StringWriter json) {
+        json.append(obj.toString());
+    }
+
+    private static void serializeNativeDate(ScriptableObject obj, StringWriter json) {
+        Double time = (Double) ScriptableObject.callMethod(obj, "getTime", new Object[0]);
+        Date date = new Date(Math.round(time));
+        json.append(dateFormat.format(date));
+    }
+
+    private static void serializeBoolean(Boolean obj, StringWriter json) {
+        json.append(Boolean.toString(obj));
+    }
+
+    private static void serializeString(String obj, StringWriter json) {
+        json.append("\"").append(obj.replaceAll("\\n", "\\\\n").replaceAll("\"", "\\\\\"")).append("\"");
+    }
+
+    private static void serializeXML(ScriptableObject obj, StringWriter json) {
+        String xml = (String) ScriptableObject.callMethod(obj, "toXMLString", new Object[0]);
+        xml = xml.replaceAll("\\n", "\\\\n").replaceAll("\"", "\\\\\"");
+        json.append("\"").append(xml).append("\"");
+    }
+
+    private static void serializeNativeArray(NativeArray obj, StringWriter json) {
+        json.append("[");
+        Object[] ids = obj.getIds();
+        boolean first = true;
+        for (Object id : ids) {
+            Object value = obj.get((Integer) id, obj);
+            if (!first) {
+                json.append(", ");
+            } else {
+                first = false;
+            }
+            json.append(serializeJSON(value));
+        }
+        json.append("]");
+    }
+
+    private static void serializeNativeObject(NativeObject obj, StringWriter json) {
+        json.append("{");
+        Object[] ids = obj.getIds();
+        boolean first = true;
+        for (Object id : ids) {
+            String key = (String) id;
+            Object value = obj.get((String) id, obj);
+            if (!first) {
+                json.append(", ");
+            } else {
+                first = false;
+            }
+            json.append("\"").append(key).append("\" : ").append(serializeJSON(value));
+        }
+        json.append("}");
     }
 }

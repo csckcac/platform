9bd2e81e3397a335d9071e9d748424f9c5c6a071
rajika <rajika@a5903396-d722-0410-b921-86c7d4935375>
2012-06-13 09:47:19 +0000

    Fixed the role name.
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@129908 a5903396-d722-0410-b921-86c7d4935375

--- components/csg/org.wso2.carbon.cloud.csg.common/src/main/java/org/wso2/carbon/cloud/csg/common/CSGConstant.java
+++ components/csg/org.wso2.carbon.cloud.csg.common/src/main/java/org/wso2/carbon/cloud/csg/common/CSGConstant.java
@@ -294,7 +294,7 @@ public class CSGConstant {
 
     public static final String CSG_ROLE_NAME = "csg-role-name";
 
-    public static final String DEFAULT_CSG_ROLE_NAME = CSG_ROLE_NAME;
+    public static final String DEFAULT_CSG_ROLE_NAME = "csg";
 
     public static final String CSG_PUBLISH_ROLE_NAME = "publish";
 

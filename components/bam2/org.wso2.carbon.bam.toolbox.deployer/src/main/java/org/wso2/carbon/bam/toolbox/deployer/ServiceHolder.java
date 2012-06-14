3be4de639db72067aaddd685a2ff786086b8940b
sinthuja <sinthuja@a5903396-d722-0410-b921-86c7d4935375>
2012-06-14 07:17:57 +0000

    removing commented lines od code
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@129995 a5903396-d722-0410-b921-86c7d4935375

--- components/bam2/org.wso2.carbon.bam.toolbox.deployer/src/main/java/org/wso2/carbon/bam/toolbox/deployer/ServiceHolder.java
+++ components/bam2/org.wso2.carbon.bam.toolbox.deployer/src/main/java/org/wso2/carbon/bam/toolbox/deployer/ServiceHolder.java
@@ -41,7 +41,6 @@ public class ServiceHolder {
 
     public static void setConfigurationContextService(ConfigurationContextService configurationContextService) {
         ServiceHolder.configurationContextService = configurationContextService;
-//        triggerInitialDeployments();
     }
 
     public static Registry getRegistry(int tenantId) throws RegistryException {
@@ -50,12 +49,10 @@ public class ServiceHolder {
 
     public static void setRegistryService(RegistryService registryService) {
         ServiceHolder.registryService = registryService;
-//         triggerInitialDeployments();
     }
 
     public static void setUserRealm(UserRealm realmService) {
         ServiceHolder.userRealm = realmService;
-//         triggerInitialDeployments();
     }
 
     public static UserRealm getUserRealm() {
@@ -68,7 +65,6 @@ public class ServiceHolder {
 
     public static void setRealmService(RealmService realmService) {
         ServiceHolder.realmService = realmService;
-        // triggerInitialDeployments();
     }
 
     public static DashboardDSService getDashboardService() {
@@ -77,29 +73,7 @@ public class ServiceHolder {
 
     public static void setDashboardService(DashboardDSService dashboardService) {
         ServiceHolder.dashboardService = dashboardService;
-         //triggerInitialDeployments();
     }
-
-//    private static boolean isLoadedAllServices() {
-//        if (
-//                null != registryService &&
-//                        null != configurationContextService &&
-//                        null != dashboardService &&  //TODO - there is a possiblity dashboard service is not registered
-//                        null != realmService &&
-//                        null != userRealm) {
-//            return true;
-//        } else {
-//            return false;
-//        }
-//    }
-//
-//    private static void triggerInitialDeployments() {
-//        if (isLoadedAllServices()) {
-//            for (BAMToolBoxDeployer deployerInstance : BAMToolBoxDeployer.pausedDeployments) {
-//                deployerInstance.doInitialUnDeployments();
-//            }
-//        }
-//    }
 }
 
 

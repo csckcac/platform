4ec708d5ddf5c6dce54a4cf5bbc41359fa2574ee
suho <suho@a5903396-d722-0410-b921-86c7d4935375>
2012-06-15 06:16:10 +0000

    use different ports
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@130166 a5903396-d722-0410-b921-86c7d4935375

--- components/agent/org.wso2.carbon.agent.server/src/test/java/org/wso2/carbon/agent/server/EventTest.java
+++ components/agent/org.wso2.carbon.agent.server/src/test/java/org/wso2/carbon/agent/server/EventTest.java
@@ -79,7 +79,9 @@ public class EventTest extends TestCase {
         Thread.sleep(3000);
         dataPublisher.stop();
         testServer.stop();
-    }    public void testSendingEventSendingNull()
+    }
+
+    public void testSendingEventSendingNull()
             throws MalformedURLException, AuthenticationException, TransportException,
                    AgentException, UndefinedEventTypeException,
                    DifferentStreamDefinitionAlreadyDefinedException,
@@ -88,12 +90,12 @@ public class EventTest extends TestCase {
                    StreamDefinitionException {
 
         TestServer testServer = new TestServer();
-        testServer.start(7625);
+        testServer.start(7626);
         KeyStoreUtil.setTrustStoreParams();
         Thread.sleep(2000);
 
         //according to the convention the authentication port will be 7611+100= 7711 and its host will be the same
-        DataPublisher dataPublisher = new DataPublisher("tcp://localhost:7625", "admin", "admin");
+        DataPublisher dataPublisher = new DataPublisher("tcp://localhost:7626", "admin", "admin");
         String id1 = dataPublisher.defineEventStream("{" +
                                                      "  'name':'org.wso2.esb.MediatorStatistics'," +
                                                      "  'version':'2.3.0'," +
@@ -179,7 +181,7 @@ public class EventTest extends TestCase {
         Thread.sleep(2000);
 
         //according to the convention the authentication port will be 7611+100= 7711 and its host will be the same
-        DataPublisher dataPublisher = new DataPublisher("ssl://localhost:7713","tcp://localhost:7613", "admin", "admin");
+        DataPublisher dataPublisher = new DataPublisher("ssl://localhost:7713", "tcp://localhost:7613", "admin", "admin");
         String streamId = dataPublisher.defineEventStream("{" +
                                                           "  'name':'org.wso2.esb.MediatorStatistics'," +
                                                           "  'version':'2.3.0'," +
@@ -278,24 +280,24 @@ public class EventTest extends TestCase {
         //according to the convention the authentication port will be 7611+100= 7711 and its host will be the same
         DataPublisher dataPublisher = new DataPublisher("tcp://localhost:7619", "admin", "admin");
         dataPublisher.defineEventStream("{" +
-                                                          "  'name':'org.wso2.esb.MediatorStatistics'," +
-                                                          "  'version':'1.3.0'," +
-                                                          "  'nickName': 'Stock Quote Information'," +
-                                                          "  'description': 'Some Desc'," +
-                                                          "  'metaData':[" +
-                                                          "          {'name':'ipAdd','type':'STRING'}" +
-                                                          "  ]," +
-                                                          "  'payloadData':[" +
-                                                          "          {'name':'symbol','type':'STRING'}," +
-                                                          "          {'name':'price','type':'DOUBLE'}," +
-                                                          "          {'name':'volume','type':'INT'}," +
-                                                          "          {'name':'max','type':'DOUBLE'}," +
-                                                          "          {'name':'min','type':'Double'}" +
-                                                          "  ]" +
-                                                          "}");
+                                        "  'name':'org.wso2.esb.MediatorStatistics'," +
+                                        "  'version':'1.3.0'," +
+                                        "  'nickName': 'Stock Quote Information'," +
+                                        "  'description': 'Some Desc'," +
+                                        "  'metaData':[" +
+                                        "          {'name':'ipAdd','type':'STRING'}" +
+                                        "  ]," +
+                                        "  'payloadData':[" +
+                                        "          {'name':'symbol','type':'STRING'}," +
+                                        "          {'name':'price','type':'DOUBLE'}," +
+                                        "          {'name':'volume','type':'INT'}," +
+                                        "          {'name':'max','type':'DOUBLE'}," +
+                                        "          {'name':'min','type':'Double'}" +
+                                        "  ]" +
+                                        "}");
         String receivedStreamId = dataPublisher.findEventStream("org.wso2.esb.MediatorStatistics", "1.3.0");
         //In this case correlation data is null
-        dataPublisher.publish(receivedStreamId, new Object[]{"127.0.0.1"}, null, new Object[]{"IBM", 96.8, 300, 120.6,20.6});
+        dataPublisher.publish(receivedStreamId, new Object[]{"127.0.0.1"}, null, new Object[]{"IBM", 96.8, 300, 120.6, 20.6});
         dataPublisher.publish(receivedStreamId, System.currentTimeMillis(), new Object[]{"127.0.0.2"}, null, new Object[]{"WSO2", 100.8, 200, 110.4, 74.7});
 //        else the user can publish event it self
         dataPublisher.publish(new Event(receivedStreamId, System.currentTimeMillis(), new Object[]{"127.0.0.3"}, null, new Object[]{"WSO2", 100.8, 200, 110.4, 74.7}));
@@ -349,6 +351,7 @@ public class EventTest extends TestCase {
         testServer.stop();
 
     }
+
     public void testSendingSecureEventsByDefiningAllUrls()
             throws MalformedURLException, AuthenticationException, TransportException,
                    AgentException, UndefinedEventTypeException,
@@ -363,7 +366,7 @@ public class EventTest extends TestCase {
 
         Thread.sleep(2000);
         //according to the convention the authentication port will be 7612+100= 7711 and its host will be the same
-        DataPublisher dataPublisher = new DataPublisher("ssl://localhost:7721","ssl://localhost:7721", "admin", "admin");
+        DataPublisher dataPublisher = new DataPublisher("ssl://localhost:7721", "ssl://localhost:7721", "admin", "admin");
         String streamId = dataPublisher.defineEventStream("{" +
                                                           "  'name':'org.wso2.esb.MediatorStatistics'," +
                                                           "  'version':'1.3.0'," +

0bcf43de5954f85eaa5b6765050c62fbb05d6ad0
madhuka <madhuka@a5903396-d722-0410-b921-86c7d4935375>
2012-06-15 09:38:43 +0000

    REST jaggery sample (Coffee shop) BE is Complete with plain HTML Client
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@130194 a5903396-d722-0410-b921-86c7d4935375

--- products/jaggery/modules/apps/coffeeshop/client/js/coffeeshopclient.js
+++ products/jaggery/modules/apps/coffeeshop/client/js/coffeeshopclient.js
@@ -2,51 +2,47 @@ CoffeeShopClient = new function() {
 	
 this.addOrder = function() {
 var orderName = $('#orderName').val();
-CoffeShopAppUtil.makeRequest("POST","/coffeeshop/order.jag?", "order="+orderName, function(html) {	});
+CoffeShopAppUtil.makeRequest("POST","/coffeeshop/orders/order.jag?", "order="+orderName, function(html) {	});
 console.log("Call add Order"+orderName);
 }
 
 this.viewOrder = function() {
 var orderid = $('#orderid').val();
-//var content = '{"id":'+orderid+'}';
-CoffeShopAppUtil.makeRequest("GET","/coffeeshop/order.jag?", "orderid="+orderid , function(html) {	});
+CoffeShopAppUtil.makeRequest("GET","/coffeeshop/orders/"+orderid+"", null , function(html) {	});
 console.log("view order "+orderid);
 }
 
 this.addAddittion = function() {
 var addition = $('#addition').val();
 var orderid = $('#orderid').val();
-var content = '{"id":'+orderid+',"addition":'+addition+'}';
-CoffeShopAppUtil.makeRequest("PUT","/coffeeshop/order.jag?", content , function(html) {	});
+var content = '{"addition":'+addition+'}';
+CoffeShopAppUtil.makeRequest("PUT","/coffeeshop/orders/"+orderid, content , function(html) {	});
 console.log("Call add additon "+ addition + " on "+orderid);
 }
 
 this.updateStatus = function() {
 var status = $('#status').val();
 var orderid = $('#orderid').val();
-var content = '{"id":'+orderid+',"status":'+status+'}';
-CoffeShopAppUtil.makeRequest("PUT","/coffeeshop/order.jag?", content , function(html) {	});
+var content = '{"status":'+status+'}';
+CoffeShopAppUtil.makeRequest("PUT","/coffeeshop/orders/"+orderid, content , function(html) {	});
 console.log("Call add status "+ status + " on "+orderid);
 }
 
 this.isPaidOrder = function() {
 var orderid = $('#orderid').val();
-//var content = '{"id":'+orderid+'}';
-CoffeShopAppUtil.makeRequest("GET","/coffeeshop/payment.jag?", "orderid="+orderid , function(html) {	});
+CoffeShopAppUtil.makeRequest("GET","/coffeeshop/payments/"+orderid, null , function(html) {	});
 console.log("Check order is paid "+orderid);
 }
 
 this.payOrder = function() {
 var orderid = $('#orderid').val();
-var content = '{"id":'+orderid+'}';
-CoffeShopAppUtil.makeRequest("PUT","/coffeeshop/payment.jag?", content , function(html) {	});
+CoffeShopAppUtil.makeRequest("PUT","/coffeeshop/payments/"+orderid, null , function(html) {	});
 console.log("Call for pay order "+orderid);
 }
 
 this.deletOrder = function() {
 var orderid = $('#orderid').val();
-var content = '{"id":'+orderid+'}';
-CoffeShopAppUtil.makeRequest("DELETE","/coffeeshop/order.jag?", content , function(html) {	});
+CoffeShopAppUtil.makeRequest("DELETE","/coffeeshop/orders/"+orderid+"/", null , function(html) {	});
 console.log("Call for delet order "+orderid);
 }
 

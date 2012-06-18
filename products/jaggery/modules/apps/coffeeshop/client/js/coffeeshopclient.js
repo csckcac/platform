4710548651fb3e6e7e36fb363ab563a6c1e6191c
madhuka <madhuka@a5903396-d722-0410-b921-86c7d4935375>
2012-06-18 18:29:50 +0000

    Connection with REST for loading orders, adding orders, adding additions, changing status
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@130409 a5903396-d722-0410-b921-86c7d4935375

--- products/jaggery/modules/apps/coffeeshop/client/js/coffeeshopclient.js
+++ products/jaggery/modules/apps/coffeeshop/client/js/coffeeshopclient.js
@@ -1,7 +1,6 @@
 CoffeeShopClient = new function() {
 	
 this.addOrder = function(orderName) {
-var orderName = $('#orderName').val();
 CoffeShopAppUtil.makeRequest("POST","/coffeeshop/orders/order.jag?", "order="+orderName, function(html) {	});
 console.log("Call add Order "+orderName);
 }
@@ -12,17 +11,21 @@ CoffeShopAppUtil.makeRequest("GET","/coffeeshop/orders/"+orderid+"", null , func
 console.log("view order "+orderid);
 }
 
-this.addAddittion = function() {
-var addition = $('#addition').val();
-var orderid = $('#orderid').val();
+this.viewOrders = function() {
+CoffeShopAppUtil.makeRequest("GET","/coffeeshop/orders/", null , function(html) {	});
+}
+
+this.addAddittion = function(orderid,addition) {
+//var addition = $('#addition').val();
+//var orderid = $('#orderid').val();
 var content = '{"addition":'+addition+'}';
 CoffeShopAppUtil.makeRequest("PUT","/coffeeshop/orders/"+orderid, content , function(html) {	});
 console.log("Call add additon "+ addition + " on "+orderid);
 }
 
-this.updateStatus = function() {
-var status = $('#status').val();
-var orderid = $('#orderid').val();
+this.updateStatus = function(orderid,status) {
+//var status = $('#status').val();
+//var orderid = $('#orderid').val();
 var content = '{"status":'+status+'}';
 CoffeShopAppUtil.makeRequest("PUT","/coffeeshop/orders/"+orderid, content , function(html) {	});
 console.log("Call add status "+ status + " on "+orderid);
@@ -34,8 +37,8 @@ CoffeShopAppUtil.makeRequest("GET","/coffeeshop/payments/"+orderid, null , funct
 console.log("Check order is paid "+orderid);
 }
 
-this.payOrder = function() {
-var orderid = $('#orderid').val();
+this.payOrder = function(orderid) {
+//var orderid = $('#orderid').val();
 CoffeShopAppUtil.makeRequest("PUT","/coffeeshop/payments/"+orderid, null , function(html) {	});
 console.log("Call for pay order "+orderid);
 }

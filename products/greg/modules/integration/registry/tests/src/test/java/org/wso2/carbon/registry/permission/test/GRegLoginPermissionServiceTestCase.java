b535b17e5afd2a0225774a559a56f315f2bb526d
krishantha <krishantha@a5903396-d722-0410-b921-86c7d4935375>
2012-06-19 10:02:59 +0000

    Adding G-Reg Permission tests
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@130495 a5903396-d722-0410-b921-86c7d4935375

---
+++ products/greg/modules/integration/registry/tests/src/test/java/org/wso2/carbon/registry/permission/test/GRegLoginPermissionServiceTestCase.java
@@ -0,0 +1,126 @@
+/*
+* Copyright (c) 2010, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+
+package org.wso2.carbon.registry.permission.test;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import org.wso2.carbon.admin.service.AdminServiceAuthentication;
+import org.wso2.carbon.admin.service.AdminServiceResourceAdmin;
+import org.wso2.carbon.admin.service.AdminServiceUserMgtService;
+import org.wso2.carbon.authenticator.stub.LoginAuthenticationExceptionException;
+import org.wso2.carbon.authenticator.stub.LogoutAuthenticationExceptionException;
+import org.wso2.carbon.integration.framework.ClientConnectionUtil;
+import org.wso2.carbon.integration.framework.LoginLogoutUtil;
+import org.wso2.carbon.integration.framework.utils.FrameworkSettings;
+import org.wso2.carbon.registry.resource.stub.ResourceAdminServiceExceptionException;
+import org.wso2.carbon.user.mgt.common.UserAdminException;
+
+import java.rmi.RemoteException;
+
+import static org.testng.Assert.assertTrue;
+
+
+public class GRegLoginPermissionServiceTestCase{
+
+    private static final Log log = LogFactory.getLog(GRegLoginPermissionServiceTestCase.class);
+    private AdminServiceUserMgtService userAdminStub;
+    private static AdminServiceAuthentication userAuthenticationStub;
+    private static AdminServiceResourceAdmin admin_service_resource_admin;
+    private String sessionCookie;
+    private String roleName;
+    private String userName;
+    private String userPassword;
+    private LoginLogoutUtil util = new LoginLogoutUtil();
+
+    @BeforeClass(alwaysRun = true)
+    public void init() throws Exception {
+        ClientConnectionUtil.waitForPort(Integer.parseInt(FrameworkSettings.HTTP_PORT));
+        sessionCookie = util.login();
+        String SERVER_URL = "https://" + FrameworkSettings.HOST_NAME +
+                            ":" + FrameworkSettings.HTTPS_PORT + "/services/";
+
+        userAdminStub = new AdminServiceUserMgtService(SERVER_URL);
+        userAuthenticationStub = new AdminServiceAuthentication(SERVER_URL);
+        admin_service_resource_admin = new AdminServiceResourceAdmin(SERVER_URL);
+        roleName = "login_role";
+        userName = "greg_login_user";
+        userPassword = "welcome";
+
+        if (userAdminStub.roleNameExists(roleName, sessionCookie)) {  //delete the role if exists
+            userAdminStub.deleteRole(sessionCookie, roleName);
+        }
+
+        if (userAdminStub.userNameExists(roleName, sessionCookie, userName)) { //delete user if exists
+            userAdminStub.deleteUser(sessionCookie, userName);
+        }
+    }
+
+
+    @Test(groups = {"wso2.greg"}, description = "test add a role with login permission",
+          priority = 1)
+    public void testAddLoginPermissionUser()
+            throws UserAdminException, RemoteException, ResourceAdminServiceExceptionException,
+                   LoginAuthenticationExceptionException, LogoutAuthenticationExceptionException {
+
+
+        String permission[] = {"/permission/admin/login"};
+        String userList[] = {userName};
+        String sessionCookieUser = null;
+        boolean status;
+
+        addRoleWithUser(permission, userList);
+        sessionCookieUser = userAuthenticationStub.login(userName, userPassword, FrameworkSettings.HOST_NAME);
+        log.info("Newly Created User Logged in :" + userName);
+
+
+        try {
+            status = false;
+            // greg_login_user does not have permission to add a Text resource
+            admin_service_resource_admin.addTextResource(sessionCookieUser, "/", "login.txt",
+                                                         "", "", "");
+        } catch (RemoteException e) {
+            status = true;
+            assertTrue(e.getMessage().indexOf("Access Denied.") > 0, "Access Denied Remote" +
+                                                                     " Exception assertion Failed :");
+            log.info("greg_login_user does not have permission to add a text resource :");
+        }
+        assertTrue(status, "Only Login user permission has uploaded a text resource ? :");
+        userAuthenticationStub.logOut();
+
+        log.info("*************Login Permission Only Test Scenario-Passed ******************");
+    }
+
+    private void addRoleWithUser(String[] permission, String[] userList) throws UserAdminException {
+        userAdminStub.addRole(roleName, null, permission, sessionCookie);
+        log.info("Successfully added Role :" + roleName);
+        String roles[] = {roleName};
+        userAdminStub.addUser(sessionCookie, userName, userPassword, roles, null);
+        log.info("Successfully User Crated :" + userName);
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void deleteRoleAndUsers() {
+        userAdminStub.deleteRole(sessionCookie, roleName);
+        log.info("Role " + roleName + " deleted successfully");
+        userAdminStub.deleteUser(sessionCookie, userName);
+    }
+
+}
---
+++ products/greg/modules/integration/registry/tests/src/test/java/org/wso2/carbon/registry/permission/test/GRegMetaDataPermissionServiceTestCase.java
@@ -0,0 +1,139 @@
+/*
+* Copyright (c) 2010, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package org.wso2.carbon.registry.permission.test;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import org.wso2.carbon.admin.service.AdminServiceAuthentication;
+import org.wso2.carbon.admin.service.AdminServiceResourceAdmin;
+import org.wso2.carbon.admin.service.AdminServiceUserMgtService;
+import org.wso2.carbon.authenticator.stub.LoginAuthenticationExceptionException;
+import org.wso2.carbon.authenticator.stub.LogoutAuthenticationExceptionException;
+import org.wso2.carbon.integration.framework.ClientConnectionUtil;
+import org.wso2.carbon.integration.framework.LoginLogoutUtil;
+import org.wso2.carbon.integration.framework.utils.FrameworkSettings;
+import org.wso2.carbon.registry.resource.stub.ResourceAdminServiceExceptionException;
+import org.wso2.carbon.user.mgt.common.UserAdminException;
+
+import java.rmi.RemoteException;
+
+import static org.testng.Assert.assertTrue;
+
+
+public class GRegMetaDataPermissionServiceTestCase {
+
+    private static final Log log = LogFactory.getLog(GRegMetaDataPermissionServiceTestCase.class);
+    private AdminServiceUserMgtService userAdminStub;
+    private static AdminServiceAuthentication userAuthenticationStub;
+    private static AdminServiceResourceAdmin admin_service_resource_admin;
+    private String gregHostName;
+    private String sessionCookie;
+    private String roleName;
+    private String userName;
+    private String userPassword;
+    private LoginLogoutUtil util = new LoginLogoutUtil();
+    private String SERVER_URL;
+
+    @BeforeClass(alwaysRun = true)
+    public void init() throws Exception {
+        ClientConnectionUtil.waitForPort(Integer.parseInt(FrameworkSettings.HTTP_PORT));
+        sessionCookie = util.login();
+        SERVER_URL = "https://" + FrameworkSettings.HOST_NAME +
+                            ":" + FrameworkSettings.HTTPS_PORT + "/services/";
+        gregHostName = FrameworkSettings.HOST_NAME;
+        userAdminStub = new AdminServiceUserMgtService(SERVER_URL);
+        userAuthenticationStub = new AdminServiceAuthentication(SERVER_URL);
+        admin_service_resource_admin = new AdminServiceResourceAdmin(SERVER_URL);
+        roleName = "meta_role";
+        userName = "greg_meta_user";
+
+        if (userAdminStub.roleNameExists(roleName, sessionCookie)) {  //delete the role if exists
+            userAdminStub.deleteRole(sessionCookie, roleName);
+        }
+
+        if (userAdminStub.userNameExists(roleName, sessionCookie, userName)) { //delete user if exists
+            userAdminStub.deleteUser(sessionCookie, userName);
+        }
+    }
+
+
+    @Test(groups = {"wso2.greg"}, description = "test add a role with login permission",
+          priority = 1)
+    public void testAddMetaDataPermissionUser()
+            throws UserAdminException, RemoteException, ResourceAdminServiceExceptionException,
+                   LoginAuthenticationExceptionException, LogoutAuthenticationExceptionException {
+
+        userPassword = "welcome";
+        String permission1[] = {"/permission/admin/login"};
+//                                "/permission/admin/manage/resources/govern/metadata"};
+        String permission2[] = {"/permission/admin/login",
+                                "/permission/admin/manage/resources"};
+        String sessionCookieUser;
+        boolean status;
+        String resourceName = "echo.wsdl";
+        String fetchUrl = "http://people.wso2.com/~evanthika/wsdls/echo.wsdl";
+        addRolewithUser(permission1);
+        sessionCookieUser = new AdminServiceAuthentication(SERVER_URL).login(userName, userPassword, gregHostName);
+        log.info("Newly Created User Loged in :" + userName);
+
+
+        try {
+            status = false;
+            // greg_meta_user does not have permission to add a Text resource
+            admin_service_resource_admin.addTextResource(sessionCookieUser, "/", "resource.txt",
+                                                         "", "", "");
+        } catch (RemoteException e) {
+            status = true;
+            assertTrue(e.getMessage().indexOf("Access Denied.") > 0, "Access Denied Remote" +
+                                                                     " Exception assertion Failed :");
+            log.info("greg_login_user does not have permission to add a text resource :");
+        }
+        assertTrue(status, "Only user with write permission can put text resource");
+        userAuthenticationStub.logOut();
+        deleteRoleAndUsers(roleName, userName);
+        addRolewithUser(permission2);
+        sessionCookieUser = userAuthenticationStub.login(userName, userPassword, gregHostName);
+        log.info("Newly Created User Loged in :" + userName);
+        admin_service_resource_admin.addWSDL(sessionCookieUser, resourceName, "", fetchUrl);
+        admin_service_resource_admin.deleteResource(sessionCookieUser,
+                                                    "/_system/governance/trunk/services/" +
+                                                    "org/wso2/carbon/core/services/echo/");
+        admin_service_resource_admin.deleteResource(sessionCookieUser,
+                                                    "/_system/governance/trunk/wsdls/org/" +
+                                                    "wso2/carbon/core/services/echo/");
+        userAuthenticationStub.logOut();
+        deleteRoleAndUsers(roleName, userName);
+        log.info("*********GReg Metadata Permission Asigning Scenario test - Passed**********");
+    }
+
+    private void addRolewithUser(String[] permission) throws
+                                                      UserAdminException {
+        userAdminStub.addRole(roleName, null, permission, sessionCookie);
+        log.info("Successfully added Role :" + roleName);
+        String roles[] = {roleName};
+        userAdminStub.addUser(sessionCookie, userName, userPassword, roles, null);
+        log.info("Successfully User Crated :" + userName);
+    }
+
+
+    private void deleteRoleAndUsers(String roleName, String userName) {
+        userAdminStub.deleteRole(sessionCookie, roleName);
+        log.info("Role " + roleName + " deleted successfully");
+        userAdminStub.deleteUser(sessionCookie, userName);
+    }
+}

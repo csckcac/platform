78e2fd2f3838b44886e2b0e2b8dc75eebc591c0a
ajanthan <ajanthan@a5903396-d722-0410-b921-86c7d4935375>
2012-06-19 03:58:33 +0000

    converting the logic to use available client impl base on availability
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@130423 a5903396-d722-0410-b921-86c7d4935375

--- components/hostobjects/org.wso2.carbon.hostobjects.svnclient/src/main/java/org/wso2/carbon/hostobjects/svnclient/SVNClientHostObject.java
+++ components/hostobjects/org.wso2.carbon.hostobjects.svnclient/src/main/java/org/wso2/carbon/hostobjects/svnclient/SVNClientHostObject.java
@@ -19,49 +19,102 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.mozilla.javascript.*;
 import org.tigris.subversion.javahl.ClientException;
-import org.tigris.subversion.javahl.Info;
-import org.tigris.subversion.javahl.LogMessage;
-import org.tigris.subversion.javahl.Revision;
-import org.tigris.subversion.javahl.SVNClient;
+import org.tigris.subversion.svnclientadapter.*;
+import org.tigris.subversion.svnclientadapter.commandline.CmdLineClientAdapterFactory;
+import org.tigris.subversion.svnclientadapter.javahl.JhlClientAdapterFactory;
+import org.tigris.subversion.svnclientadapter.svnkit.SvnKitClientAdapterFactory;
 import org.wso2.carbon.scriptengine.exceptions.ScriptException;
 
 import java.net.MalformedURLException;
-import java.net.URL;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Properties;
 
 /**
  * The SVN Client host object allows the mashups to get the information related to a given svn repository.
  * The information can be svn log, svn info, ..
  * This host object can be further extended to handle similar uses.
  */
+
 public class SVNClientHostObject extends ScriptableObject {
     private static final String hostObjectName = "SVNClient";
 
-    private static String svnUrl;
-    private static String userName;
-    private static String password;
-    private NativeObject nativeObject;
+    private SVNUrl svnUrl;
+    /*   private  String userName;
+ private  String password;*/
+
     private int logCount = 0;
     private int commitRange = 0;
 
-    private static String[] svnRepoInfo = new String[3];
+    private String[] svnRepoInfo = new String[3];
 
-    private static SVNClient svnClient;
-
-    private Properties properties;
+    private ISVNClientAdapter svnClient;
 
     private static final Log log = LogFactory.getLog(SVNClientHostObject.class);
 
-    public SVNClientHostObject(String url) {
-        svnUrl = url;
-        svnClient = new org.tigris.subversion.javahl.SVNClient();
+    private void initSVNClient(String userName, String password) throws ScriptException {
+        try {
+            SvnKitClientAdapterFactory.setup();
+            log.debug("SVN Kit client adapter initialized");
+        } catch (Throwable t) {
+            log.debug("Unable to initialize the SVN Kit client adapter - Required jars " +
+                    "may be missing", t);
+        }
+
+        try {
+            JhlClientAdapterFactory.setup();
+            log.debug("Java HL client adapter initialized");
+        } catch (Throwable t) {
+            log.debug("Unable to initialize the Java HL client adapter - Required jars " +
+                    " or the native libraries may be missing", t);
+        }
+
+        try {
+            CmdLineClientAdapterFactory.setup();
+            log.debug("Command line client adapter initialized");
+        } catch (Throwable t) {
+            log.debug("Unable to initialize the command line client adapter - SVN command " +
+                    "line tools may be missing", t);
+        }
+
+
+        String clientType;
+        try {
+            clientType = SVNClientAdapterFactory.getPreferredSVNClientType();
+            svnClient = SVNClientAdapterFactory.createSVNClient(clientType);
+            if ((userName != null) && (password != null)) {
+                svnClient.setUsername(userName);
+                svnClient.setPassword(password);
+            }
+        } catch (SVNClientException e) {
+            throw new ScriptException("Client type can not be defined.");
+        }
+
+        if (svnClient == null) {
+            throw new ScriptException("Failed to instantiate svn client.");
+        }
     }
 
-    public SVNClientHostObject() {
-        svnClient = new org.tigris.subversion.javahl.SVNClient();
+
+    public SVNClientHostObject(String userName, String password) throws ScriptException {
+        try {
+            initSVNClient(userName, password);
+        } catch (ScriptException e) {
+            String msg="Failed to initiate svn client";
+            log.error(msg,e);
+            throw new ScriptException(msg,e);
+        }
+    }
+
+    public SVNClientHostObject() throws ScriptException {
+        try {
+            initSVNClient(null, null);
+        } catch (ScriptException e) {
+            String msg="Failed to initiate svn client";
+            log.error(msg,e);
+            throw new ScriptException(msg,e);
+        }
     }
 
     /**
@@ -73,6 +126,7 @@ public class SVNClientHostObject extends ScriptableObject {
      */
     public void jsSet_params(Object object) throws ScriptException {
         if (object instanceof NativeObject) {
+            NativeObject nativeObject;
             nativeObject = (NativeObject) object;
 
             Object commitRangeObj = ScriptableObject.getProperty(nativeObject, "commitRange");
@@ -126,36 +180,38 @@ public class SVNClientHostObject extends ScriptableObject {
      */
     public static Scriptable jsConstructor(Context cx, Object[] args, Function ctorObj,
                                            boolean inNewExpr) throws ScriptException, MalformedURLException {
-
-        SVNClientHostObject svnClientHostObject = new SVNClientHostObject();
-        svnClientHostObject.properties = new Properties();
-
+        String svnUrlL, userNameL = null, passwordL = null;
         int length = args.length;
         if (length == 1) {
-            svnUrl = (String) args[0];
+            svnUrlL = (String) args[0];
         } else if (length == 3) {
-            svnUrl = (String) args[0];
-            userName = (String) args[1];
-            password = (String) args[2];
+            svnUrlL = (String) args[0];
+            userNameL = (String) args[1];
+            passwordL = (String) args[2];
         } else {
             throw new ScriptException("Incorrect number of arguments. Please specify repositoryUrl " +
                     "or repositoryUrl, username, password within the constructListor of SVNClient hostobject.");
         }
 
-        if (svnUrl == null) {
+        if (svnUrlL == null) {
             throw new ScriptException("Invalid SVN Repository. Please recheck the given details of the repository.");
         }
-
+        SVNUrl url;
         try {
-            URL url = new URL(svnUrl);
+            url = new SVNUrl(svnUrlL);
         } catch (MalformedURLException e) {
+            log.error("SVN URL is malformed",e);
             throw e;
         }
+        SVNClientHostObject svnClientHostObject = new SVNClientHostObject(userNameL, passwordL);
+        svnClientHostObject.setSvnUrl(url);
+
+
         return svnClientHostObject;
     }
 
 
-    public String[] jsFunction_getRepositoryInfo() throws MalformedURLException, ClientException {
+    public String[] jsFunction_getRepositoryInfo() throws MalformedURLException, ClientException, ScriptException {
         getRepositoryInformation();
         if (log.isDebugEnabled()) {
             log.debug("Last modified on: " + svnRepoInfo[0]);
@@ -165,44 +221,80 @@ public class SVNClientHostObject extends ScriptableObject {
         return svnRepoInfo;
     }
 
-    private static String[] getRepositoryInformation() throws MalformedURLException, ClientException {
-        Info svnInfo = svnClient.info(svnUrl);
+    private String[] getRepositoryInformation() throws MalformedURLException, ClientException, ScriptException {
+        ISVNInfo svnInfo;
+        try {
+            svnInfo = svnClient.getInfo(svnUrl);
+        } catch (SVNClientException e) {
+            String msg="Failed to get svn info from "+svnUrl;
+            log.error(msg,e);
+            throw new ScriptException(msg,e);
+        }
         Date lastChangedDate = svnInfo.getLastChangedDate();
         svnRepoInfo[0] = lastChangedDate.toString();
 
-        String lastChangedAuthor = svnInfo.getAuthor();
+        String lastChangedAuthor = svnInfo.getLastCommitAuthor();
         svnRepoInfo[1] = lastChangedAuthor;
 
-        long lastChangedRev = svnInfo.getLastChangedRevision();
+        SVNRevision.Number lastChangedRev = svnInfo.getLastChangedRevision();
         svnRepoInfo[2] = String.valueOf(lastChangedRev);
         return svnRepoInfo;
     }
 
-    private LogMessage[] getCommitMessages() throws ClientException, MalformedURLException {
+    private ISVNLogMessage[] getCommitMessages() throws ClientException, MalformedURLException, ScriptException {
         getRepositoryInformation();
-        int lastChangedRevisionNumber = Integer.parseInt(svnRepoInfo[2]);
-        Revision endRevision = Revision.getInstance(lastChangedRevisionNumber);
-        Revision startRevision;
-        if (commitRange < 1) {
-            startRevision = Revision.getInstance(0);
-        } else {
-            startRevision = Revision.getInstance(lastChangedRevisionNumber - commitRange);
-        }
 
-        return svnClient.logMessages(svnUrl, startRevision, endRevision);
+        SVNRevision endRevision;
+        try {
+            endRevision = SVNRevision.getRevision(svnRepoInfo[2]);
+        } catch (ParseException e) {
+            String msg="Failed to get svn revision from "+svnUrl;
+            log.error(msg,e);
+            throw new ScriptException(msg,e);
+        }
+        SVNRevision startRevision = null;
+        if (commitRange < 1) {
+            try {
+                startRevision = SVNRevision.getRevision("0");
+            } catch (ParseException e) {
+                String msg="Failed to get svn revision from "+svnUrl;
+                log.error(msg,e);
+                throw new ScriptException(msg,e);
+            }
+        } else {
+            try {
+                System.out.println(startRevision);
+                startRevision = SVNRevision.getRevision(Integer.toString(Integer.parseInt(svnRepoInfo[2]) - commitRange));
+                System.out.println(startRevision);
+            } catch (ParseException e) {
+                String msg="Failed to get svn revision from "+svnUrl;
+                log.error(msg,e);
+                throw new ScriptException(msg,e);
+            }
+        }
+        ISVNLogMessage logMessages[];
+        try {
+            logMessages = svnClient.getLogMessages(svnUrl, startRevision, endRevision);
+
+        } catch (SVNClientException e) {
+            String msg="Failed to get svn log messages from "+svnUrl;
+            log.error(msg,e);
+            throw new ScriptException(msg,e);
+        }
+        return logMessages;
     }
 
 
     public String[] jsFunction_getCommitLogs() throws Exception {
         List<String> commitLogs = new ArrayList<String>();
-        LogMessage[] logMessages;
+        ISVNLogMessage[] logMessages;
         StringBuilder commitLogBuf;
         int count = 1;
 
         logMessages = getCommitMessages(); //limit only upto the given count
         try {
             for (int i = logMessages.length - 1; i > 0; i--) {
-                LogMessage logMessage = logMessages[i];
+                ISVNLogMessage logMessage = logMessages[i];
                 commitLogBuf = new StringBuilder();
                 commitLogBuf.append(logMessage.getRevision()).append(" | ").append(logMessage.getAuthor()).append(" | ").
                         append(logMessage.getDate()).append("\n").append(" | ").append(logMessage.getMessage());
@@ -227,5 +319,25 @@ public class SVNClientHostObject extends ScriptableObject {
     @Override
     public String getClassName() {
         return hostObjectName;
+    }
+
+    public void setSvnUrl(SVNUrl svnUrl) {
+        this.svnUrl = svnUrl;
+    }
+
+    public int getLogCount() {
+        return logCount;
+    }
+
+    public void setLogCount(int logCount) {
+        this.logCount = logCount;
+    }
+
+    public int getCommitRange() {
+        return commitRange;
+    }
+
+    public void setCommitRange(int commitRange) {
+        this.commitRange = commitRange;
     }
 }

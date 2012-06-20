37f9c6ae7b5bd0dc3ffeb7b06e1c877f3a901b4a
madhuka <madhuka@a5903396-d722-0410-b921-86c7d4935375>
2012-06-20 12:46:42 +0000

    Mustache template added
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@130651 a5903396-d722-0410-b921-86c7d4935375

--- products/jaggery/apps/coffeeshop/js/coffeeshop.js
+++ products/jaggery/apps/coffeeshop/js/coffeeshop.js
@@ -43,59 +43,17 @@ $('#addition').modal('toggle');
 }
 
 this.addAddition = function() {
-//console.log(orderID+"Click  on addAddition ");
-//document.getElementById("modal-order-no").innerHTML = orderID;
 var orderID = $("input#orderAdditionId").val();
 var additional = $("#additional-toppings").val();
-
-//alert("order "+orderID+" has been saved");
 CoffeeShopClient.addAddittion(orderID,additional);
-console.log(orderID+"Clickx  on addAddition "+additional);
 $('#addition').modal('toggle');
 }
 
 this.loadOrders = function(data) {
-var template = '{{#orders}}<div class="order-entry"> '
-+'<div class="order-number">{{ORDER_ID}}</div>'
-+'<div class="order-content">'
-+'{{DRINK}} <span class="help-block">{{ADDITION}}<a href="#" onclick="CoffeeShop.showAddition({{ORDER_ID}});" > Add addition</a></span>'
-+'</div> <div class="order-actions" id="{{ORDER_ID}}"> '
-+'<a class="btn btn-success completed" style="display:none;">Completed</a>'
-+'<ul class="order-status">'
-+'<li class="dropdown">'
-+'<a class="dropdown-toggle btn btn-warning" data-toggle="dropdown" rel="tooltip" title="Order Status" href="#menu1"> {{STATUS}} <b class="caret"></b> </a>'
-+'<ul class="dropdown-menu">'
-+'<li>'
-+'<a href="#" class="mark-paid" onclick="CoffeeShop.markComplete({{ORDER_ID}})">Mark as Complete</a>'
-+'</li>'
-+'<li>'
-+'<a href="#" onclick="CoffeeShop.markDeletOrder({{ORDER_ID}})">Delete Order</a>'
-+'</li>'
-+'</ul>'
-+'</li>'
-+'</ul>'
-+'<a class="btn btn-success paid" style="display:none;">Paid</a>'
-+'<ul class="payment-action">'
-+'{{^PAY}}<li class="dropdown">'
-+'<a class="dropdown-toggle btn btn-danger" data-toggle="dropdown" href="#menu1"> Not Paid <b class="caret"></b> </a>'
-+'<ul class="dropdown-menu">'
-+'<li>'
-+'<a href="#" class="mark-paid" onclick="CoffeeShop.markPaid({{ORDER_ID}})">Mark as Paid</a>'
-+'</li>{{/PAY}}'
-+'</ul> '
-+'{{#PAY}} <button class="btn btn-success"  href="#"> paid</button>{{/PAY}}'
-+'</li>'
-+'</ul>'
-+'</div>'
-+'<div class="amount">'
-+'{{COST}}'
-+'</div>'
-+'</div>{{/orders}}</div></div>';
-
+$.get('template/order.html', function(template) {
 var html = Mustache.to_html(template, data);
-console.log("Call template onload ");
 $("#order-window").html(html);
-
+});
 }
 
 }

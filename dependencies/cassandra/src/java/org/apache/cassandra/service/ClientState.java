d31616d2b9179d531df94a38d5e60808d9b1d390
sinthuja <sinthuja@a5903396-d722-0410-b921-86c7d4935375>
2012-06-22 07:18:39 +0000

    fixing unconfigured column family issue
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@130879 a5903396-d722-0410-b921-86c7d4935375

--- dependencies/cassandra/src/java/org/apache/cassandra/service/ClientState.java
+++ dependencies/cassandra/src/java/org/apache/cassandra/service/ClientState.java
@@ -215,7 +215,7 @@ public class ClientState
      * @return <String> the resolved keyspace name
      */
     public String getResolvedKeyspace() {
-
+        qualifiedKeyspace = prefix.equals("carbon_super_")?keyspace:prefix + keyspace;
         return qualifiedKeyspace == null ? keyspace : qualifiedKeyspace;
     }
 

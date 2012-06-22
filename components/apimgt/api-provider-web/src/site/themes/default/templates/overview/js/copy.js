2cf2bed7c633fc52fccb7308d6883cb4f55016eb
lalaji <lalaji@a5903396-d722-0410-b921-86c7d4935375>
2012-06-22 04:33:41 +0000

    Corrected getting apiname and version
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@130853 a5903396-d722-0410-b921-86c7d4935375

--- components/apimgt/api-provider-web/src/site/themes/default/templates/overview/js/copy.js
+++ components/apimgt/api-provider-web/src/site/themes/default/templates/overview/js/copy.js
@@ -1,6 +1,7 @@
 var copyAPIToNewVersion = function (provider) {
-    var apiName = $("#item-info h2")[0].innerHTML.split("-v")[0];
-    var version = $("#item-info h2")[0].innerHTML.split("-v")[1];
+    var api=$("#item-info h2")[0].innerHTML;
+    var apiName = $.trim(api.split("-")[0]);
+    var version = $.trim(api.split("-")[1]);
     var newVersion = $("#copy-api #new-version").val();
     jagg.post("/site/blocks/overview/ajax/overview.jag", { action:"createNewAPI", provider:provider,apiName:apiName, version:version, newVersion:newVersion },
               function (result) {

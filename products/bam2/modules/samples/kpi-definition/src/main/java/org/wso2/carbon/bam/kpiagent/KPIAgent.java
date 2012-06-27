be36d66e3f66629510fe37126a1026c5797d6744
sinthuja <sinthuja@a5903396-d722-0410-b921-86c7d4935375>
2012-06-27 10:07:10 +0000

    updating the sample as  phone retail store sample
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@131253 a5903396-d722-0410-b921-86c7d4935375

--- products/bam2/modules/samples/kpi-definition/src/main/java/org/wso2/carbon/bam/kpiagent/KPIAgent.java
+++ products/bam2/modules/samples/kpi-definition/src/main/java/org/wso2/carbon/bam/kpiagent/KPIAgent.java
@@ -15,23 +15,20 @@ import java.util.Random;
 
 public class KPIAgent {
     private static Logger logger = Logger.getLogger(KPIAgent.class);
-    public static final String STREAM_NAME1 = "org.wso2.bam.kpi";
-    public static final String STREAM_NAME2 = "org.wso2.bam.kpiii";
-    public static final String STREAM_NAME3 = "org.wso2.bam.retailstore.kpi";
-    public static final String VERSION1 = "1.0.5";
-    public static final String VERSION2 = "1.1.5";
-    public static final String VERSION3 = "2.0.5";
+    public static final String PHONE_RETAIL_STREAM = "org.wso2.bam.phone.retail.store.kpi";
+    public static final String VERSION = "1.0.0";
 
-    public static final String[] foods = {"Rice", "Wheat", "Oat", "Millet", "Corn"};
-    public static final String[] users = {"James", "Mary" , "John", "Peter", "Harry" , "Tom", "Paul"};
-    public static final int[] quantity = {2, 6, 3, 4, 7};
-    public static final int[] price = {120, 130, 125, 140, 115};
+    public static final String[] phoneModels = {"Nokia", "Apple", "Samsung", "Sony-Ericson", "LG"};
+    public static final String[] users = {"James", "Mary", "John", "Peter", "Harry", "Tom", "Paul"};
+    public static final int[] quantity = {2, 5, 3, 4, 1};
+    public static final int[] price = {50000, 55000, 90000, 80000, 70000};
+
 
     public static void main(String[] args) throws AgentException, MalformedStreamDefinitionException,
             StreamDefinitionException, DifferentStreamDefinitionAlreadyDefinedException, MalformedURLException,
             AuthenticationException, NoStreamDefinitionExistException,
             org.wso2.carbon.eventbridge.commons.exception.AuthenticationException, TransportException, SocketException {
-        System.out.println("Starting BAM KPI Agent");
+        System.out.println("Starting BAM Phone Reatil Shop KPI Agent");
         AgentConfiguration agentConfiguration = new AgentConfiguration();
         String currentDir = System.getProperty("user.dir");
         System.setProperty("javax.net.ssl.trustStore", currentDir + "/src/main/resources/client-truststore.jks");
@@ -42,73 +39,24 @@ public class KPIAgent {
         host = getLocalAddress().getHostAddress();
         //create data publisher
 
-        DataPublisher dataPublisher = new DataPublisher("tcp://" +host+ ":7611", "admin", "admin", agent);
-
-        String streamId1 = null;
-        String streamId2 = null;
-        String streamId3 = null;
+        DataPublisher dataPublisher = new DataPublisher("tcp://" + host + ":7611", "admin", "admin", agent);
+        String streamId = null;
 
         try {
-            streamId1 = dataPublisher.findEventStream(STREAM_NAME1, VERSION1);
+            streamId = dataPublisher.findEventStream(PHONE_RETAIL_STREAM, VERSION);
             System.out.println("Stream already defined");
 
         } catch (NoStreamDefinitionExistException e) {
-            streamId1 = dataPublisher.defineEventStream("{" +
-                    "  'name':'" + STREAM_NAME1 + "'," +
-                    "  'version':'" + VERSION1 + "'," +
-                    "  'nickName': 'KPI'," +
-                    "  'description': 'Product Sale'," +
+            streamId = dataPublisher.defineEventStream("{" +
+                    "  'name':'" + PHONE_RETAIL_STREAM + "'," +
+                    "  'version':'" + VERSION + "'," +
+                    "  'nickName': 'Phone_Retail_Shop'," +
+                    "  'description': 'Phone Sales'," +
                     "  'metaData':[" +
                     "          {'name':'clientType','type':'STRING'}" +
                     "  ]," +
                     "  'payloadData':[" +
-                    "          {'name':'item','type':'STRING'}," +
-                    "          {'name':'quantity','type':'INT'}," +
-                    "          {'name':'total','type':'INT'}," +
-                    "          {'name':'user','type':'STRING'}" +
-                    "  ]" +
-                    "}");
-//            //Define event stream
-        }
-
-        try {
-            streamId2 = dataPublisher.findEventStream(STREAM_NAME2, VERSION2);
-            System.out.println("Stream already defined");
-
-        } catch (NoStreamDefinitionExistException e) {
-            streamId2 = dataPublisher.defineEventStream("{" +
-                    "  'name':'" + STREAM_NAME2 + "'," +
-                    "  'version':'" + VERSION2 + "'," +
-                    "  'nickName': 'KPI'," +
-                    "  'description': 'Product Sale'," +
-                    "  'metaData':[" +
-                    "          {'name':'clientType','type':'STRING'}" +
-                    "  ]," +
-                    "  'payloadData':[" +
-                    "          {'name':'item','type':'STRING'}," +
-                    "          {'name':'quantity','type':'INT'}," +
-                    "          {'name':'total','type':'INT'}," +
-                    "          {'name':'user','type':'STRING'}" +
-                    "  ]" +
-                    "}");
-//            //Define event stream
-        }
-
-        try {
-            streamId3 = dataPublisher.findEventStream(STREAM_NAME3, VERSION3);
-            System.out.println("Stream already defined");
-
-        } catch (NoStreamDefinitionExistException e) {
-            streamId3 = dataPublisher.defineEventStream("{" +
-                    "  'name':'" + STREAM_NAME3 + "'," +
-                    "  'version':'" + VERSION3 + "'," +
-                    "  'nickName': 'KPI'," +
-                    "  'description': 'Product Sale'," +
-                    "  'metaData':[" +
-                    "          {'name':'clientType','type':'STRING'}" +
-                    "  ]," +
-                    "  'payloadData':[" +
-                    "          {'name':'item','type':'STRING'}," +
+                    "          {'name':'brand','type':'STRING'}," +
                     "          {'name':'quantity','type':'INT'}," +
                     "          {'name':'total','type':'INT'}," +
                     "          {'name':'user','type':'STRING'}" +
@@ -119,17 +67,15 @@ public class KPIAgent {
 
 
         //Publish event for a valid stream
-        if (!streamId3.isEmpty()) {
-            System.out.println("Stream ID: " + streamId1);
+        if (!streamId.isEmpty()) {
+            System.out.println("Stream ID: " + streamId);
 
             for (int i = 0; i < 100; i++) {
-                publishEvents(dataPublisher, streamId1, i);
-                publishEvents(dataPublisher, streamId2, i);
-                publishEvents(dataPublisher, streamId3, i);
-                System.out.println("Events published : " + (i + 1) * 7 * 3);
+                publishEvents(dataPublisher, streamId, i);
+                System.out.println("Events published : " + i);
             }
             try {
-                Thread.sleep(3000);
+                Thread.sleep(2000);
             } catch (InterruptedException e) {
             }
 
@@ -138,47 +84,43 @@ public class KPIAgent {
     }
 
     private static void publishEvents(DataPublisher dataPublisher, String streamId, int i) throws AgentException {
+        int quantity = getRandomQuantity();
         Event eventOne = new Event(streamId, System.currentTimeMillis(), new Object[]{"external"}, null,
-                new Object[]{getRandomProduct(), getRandomQuantity(), getRandomPrice(), getRandomUser()});
+                new Object[]{getRandomProduct(), quantity, quantity*getRandomPrice(), getRandomUser()});
         dataPublisher.publish(eventOne);
     }
 
-    private static String getRandomProduct(){
-      return foods[getRandomId(5)];
+    private static String getRandomProduct() {
+        return phoneModels[getRandomId(5)];
     }
 
-    private static String getRandomUser(){
-       return users[getRandomId(7)];
+    private static String getRandomUser() {
+        return users[getRandomId(7)];
     }
 
-    private static int getRandomQuantity(){
+    private static int getRandomQuantity() {
         return quantity[getRandomId(5)];
     }
 
-    private static int getRandomPrice(){
+    private static int getRandomPrice() {
         return price[getRandomId(5)];
     }
 
 
-
-    private static int getRandomId(int i){
+    private static int getRandomId(int i) {
         Random randomGenerator = new Random();
         return randomGenerator.nextInt(i);
     }
 
-    public static InetAddress getLocalAddress() throws SocketException
-    {
+    public static InetAddress getLocalAddress() throws SocketException {
         Enumeration<NetworkInterface> ifaces = NetworkInterface.getNetworkInterfaces();
-        while( ifaces.hasMoreElements() )
-        {
+        while (ifaces.hasMoreElements()) {
             NetworkInterface iface = ifaces.nextElement();
             Enumeration<InetAddress> addresses = iface.getInetAddresses();
 
-            while( addresses.hasMoreElements() )
-            {
+            while (addresses.hasMoreElements()) {
                 InetAddress addr = addresses.nextElement();
-                if( addr instanceof Inet4Address && !addr.isLoopbackAddress() )
-                {
+                if (addr instanceof Inet4Address && !addr.isLoopbackAddress()) {
                     return addr;
                 }
             }

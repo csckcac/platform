9236792e638e38654bc78fd1e5d8c29a6d520fbe
ajithn <ajithn@a5903396-d722-0410-b921-86c7d4935375>
2012-07-01 04:52:58 +0000

    Fixing REGISTRY-988
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@131490 a5903396-d722-0410-b921-86c7d4935375

--- components/registry/org.wso2.carbon.registry.indexing/src/main/java/org/wso2/carbon/registry/indexing/indexer/XMLIndexer.java
+++ components/registry/org.wso2.carbon.registry.indexing/src/main/java/org/wso2/carbon/registry/indexing/indexer/XMLIndexer.java
@@ -3,7 +3,6 @@ package org.wso2.carbon.registry.indexing.indexer;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.solr.common.SolrException;
-import org.apache.solr.common.SolrException.ErrorCode;
 import org.wso2.carbon.registry.core.exceptions.RegistryException;
 import org.wso2.carbon.registry.core.utils.RegistryUtils;
 import org.wso2.carbon.registry.indexing.AsyncIndexer.File2Index;
@@ -11,49 +10,34 @@ import org.wso2.carbon.registry.indexing.solr.IndexDocument;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
 import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.net.MalformedURLException;
 
 public class XMLIndexer implements Indexer {
 
 	public static final Log log = LogFactory.getLog(XMLIndexer.class);
 
 	public IndexDocument getIndexedDocument(File2Index fileData) throws SolrException, RegistryException {
-		try {
-			// we register both the content as it is and only text content
-			String xmlAsStr = RegistryUtils.decodeBytes(fileData.data);
+        // we register both the content as it is and only text content
+        String xmlAsStr = RegistryUtils.decodeBytes(fileData.data);
 
-			final StringBuffer contentOnly = new StringBuffer();
-			ByteArrayInputStream inData = new ByteArrayInputStream(fileData.data);
+        final StringBuffer contentOnly = new StringBuffer();
+        ByteArrayInputStream inData = new ByteArrayInputStream(fileData.data);
 
-			// this will handle text content
-			DefaultHandler handler = new DefaultHandler() {
-				public void characters(char ch[], int start, int length)
-				throws SAXException {
-					contentOnly.append(new String(ch, start, length)).append(" ");
-				}
-			};
-			SAXParserFactory factory = SAXParserFactory.newInstance();
-			SAXParser saxParser = factory.newSAXParser();
-			saxParser.parse(inData, handler);
+        // this will handle text content
+        DefaultHandler handler = new DefaultHandler() {
+            public void characters(char ch[], int start, int length)
+            throws SAXException {
+                contentOnly.append(new String(ch, start, length)).append(" ");
+            }
+        };
+//			SAXParserFactory factory = SAXParserFactory.newInstance();
+//			SAXParser saxParser = factory.newSAXParser();
+//			saxParser.parse(inData, handler);
 
-			return new IndexDocument(fileData.path, xmlAsStr,
-			                   contentOnly.toString());
-		} catch (MalformedURLException e) {
-			throw new SolrException(ErrorCode.SERVER_ERROR, "Error at indexing", e);
-		} catch (IOException e) {
-			throw new SolrException(ErrorCode.SERVER_ERROR, "Error at indexing", e);
-		} catch (ParserConfigurationException e) {
-			throw new SolrException(ErrorCode.SERVER_ERROR, "Error at indexing", e);
-		} catch (SAXException e) {
-			throw new SolrException(ErrorCode.SERVER_ERROR, "Error at indexing", e);
-		}
+        return new IndexDocument(fileData.path, xmlAsStr,
+                           contentOnly.toString());
 
 
-	}
+    }
 
 }

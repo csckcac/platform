41116a883cf34d549fb2c14c8875afa76fcc2285
achala <achala@a5903396-d722-0410-b921-86c7d4935375>
2012-07-05 06:10:30 +0000

    Adding SVNManager
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@131770 a5903396-d722-0410-b921-86c7d4935375

---
+++ components/appfactory/org.wso2.carbon.appfactory.svn.repository.mgt.service/src/main/java/org/wso2/carbon/appfactory/svn/repository/mgt/impl/SVNManager.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2005-2011 WSO2, Inc. (http://wso2.com)
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package org.wso2.carbon.appfactory.svn.repository.mgt.impl;
+
+import org.apache.commons.logging.LogFactory;
+import org.wso2.carbon.appfactory.common.AppFactoryConfiguration;
+import org.wso2.carbon.appfactory.common.AppFactoryConstants;
+import org.wso2.carbon.appfactory.common.AppFactoryException;
+import org.wso2.carbon.appfactory.core.RevisionControlDriver;
+import org.wso2.carbon.appfactory.core.RevisionControlDriverListener;
+import org.wso2.carbon.appfactory.svn.repository.mgt.util.Util;
+
+
+public class SVNManager implements RevisionControlDriver {
+    private static final org.apache.commons.logging.Log log = LogFactory.getLog(SCMManagerBasedRepositoryManager.class);
+    private static AppFactoryConfiguration appFactoryConfiguration = Util.getConfiguration();
+
+    @Override
+    public void getSource(String applicationId, String version, String revision, RevisionControlDriverListener listener) throws AppFactoryException {
+
+        SCMManagerBasedRepositoryManager scm = new SCMManagerBasedRepositoryManager();
+        try {
+            scm.checkoutApplication(getApplicationUrl(), applicationId, revision);
+            listener.onGetSourceCompleted(applicationId, version, revision);
+        } catch (SCMManagerExceptions scmManagerExceptions) {
+            log.error("Error in checkout" + scmManagerExceptions);
+        }
+
+    }
+
+    public String getApplicationUrl() {
+        return appFactoryConfiguration.getFirstProperty(AppFactoryConstants.DEPLOYMENT_URL);
+    }
+}

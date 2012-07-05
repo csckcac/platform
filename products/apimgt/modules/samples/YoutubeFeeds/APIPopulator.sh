5d3c7effbf986df0dbb22f32d35885bbea7a8aaa
lalaji <lalaji@a5903396-d722-0410-b921-86c7d4935375>
2012-07-05 12:07:05 +0000

    Fixed APISTORE-439
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@131828 a5903396-d722-0410-b921-86c7d4935375

--- products/apimgt/modules/samples/YoutubeFeeds/APIPopulator.sh
+++ products/apimgt/modules/samples/YoutubeFeeds/APIPopulator.sh
@@ -2,11 +2,11 @@
 START=$1
 COUNT=$2
 SERVER=http://localhost:9763
-curl -X POST -c cookies $SERVER/apiprovider/site/blocks/user/login/ajax/login.jag -d 'action=login&username=admin&password=admin';
+curl -X POST -c cookies $SERVER/publisher/site/blocks/user/login/ajax/login.jag -d 'action=login&username=admin&password=admin';
 
-curl -X POST -b cookies $SERVER/apiprovider/site/blocks/item-add/ajax/add.jag -d "action=addAPI&name=YoutubeFeeds&version=1.0.0&description=Youtube Live Feeds&endpoint=http://gdata.youtube.com/feeds/api/standardfeeds&wsdl=&tags=youtube,gdata,multimedia&tier=Silver&thumbUrl=http://www.10bigideas.com.au/www/573/files/pf-thumbnail-youtube_logo.jpg&context=/youtube&tiersCollection=Gold&resourceCount=0&resourceMethod-0=GET&uriTemplate-0=/*";
+curl -X POST -b cookies $SERVER/publisher/site/blocks/item-add/ajax/add.jag -d "action=addAPI&name=YoutubeFeeds&version=1.0.0&description=Youtube Live Feeds&endpoint=http://gdata.youtube.com/feeds/api/standardfeeds&wsdl=&tags=youtube,gdata,multimedia&tier=Silver&thumbUrl=http://www.10bigideas.com.au/www/573/files/pf-thumbnail-youtube_logo.jpg&context=/youtube&tiersCollection=Gold&resourceCount=0&resourceMethod-0=GET&uriTemplate-0=/*";
 
-curl -X POST -b cookies $SERVER/apiprovider/site/blocks/life-cycles/ajax/life-cycles.jag -d "name=YoutubeFeeds&version=1.0.0&provider=admin&status=PUBLISHED&publishToGateway=true&action=updateStatus";
+curl -X POST -b cookies $SERVER/publisher/site/blocks/life-cycles/ajax/life-cycles.jag -d "name=YoutubeFeeds&version=1.0.0&provider=admin&status=PUBLISHED&publishToGateway=true&action=updateStatus";
 
 
 

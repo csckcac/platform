a17f1848295797545a7f96585200c128a01b30b2
achala <achala@a5903396-d722-0410-b921-86c7d4935375>
2012-07-05 06:24:18 +0000

    Modifying the ServiceHolder
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@131772 a5903396-d722-0410-b921-86c7d4935375

--- components/appfactory/org.wso2.carbon.appfactory.core/src/main/java/org/wso2/carbon/appfactory/core/internal/ServiceHolder.java
+++ components/appfactory/org.wso2.carbon.appfactory.core/src/main/java/org/wso2/carbon/appfactory/core/internal/ServiceHolder.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2005-2011 WSO2, Inc. (http://wso2.com)
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package org.wso2.carbon.appfactory.core.internal;
 
 import org.wso2.carbon.appfactory.core.ArtifactStorage;
@@ -6,21 +22,45 @@ import org.wso2.carbon.appfactory.core.ContinuousIntegrationSystemDriver;
 import org.wso2.carbon.appfactory.core.RevisionControlDriver;
 
 public class ServiceHolder {
+    public static RevisionControlDriver revisionControlDriver;
+    public static BuildDriver buildDriver;
+    public static ArtifactStorage artifactStorage;
+    public static ContinuousIntegrationSystemDriver continuousIntegrationSystemDriver;
 
-	public static BuildDriver getBuildDriver() {
-		return null;
-	}
 
-	public static RevisionControlDriver getRevisionControlDriver() {
-		return null;
-	}
+    public static BuildDriver getBuildDriver() {
+        return buildDriver;
+    }
 
-	public static ContinuousIntegrationSystemDriver getContinuousIntegrationSystemDriver() {
-		return null;
-	}
+    public static void setBuildDriver(BuildDriver buildDriver) {
+        ServiceHolder.buildDriver = buildDriver;
+    }
 
-	public static ArtifactStorage getArtifactStorage() {
-		return null;
-	}
+
+    public static RevisionControlDriver getRevisionControlDriver() {
+        return revisionControlDriver;
+    }
+
+    public static void setRevisionControlDriver(RevisionControlDriver revisionControlDriver) {
+        ServiceHolder.revisionControlDriver = revisionControlDriver;
+    }
+
+
+    public static ContinuousIntegrationSystemDriver getContinuousIntegrationSystemDriver() {
+        return continuousIntegrationSystemDriver;
+    }
+
+    public static void setContinuousIntegrationSystemDriver(ContinuousIntegrationSystemDriver continuousIntegrationSystemDriver) {
+        ServiceHolder.continuousIntegrationSystemDriver = continuousIntegrationSystemDriver;
+    }
+
+
+    public static ArtifactStorage getArtifactStorage() {
+        return artifactStorage;
+    }
+
+    public static void setArtifactStorage(ArtifactStorage artifactStorage) {
+        ServiceHolder.artifactStorage = artifactStorage;
+    }
 
 }

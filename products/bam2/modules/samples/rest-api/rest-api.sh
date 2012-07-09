2abb0f8e395eba48c130f415d5883c5884f6514e
tharindu <tharindu@a5903396-d722-0410-b921-86c7d4935375>
2012-07-09 18:17:38 +0000

    adding jax rs api to product
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@132328 a5903396-d722-0410-b921-86c7d4935375

--- products/bam2/modules/samples/rest-api/rest-api.sh
+++ products/bam2/modules/samples/rest-api/rest-api.sh
@@ -1,24 +1,16 @@
 echo ""
 echo "Saving stream definition 1 with POST: "
 echo ""
-curl -k --user admin:admin https://localhost:9443/eventstreams/ --data @streamdefn1.json -H "Accept: application/json" -H "Content-type: application/json" -X POST
-echo ""
-echo "Loading stream definition 1: "
-echo ""
-curl -k --user admin:admin https://localhost:9443/eventstreams/org.wso2.bam.restapi/1.0.2 -X GET 
+curl -k --user admin:admin https://localhost:9443/datareceiver/1.0.0/streams/ --data @streamdefn1.json -H "Accept: application/json" -H "Content-type: application/json" -X POST
 echo ""
 echo "Posting events to stream definition 1: "
 echo ""
-curl -k --user admin:admin https://localhost:9443/eventstreams/org.wso2.bam.restapi/1.0.2 --data @events1.json -H "Accept: application/json" -H "Content-type: application/json" -X POST 
+curl -k --user admin:admin https://localhost:9443/datareceiver/1.0.0/stream/stockquote.stream/1.0.2/ --data @events1.json -H "Accept: application/json" -H "Content-type: application/json" -X POST 
 echo ""
 echo "Saving stream definition 2 with POST: "
 echo ""
-curl -k --user admin:admin https://localhost:9443/eventstreams/ --data @streamdefn2.json -H "Accept: application/json" -H "Content-type: application/json" -X POST
-echo ""
-echo "Loading All event streams: "
-echo ""
-curl -k --user admin:admin https://localhost:9443/eventstreams/ -X GET 
+curl -k --user admin:admin https://localhost:9443/datareceiver/1.0.0/streams/ --data @streamdefn2.json -H "Accept: application/json" -H "Content-type: application/json" -X POST
 echo ""
 echo "Posting events to stream definition 2: "
 echo ""
-curl -k --user admin:admin https://localhost:9443/eventstreams/org.wso2.bam.newrestapi/2.0.0 --data @events2.json -H "Accept: application/json" -H "Content-type: application/json" -X POST 
+curl -k --user admin:admin https://localhost:9443/datareceiver/1.0.0/stream/stockquote.stream/2.0.0 --data @events2.json -H "Accept: application/json" -H "Content-type: application/json" -X POST 

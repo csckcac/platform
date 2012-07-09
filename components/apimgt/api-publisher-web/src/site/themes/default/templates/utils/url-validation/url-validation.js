f286d0ebec2a81605fad749c6a383e92e1e857e1
lalaji <lalaji@a5903396-d722-0410-b921-86c7d4935375>
2012-07-09 12:51:03 +0000

    Update url-validate method
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@132283 a5903396-d722-0410-b921-86c7d4935375

--- components/apimgt/api-publisher-web/src/site/themes/default/templates/utils/url-validation/url-validation.js
+++ components/apimgt/api-publisher-web/src/site/themes/default/templates/utils/url-validation/url-validation.js
@@ -1,14 +1,18 @@
 var checkURLValid = function(url) {
+    if (url == '') {
+        jagg.message({content:"Enter a value for URI to check the connectivity.",type:"info"});
+        return;
+    }
     jagg.post("/site/blocks/item-add/ajax/add.jag", { action:"isURLValid", url:url },
               function (result) {
                   if (!result.error) {
                       if (result.response == "success") {
-                          jagg.message({content:"Successfully connected to the URI",type:"info"});
+                          jagg.message({content:"Successfully connected to the URI.",type:"info"});
 
                       } else {
-                          jagg.message({content:"Unable to establish a connection to the URI",type:"info"});
+                          jagg.message({content:"Unable to establish a connection to the URI.",type:"info"});
                       }
 
                   }
-              },"json");
+              }, "json");
 };

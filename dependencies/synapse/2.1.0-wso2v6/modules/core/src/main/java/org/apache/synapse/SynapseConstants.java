dfbbb4e8faaedd8d87bad0d3e45d220e84efbacb
isuruu <isuruu@a5903396-d722-0410-b921-86c7d4935375>
2012-07-09 06:22:20 +0000

    fixing ESBJAVA-1156
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@132209 a5903396-d722-0410-b921-86c7d4935375

--- dependencies/synapse/2.1.0-wso2v6/modules/core/src/main/java/org/apache/synapse/SynapseConstants.java
+++ dependencies/synapse/2.1.0-wso2v6/modules/core/src/main/java/org/apache/synapse/SynapseConstants.java
@@ -423,6 +423,8 @@ public final class SynapseConstants {
     public static final String FAIL_SAFE_MODE_EVENT_SOURCE = "eventsources";
     public static final String FAIL_SAFE_MODE_EXECUTORS = "executors";
     public static final String FAIL_SAFE_MODE_TEMPLATES = "executors";
+    public static final String FAIL_SAFE_MODE_MESSAGE_PROCESSORS = "messageprocessors";
+    public static final String FAIL_SAFE_MODE_MESSAGE_STORES = "messagestores";
 
     //fall back XPATH support (default javax.xml style xpath processing which can support XPATH 2.0)
     public static final String FAIL_OVER_DOM_XPATH_PROCESSING = "synapse.xpath.dom.failover.enabled";

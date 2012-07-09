5be8bafb6b2aabb185c25687aa9ea6ce14984599
tharindu <tharindu@a5903396-d722-0410-b921-86c7d4935375>
2012-07-09 17:28:39 +0000

    adding new rest api to build
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@132326 a5903396-d722-0410-b921-86c7d4935375

--- components/data-bridge/org.wso2.carbon.databridge.streamdefn.cassandra/src/main/java/org/wso2/carbon/databridge/streamdefn/cassandra/datastore/ClusterFactory.java
+++ components/data-bridge/org.wso2.carbon.databridge.streamdefn.cassandra/src/main/java/org/wso2/carbon/databridge/streamdefn/cassandra/datastore/ClusterFactory.java
@@ -3,24 +3,13 @@ package org.wso2.carbon.databridge.streamdefn.cassandra.datastore;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
-import me.prettyprint.cassandra.service.CassandraHostConfigurator;
 import me.prettyprint.hector.api.Cluster;
-import org.apache.axiom.om.OMElement;
-import org.apache.axiom.om.impl.builder.StAXOMBuilder;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.wso2.carbon.cassandra.dataaccess.ClusterConfiguration;
-import org.wso2.carbon.cassandra.dataaccess.ClusterConfigurationFactory;
 import org.wso2.carbon.cassandra.dataaccess.ClusterInformation;
-import org.wso2.carbon.cassandra.dataaccess.DataAccessComponentException;
 import org.wso2.carbon.databridge.commons.Credentials;
 import org.wso2.carbon.databridge.streamdefn.cassandra.internal.util.Utils;
-import org.wso2.carbon.utils.ServerConstants;
 
-import javax.xml.stream.XMLInputFactory;
-import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.XMLStreamReader;
-import java.io.*;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -105,78 +94,6 @@ public class ClusterFactory {
         connector.createColumnFamily(cluster, CassandraConnector.BAM_META_KEYSPACE,
                 CassandraConnector.BAM_META_STREAM_DEF_CF);
     }
-
-
-    private static final String CASSANDRA_COMPONENT_CONF = File.separator + "repository" + File.separator + "conf"
-            + File.separator + "etc" + File.separator + "cassandra-component.xml";
-
-
-
-    private static CassandraHostConfigurator createCassandraHostConfigurator() {
-
-        ClusterConfiguration configuration = ClusterConfigurationFactory.create(loadConfigXML());
-        String carbonCassandraRPCPort;
-        carbonCassandraRPCPort = System.getProperty("cassandra.rpc_port");
-        String cassandraHosts;
-        int cassandraDefaultPort = 0;
-
-        if (carbonCassandraRPCPort != null) {
-            cassandraHosts = LOCAL_HOST_NAME + ":" + carbonCassandraRPCPort;
-            cassandraDefaultPort = Integer.parseInt(carbonCassandraRPCPort);
-        } else {
-            cassandraHosts = configuration.getNodesString();
-        }
-        if (cassandraHosts == null || "".equals(cassandraHosts)) {
-            cassandraHosts = DEFAULT_HOST;
-        }
-
-        CassandraHostConfigurator configurator = new CassandraHostConfigurator(cassandraHosts);
-        configurator.setAutoDiscoverHosts(configuration.isAutoDiscovery());
-        configurator.setAutoDiscoveryDelayInSeconds(configuration.getAutoDiscoveryDelay());
-
-        if (cassandraDefaultPort > 0 && cassandraDefaultPort < 65536) {
-            configurator.setPort(cassandraDefaultPort);
-        } else {
-            configurator.setPort(configuration.getDefaultPort());
-        }
-        return configurator;
-    }
-
-
-    private static OMElement loadConfigXML() {
-
-        String carbonHome = System.getProperty(ServerConstants.CARBON_HOME);
-        String path = carbonHome + CASSANDRA_COMPONENT_CONF;
-        BufferedInputStream inputStream = null;
-        try {
-            File file = new File(path);
-            if (!file.exists()) {
-                log.info("There is no " + CASSANDRA_COMPONENT_CONF + ". Using the default configuration");
-                inputStream = new BufferedInputStream(
-                        new ByteArrayInputStream("<Cassandra/>".getBytes()));
-            } else {
-                inputStream = new BufferedInputStream(new FileInputStream(file));
-            }
-            XMLStreamReader parser = XMLInputFactory.newInstance().
-                    createXMLStreamReader(inputStream);
-            StAXOMBuilder builder = new StAXOMBuilder(parser);
-            return builder.getDocumentElement();
-        } catch (FileNotFoundException e) {
-            throw new DataAccessComponentException(CASSANDRA_COMPONENT_CONF + "cannot be found in the path : " + path, e, log);
-        } catch (XMLStreamException e) {
-            throw new DataAccessComponentException("Invalid XML for " + CASSANDRA_COMPONENT_CONF + " located in " +
-                    "the path : " + path, e, log);
-        } finally {
-            try {
-                if (inputStream != null) {
-                    inputStream.close();
-                }
-            } catch (IOException ingored) {
-                log.error("Cannot close input stream for : " + path);
-            }
-        }
-    }
-
 
     public static Cluster getCluster(Credentials credentials) {
         init();

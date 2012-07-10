3965961b0df70cf7bfca8d3c977aaa7c16e7f4e9
damitha <damitha@a5903396-d722-0410-b921-86c7d4935375>
2012-07-10 08:13:24 +0000

    hard coding the values for the m1 release
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@132400 a5903396-d722-0410-b921-86c7d4935375

--- build/tropos/php_cartridge/lamp/payload/debian_cron_script.sh
+++ build/tropos/php_cartridge/lamp/payload/debian_cron_script.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-tenant="TENANT"
-controller_ip="CONTROLLERIP"
-app_path="APPPATH"
-docroot="DOCUMENTROOT"
-duration=DURATION
 
-#tenant="foo"
-#controller_ip="172.15.0.1"
-#app_path="/opt/svc"
-#docroot="/var/www"
-#duration=1
+#tenant="TENANT"
+#controller_ip="CONTROLLERIP"
+#app_path="APPPATH"
+#docroot="DOCUMENTROOT"
+#duration=DURATION
+
+tenant=""
+controller_ip="172.15.0.1"
+app_path="/opt/wso2tropos-agent-1.0.0-SNAPSHOT/repository/deployment/server/phpapps"
+docroot="/var/www"
+duration=5
 
 pushd /opt/payload
 ./deploy_apps.sh $tenant $controller_ip $app_path $docroot $duration 

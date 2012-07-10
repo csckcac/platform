882b1ff81e9cc85f573421b6a11c7afaca93f423
damitha <damitha@a5903396-d722-0410-b921-86c7d4935375>
2012-07-10 07:13:14 +0000

    Removing the folder temporarily
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@132381 a5903396-d722-0410-b921-86c7d4935375

--- build/tropos/openstack/OpenStackInstaller-lxc-controller/wso2instancetype.sh
+++
@@ -1,1 +0,0 @@
-nova-manage flavor create --name=m1.dam --memory=128 --cpu=1 --root_gb=2 --ephemeral_gb=0 --flavor=6 --swap=0 --rxtx_factor=1
--- build/tropos/openstack/OpenStackInstaller-lxc-controller/wso2uploadimage.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-rm -rf /tmp/__upload
-
-#./upload_ubuntu.sh -a admin -p openstack -t demo -C 172.16.0.1 -x amd64 -y ubuntu -w 10.04 -z 
-#./upload_ubuntu.sh -a admin -p openstack -t demo -C 172.16.0.1 -x amd64 -y ubuntu -w 11.04 -z 
-./upload_ubuntu.sh -a admin -p openstack -t demo -C 172.15.0.1 -x amd64 -y ubuntu -w 12.04 -z ubuntu-12.04-server-cloudimg-amd64.tar.gz

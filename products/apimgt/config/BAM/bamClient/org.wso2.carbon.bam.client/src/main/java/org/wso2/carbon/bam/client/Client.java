002a7f7e9162ef861eefdadd5c3f2266926d0b7a
johann <johann@a5903396-d722-0410-b921-86c7d4935375>
2012-07-10 14:27:44 +0000

    Committing changes required for post M2 BAM
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@132456 a5903396-d722-0410-b921-86c7d4935375

--- products/apimgt/config/BAM/bamClient/org.wso2.carbon.bam.client/src/main/java/org/wso2/carbon/bam/client/Client.java
+++ products/apimgt/config/BAM/bamClient/org.wso2.carbon.bam.client/src/main/java/org/wso2/carbon/bam/client/Client.java
@@ -5,35 +5,23 @@ import org.apache.axis2.client.Options;
 import org.apache.axis2.client.ServiceClient;
 import org.apache.axis2.context.ServiceContext;
 import org.apache.axis2.transport.http.HTTPConstants;
+import org.wso2.carbon.analytics.hive.stub.HiveScriptStoreServiceHiveScriptStoreException;
+import org.wso2.carbon.analytics.hive.stub.HiveScriptStoreServiceStub;
 import org.wso2.carbon.authenticator.stub.AuthenticationAdminStub;
 import org.wso2.carbon.authenticator.stub.LoginAuthenticationExceptionException;
-import org.wso2.carbon.bam.analyzer.stub.AnalyzerAdminServiceAnalyzerException;
-import org.wso2.carbon.bam.analyzer.stub.AnalyzerAdminServiceStub;
-import org.wso2.carbon.bam.core.stub.ConnectionAdminServiceConfigurationException;
-import org.wso2.carbon.bam.core.stub.ConnectionAdminServiceStub;
-import org.wso2.carbon.bam.index.stub.IndexAdminServiceConfigurationException;
-import org.wso2.carbon.bam.index.stub.IndexAdminServiceIndexingException;
-import org.wso2.carbon.bam.index.stub.IndexAdminServiceStub;
-import org.wso2.carbon.bam.index.stub.service.types.IndexDTO;
 import org.wso2.carbon.utils.FileUtil;
-import org.wso2.carbon.utils.NetworkUtils;
 
-import javax.xml.stream.XMLStreamException;
 import java.io.IOException;
 
 public class Client {
 
-    private static String ANALYZER_ADMIN_SERVICE_URL = "AnalyzerAdminService";
-    private static String INDEX_ADMIN_SERVICE_URL = "IndexAdminService";
-    
-    private static AnalyzerAdminServiceStub analyzerAdminServiceStub;
-    private static IndexAdminServiceStub indexAdminServiceStub;
-    public static final String CONNECTION_ADMIN_SERVICE = "ConnectionAdminService";
+    private static String HIVE_SCRIPT_STORE_SERVICE = "HiveScriptStoreService";
+
+    private static HiveScriptStoreServiceStub hiveScriptStoreServiceStub;
 
 
     public static void main(String[] args)
-            throws IOException, AnalyzerAdminServiceAnalyzerException,
-                   IndexAdminServiceConfigurationException, IndexAdminServiceIndexingException,
+            throws IOException,
                    LoginAuthenticationExceptionException {
 
         String trustStore = System.getProperty("carbon.home") + "/repository/resources/security";
@@ -47,197 +35,51 @@ public class Client {
         Options options = client.getOptions();
         options.setManageSession(true);
 
-        authenticationAdminStub.login("admin", "admin", NetworkUtils.getLocalHostname());
+        authenticationAdminStub.login("admin", "admin", "localhost");
 
         ServiceContext serviceContext = authenticationAdminStub.
                 _getServiceClient().getLastOperationContext().getServiceContext();
         String sessionCookie = (String) serviceContext.getProperty(HTTPConstants.COOKIE_STRING);
 
+        String hiveScriptStoreServiceURL = getProperty("bamUrl") + HIVE_SCRIPT_STORE_SERVICE;
+        hiveScriptStoreServiceStub = new HiveScriptStoreServiceStub(hiveScriptStoreServiceURL);
 
-        String analyzerAdminServiceURL = getProperty("bamUrl") + ANALYZER_ADMIN_SERVICE_URL;
-        analyzerAdminServiceStub =  new AnalyzerAdminServiceStub(analyzerAdminServiceURL);
+        ServiceClient hiveScriptStoreServiceStubClient =  hiveScriptStoreServiceStub._getServiceClient();
+        Options  hiveScriptStoreServiceStubOption = hiveScriptStoreServiceStubClient.getOptions();
+        hiveScriptStoreServiceStubOption.setManageSession(true);
+        hiveScriptStoreServiceStubOption.setProperty(org.apache.axis2.transport.http.HTTPConstants.COOKIE_STRING, sessionCookie);
 
-        ServiceClient analyzerAdminServiceStubClient = analyzerAdminServiceStub._getServiceClient();
-        Options analyzerAdminServiceStubOption = analyzerAdminServiceStubClient.getOptions();
-        analyzerAdminServiceStubOption.setManageSession(true);
-        analyzerAdminServiceStubOption.setProperty(org.apache.axis2.transport.http.HTTPConstants.COOKIE_STRING, sessionCookie);
-        
-        String indexAdminServiceURL =  getProperty("bamUrl") + INDEX_ADMIN_SERVICE_URL;
-        indexAdminServiceStub = new IndexAdminServiceStub(indexAdminServiceURL);
-
-        ServiceClient indexAdminServiceStubClient =  indexAdminServiceStub._getServiceClient();
-        Options  indexAdminServiceStubOption = indexAdminServiceStubClient.getOptions();
-        indexAdminServiceStubOption.setManageSession(true);
-        indexAdminServiceStubOption.setProperty(org.apache.axis2.transport.http.HTTPConstants.COOKIE_STRING, sessionCookie);
-
-        String ConnectionMgtServiceURL = getProperty("bamUrl") + CONNECTION_ADMIN_SERVICE;
-        ConnectionAdminServiceStub connectionAdminServiceStub = new ConnectionAdminServiceStub(ConnectionMgtServiceURL);
-
-        ServiceClient connectionAdminServiceStubClient =  connectionAdminServiceStub._getServiceClient();
-        Options  connectionAdminServiceStubOption = connectionAdminServiceStubClient.getOptions();
-        connectionAdminServiceStubOption.setManageSession(true);
-        connectionAdminServiceStubOption.setProperty(org.apache.axis2.transport.http.HTTPConstants.COOKIE_STRING, sessionCookie);
-
-
-        String classAnalyzerContent = FileUtil.readFileToString(System.getProperty("configFilePath") + "ClassAnalyzer.xml");
-        String apiVersionUsageSummarySequenceContent = FileUtil.readFileToString( System.getProperty("configFilePath") +
-                                                                             "APIVersionUsageSummarySequence.xml");
-        String apiVersionKeyUsageSummarySequenceContent = FileUtil.readFileToString( System.getProperty("configFilePath") +
-                                                                             "APIVersionKeyUsageSummarySequence.xml");
-        String apiVersionKeyLastAccessSummarySequenceContent = FileUtil.readFileToString( System.getProperty("configFilePath") +
-                                                                             "APIVersionKeyLastAccessSummarySequence.xml");
-        String apiVersionServiceTimeSummarySequenceContent = FileUtil.readFileToString( System.getProperty("configFilePath") +
-                                                                             "APIVersionServiceTimeSummarySequence.xml");
-        String keyUsageSummarySequenceContent = FileUtil.readFileToString( System.getProperty("configFilePath") +
-                                                                             "KeyUsageSummarySequence.xml");
+        String apiVersionUsageSummaryContent = FileUtil.readFileToString(System.getProperty("configFilePath") +
+                                                                         "APIVersionUsageSummaryScript.hiveql");
+        String apiVersionKeyUsageSummaryContent = FileUtil.readFileToString( System.getProperty("configFilePath") +
+                                                                             "APIVersionKeyUsageSummaryScript.hiveql");
+        String apiVersionKeyLastAccessSummaryContent = FileUtil.readFileToString( System.getProperty("configFilePath") +
+                                                                             "APIVersionKeyLastAccessSummaryScript.hiveql");
+        String apiVersionServiceTimeSummaryContent = FileUtil.readFileToString( System.getProperty("configFilePath") +
+                                                                             "APIVersionServiceTimeSummaryScript.hiveql");
+        String keyUsageSummaryContent = FileUtil.readFileToString( System.getProperty("configFilePath") +
+                                                                             "KeyUsageSummaryScript.hiveql");
 
 
 
         try {
 
-            connectionAdminServiceStub.configureConnectionParameters("admin","admin");
-
-            System.out.println("Connection parameters have been added");
-
-            APIMgtUsageBAMDataPublisher apiMgtUsageBAMDataPublisher = new APIMgtUsageBAMDataPublisher();
-
-            apiMgtUsageBAMDataPublisher.publishEvent();
-
-            System.out.println("Adding ClassAnalyzer");
-
-            analyzerAdminServiceStub.addTask(classAnalyzerContent);
-
-            System.out.println("Waiting 1:30 min until analyzer creates the BASEEnriched CF.....");
-
-            //Wait for 1:30 min
-            Thread.sleep(1000*60 + 30000);
-
-            System.out.println("Adding APIVersionUsageSummarySequence Analyzer");
-
-            analyzerAdminServiceStub.addTask(apiVersionUsageSummarySequenceContent);
-
-            System.out.println("Waiting 1:30 min until analyzer creates the APIVersionUsageSummaryTable CF.......");
-
-            //Wait for 1:30 min
-            Thread.sleep(1000*60 + 30000);
-
-            System.out.println("Adding APIVersionKeyUsageSummarySequence Analyzer");
-
-            analyzerAdminServiceStub.addTask(apiVersionKeyUsageSummarySequenceContent);
-
-            System.out.println("Waiting 1:30 min until analyzer creates the APIVersionKeyUsageSummaryTable CF.......");
-
-            //Wait for 1:30 min
-            Thread.sleep(1000*60 + 30000);
-
-            System.out.println("Adding APIVersionKeyLastAccessSummarySequence Analyzer");
-
-            analyzerAdminServiceStub.addTask(apiVersionKeyLastAccessSummarySequenceContent);
-
-            System.out.println("Waiting 1:30 min until analyzer creates the APIVersionKeyLastAccessSummaryTable CF.......");
-
-            //Wait for 1:30 min
-            Thread.sleep(1000*60 + 30000);
-
-            System.out.println("Adding APIVersionServiceTimeSummarySequence Analyzer");
-
-            analyzerAdminServiceStub.addTask(apiVersionServiceTimeSummarySequenceContent);
-
-            System.out.println("Waiting 1:30 min until analyzer creates the APIVersionServiceTimeSummaryTable CF.......");
-
-            //Wait for 1:30 min
-            Thread.sleep(1000*60 + 30000);
-
-            System.out.println("Adding KeyUsageSummarySequence Analyzer");
-
-            analyzerAdminServiceStub.addTask(keyUsageSummarySequenceContent);
-
-            System.out.println("Waiting 1:30 min until analyzer creates the KeyUsageSummaryTable CF.......");
-
-            //Wait for 1:30 min
-            Thread.sleep(1000*60 + 30000);
-
-            System.out.println("Creating APIVersionUsageSummaryTableIndex ...");
-
-            IndexDTO index = new IndexDTO();
-            index.setIndexName("APIVersionUsageSummaryTableIndex");
-            index.setIndexedTable("APIVersionUsageSummaryTable");
-            index.setDataSourceType("CASSANDRA");
-            String[] indexColumns = {"api"};
-            index.setIndexedColumns(indexColumns);
-            index.setCron("1 * * * * ? *");
-            index.setGranularity(null);
-
-            indexAdminServiceStub.createIndex(index);
-
-            System.out.println("Creating APIVersionKeyUsageSummaryTableIndex ...");
-
-            index = new IndexDTO();
-            index.setIndexName("APIVersionKeyUsageSummaryTableIndex");
-            index.setIndexedTable("APIVersionKeyUsageSummaryTable");
-            index.setDataSourceType("CASSANDRA");
-            indexColumns[0] = "api";
-            index.setIndexedColumns(indexColumns);
-            index.setCron("1 * * * * ? *");
-            index.setGranularity(null);
-
-            indexAdminServiceStub.createIndex(index);
-
-            System.out.println("Creating APIVersionKeyLastAccessSummaryTableIndex ...");
-
-            index = new IndexDTO();
-            index.setIndexName("APIVersionKeyLastAccessSummaryTableIndex");
-            index.setIndexedTable("APIVersionKeyLastAccessSummaryTable");
-            index.setDataSourceType("CASSANDRA");
-            indexColumns[0] = "api_version";
-            index.setIndexedColumns(indexColumns);
-            index.setCron("1 * * * * ? *");
-            index.setGranularity(null);
-
-            indexAdminServiceStub.createIndex(index);
-
-            System.out.println("Creating APIVersionServiceTimeSummaryTableIndex ...");
-
-            index = new IndexDTO();
-            index.setIndexName("APIVersionServiceTimeSummaryTableIndex");
-            index.setIndexedTable("APIVersionServiceTimeSummaryTable");
-            index.setDataSourceType("CASSANDRA");
-            indexColumns[0] = "api_version";
-            index.setIndexedColumns(indexColumns);
-            index.setCron("1 * * * * ? *");
-            index.setGranularity(null);
-
-            indexAdminServiceStub.createIndex(index);
-
-            System.out.println("Creating KeyUsageSummaryTableIndex ...");
-
-            index = new IndexDTO();
-            index.setIndexName("KeyUsageSummaryTableIndex");
-            index.setIndexedTable("KeyUsageSummaryTable");
-            index.setDataSourceType("CASSANDRA");
-            indexColumns[0] = "consumerKey";
-            index.setIndexedColumns(indexColumns);
-            index.setCron("1 * * * * ? *");
-            index.setGranularity(null);
-
-            indexAdminServiceStub.createIndex(index);
-
-            System.out.println("BAM configured successfully for collecting API stats");
-
+            hiveScriptStoreServiceStub.saveHiveScript("APIVersionUsageSummaryScript",apiVersionUsageSummaryContent,"1 * * * * ? *");
+            hiveScriptStoreServiceStub.saveHiveScript("APIVersionKeyUsageSummaryScript",apiVersionKeyUsageSummaryContent,"1 * * * * ? *");
+            hiveScriptStoreServiceStub.saveHiveScript("APIVersionKeyLastAccessSummaryScript",apiVersionKeyLastAccessSummaryContent,"1 * * * * ? *");
+            hiveScriptStoreServiceStub.saveHiveScript("APIVersionServiceTimeSummaryScript",apiVersionServiceTimeSummaryContent,"1 * * * * ? *");
+            hiveScriptStoreServiceStub.saveHiveScript("KeyUsageSummaryScript",keyUsageSummaryContent,"1 * * * * ? *");
             return;
 
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (XMLStreamException e) {
-            e.printStackTrace();
-        } catch (ConnectionAdminServiceConfigurationException e) {
+        }  catch (HiveScriptStoreServiceHiveScriptStoreException e) {
             e.printStackTrace();
         }
+
 
     }
 
     private static String getProperty(String bamUrl) {
-        String defaultVal = "https://localhost:9444/services/";
+        String defaultVal = "https://localhost:9443/services/";
         String result = System.getProperty(bamUrl);
         if (result == null || result.length() == 0) {
             result = defaultVal;

54ccdbd8228560c9d2ac59886c98c88444ca477c
achala <achala@a5903396-d722-0410-b921-86c7d4935375>
2012-07-10 05:49:41 +0000

    Call the bpel back after the artifact storage
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@132366 a5903396-d722-0410-b921-86c7d4935375

--- components/appfactory/org.wso2.carbon.appfactory.core/src/main/java/org/wso2/carbon/appfactory/core/build/DefaultBuildDriverListener.java
+++ components/appfactory/org.wso2.carbon.appfactory.core/src/main/java/org/wso2/carbon/appfactory/core/build/DefaultBuildDriverListener.java
@@ -1,31 +1,76 @@
 package org.wso2.carbon.appfactory.core.build;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 
+import org.apache.axiom.om.OMElement;
+import org.apache.axiom.om.impl.builder.StAXOMBuilder;
+import org.apache.axis2.AxisFault;
+import org.apache.axis2.addressing.EndpointReference;
+import org.apache.axis2.client.ServiceClient;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+
 import org.wso2.carbon.appfactory.common.AppFactoryException;
 import org.wso2.carbon.appfactory.core.ArtifactStorage;
 import org.wso2.carbon.appfactory.core.BuildDriverListener;
 import org.wso2.carbon.appfactory.core.internal.ServiceHolder;
 
+import javax.xml.stream.XMLStreamException;
+
 
 public class DefaultBuildDriverListener implements BuildDriverListener {
 
-	private static final Log log = LogFactory.getLog(DefaultBuildDriverListener.class);
-	
-	@Override
+    private static final Log log = LogFactory.getLog(DefaultBuildDriverListener.class);
+    private static final String EPR = "http://localhost:9763/services/ArtifactCreateCallbackService";
+
+    @Override
     public void onBuildSuccessful(String applicationId, String version, String revision, File file) {
         ArtifactStorage storage = ServiceHolder.getArtifactStorage();
-	    storage.storeArtifact(applicationId, version, revision, file);
-	    // TODO call the bpel back
+        storage.storeArtifact(applicationId, version, revision, file);
+
+        // call the bpel back
+        sendMessageToCreateArtifactCallback(applicationId, version, revision);
+
     }
 
-	@Override
+    @Override
     public void onBuildFailure(String applicationId, String version, String revision, File file)
-                                                                                                throws AppFactoryException {
-		
-	    
+            throws AppFactoryException {
+
+
     }
 
+    public void sendMessageToCreateArtifactCallback(final String applicationId, final String version, final String revision) {
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException ignored) {
+                }
+                try {
+                    //Create a service client
+                    ServiceClient client = new ServiceClient();
+
+                    //Set the endpoint address
+                    client.getOptions().setTo(new EndpointReference(EPR));
+
+                    //Make the request and get the response
+                    client.sendRobust(getPayload(applicationId, version, revision));
+                } catch (AxisFault e) {
+                    log.error(e);
+                    e.printStackTrace();
+                } catch (XMLStreamException e) {
+                    log.error(e);
+                }
+            }
+        }).start();
+    }
+
+    private static OMElement getPayload(String applicationId, String version, String revision) throws XMLStreamException, javax.xml.stream.XMLStreamException {
+        String payload = "<p:sendCreateArtifactMessage xmlns:p=\"http://www.example.org/CreateArtifactCallback/\"><applicationId>" + applicationId +
+                "</applicationId><revision>" + revision + "</revision><version>" + version + "</version></p:sendCreateArtifactMessage>";
+
+        return new StAXOMBuilder(new ByteArrayInputStream(payload.getBytes())).getDocumentElement();
+    }
 }

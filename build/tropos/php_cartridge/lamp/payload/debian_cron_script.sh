8e4a47b51fefd7a2f9413e2b1c40ec07711e0502
damitha <damitha@a5903396-d722-0410-b921-86c7d4935375>
2012-07-11 14:55:01 +0000

    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@132636 a5903396-d722-0410-b921-86c7d4935375

--- build/tropos/php_cartridge/lamp/payload/debian_cron_script.sh
+++ build/tropos/php_cartridge/lamp/payload/debian_cron_script.sh
@@ -6,7 +6,7 @@
 #docroot="DOCUMENTROOT"
 #duration=DURATION
 
-tenant=""
+tenant="foo"
 controller_ip="172.15.0.1"
 app_path="/opt/wso2tropos-agent-1.0.0-SNAPSHOT/repository/deployment/server/phpapps"
 docroot="/var/www"

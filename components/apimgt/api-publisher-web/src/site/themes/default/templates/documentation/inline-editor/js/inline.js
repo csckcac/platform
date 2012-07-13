0aaeab3f93fdf0d30c52f47d54a34f1d60afe723
lalaji <lalaji@a5903396-d722-0410-b921-86c7d4935375>
2012-07-13 10:30:43 +0000

    Fixed APISTORE-471
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@132908 a5903396-d722-0410-b921-86c7d4935375

--- components/apimgt/api-publisher-web/src/site/themes/default/templates/documentation/inline-editor/js/inline.js
+++ components/apimgt/api-publisher-web/src/site/themes/default/templates/documentation/inline-editor/js/inline.js
@@ -7,7 +7,6 @@ $(document).ready(function() {
                      theme_advanced_buttons3 : "insertdate,inserttime,|,spellchecker,advhr,,removeformat,|,sub,sup,|,charmap,emotions",
                      theme_advanced_toolbar_location : "top",
                      theme_advanced_toolbar_align : "left",
-                     theme_advanced_statusbar_location : "bottom",
                      theme_advanced_resizing : true
 
                  });

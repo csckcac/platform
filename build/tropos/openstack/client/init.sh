5b94dbc1a72f8e72f59d8f52885c47f14f79aa1e
damitha <damitha@a5903396-d722-0410-b921-86c7d4935375>
2012-07-13 08:59:24 +0000

    Add some test scripts that update environment and create instances
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@132886 a5903396-d722-0410-b921-86c7d4935375

---
+++ build/tropos/openstack/client/init.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+rm ./demorc
+rm ./demo.pem
+cp -f ../OpenStackInstaller-lxc-controller/demorc ./
+source ./demorc
+euca-add-keypair demo > demo.pem
+chmod 0600 demo.pem
+
+euca-authorize default -P tcp -p 22 -s 0.0.0.0/0
+euca-authorize default -P tcp -p 80 -s 0.0.0.0/0
+euca-authorize default -P tcp -p 8080 -s 0.0.0.0/0
+euca-authorize default -P icmp -t -1:-1

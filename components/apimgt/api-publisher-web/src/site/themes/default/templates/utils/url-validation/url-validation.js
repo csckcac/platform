21baddf9a32667aa59c1d8986d8c2d28816e6578
chanaka <chanaka@a5903396-d722-0410-b921-86c7d4935375>
2012-07-17 05:13:51 +0000

    Fade the url validation messages in a time period
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@133178 a5903396-d722-0410-b921-86c7d4935375

--- components/apimgt/api-publisher-web/src/site/themes/default/templates/utils/url-validation/url-validation.js
+++ components/apimgt/api-publisher-web/src/site/themes/default/templates/utils/url-validation/url-validation.js
@@ -1,22 +1,29 @@
+
 var checkURLValid = function(url,btn) {
     if($(btn).next().get(0).nodeName == "SPAN"){
         $(btn).next().remove();
     }
     if (url == '') {
         $(btn).after(' <span class="label label-important"><i class="icon-remove icon-white"></i> Invalid</span>');
+        var toFade = $(btn).next();
+        var foo = setTimeout(function(){$(toFade).hide()},3000);
         return;
     }
 
     jagg.post("/site/blocks/item-add/ajax/add.jag", { action:"isURLValid", url:url },
               function (result) {
                   if (!result.error) {
+
                       if (result.response == "success") {
                           $(btn).after(' <span class="label label-success"><i class="icon-ok icon-white"></i> Valid</span>');
 
                       } else {
                           $(btn).after(' <span class="label label-important"><i class="icon-remove icon-white"></i> Invalid</span>');
                       }
+                      var toFade = $(btn).next();
+                      var foo = setTimeout(function(){$(toFade).hide()},3000);
 
                   }
               }, "json");
 };
+

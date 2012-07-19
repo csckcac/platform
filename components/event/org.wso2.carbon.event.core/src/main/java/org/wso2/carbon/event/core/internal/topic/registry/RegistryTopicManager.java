184644b515fc181abefab2c50edf1e6169f6220a
subash <subash@a5903396-d722-0410-b921-86c7d4935375>
2012-07-19 09:59:50 +0000

    Fix for REGISTRY-987
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@133607 a5903396-d722-0410-b921-86c7d4935375

--- components/event/org.wso2.carbon.event.core/src/main/java/org/wso2/carbon/event/core/internal/topic/registry/RegistryTopicManager.java
+++ components/event/org.wso2.carbon.event.core/src/main/java/org/wso2/carbon/event/core/internal/topic/registry/RegistryTopicManager.java
@@ -346,8 +346,7 @@ public class RegistryTopicManager implements TopicManager {
     }
 
     private boolean validateTopicName(String topicName) {
-        String topicNameRegex = "([a-zA-Z0-9_\\-/#*.?&\\s])+";
-        return Pattern.matches(topicNameRegex, topicName);
+      return Pattern.matches("[[a-zA-Z]+[^(\\x00-\\x80)]+[0-9_\\-/#*.?&\\s]+]+",topicName);
     }
 
     /**

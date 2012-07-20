085595453901b1aa182a648519c54842e2013ccb
damitha <damitha@a5903396-d722-0410-b921-86c7d4935375>
2012-07-20 13:34:38 +0000

    Updating documentation
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@134027 a5903396-d722-0410-b921-86c7d4935375

--- build/tropos/openstack/openStack-lxc-ubuntu-precise/uploadimage.sh
+++ build/tropos/openstack/openStack-lxc-ubuntu-precise/uploadimage.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm -rf /tmp/__upload
 
-./wso2imageupload.sh -a admin -p openstack -t demo -C 172.15.0.1 -x amd64 -y ubuntu -w 12.04 -z ubuntu-12.04-server-cloudimg-amd64.tar.gz
+./wso2imageupload.sh -a admin -p openstack -t demo -C 172.16.0.1 -x amd64 -y ubuntu -w 12.04 -z ubuntu-12.04-server-cloudimg-amd64.tar.gz

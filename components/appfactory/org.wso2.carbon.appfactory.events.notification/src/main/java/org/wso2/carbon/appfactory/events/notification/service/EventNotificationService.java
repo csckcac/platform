d689c9655689ae716896e977a9d9ba9a3bb82a0f
ashansa <ashansa@a5903396-d722-0410-b921-86c7d4935375>
2012-07-23 10:48:56 +0000

    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@134282 a5903396-d722-0410-b921-86c7d4935375

--- components/appfactory/org.wso2.carbon.appfactory.events.notification/src/main/java/org/wso2/carbon/appfactory/events/notification/service/EventNotificationService.java
+++ components/appfactory/org.wso2.carbon.appfactory.events.notification/src/main/java/org/wso2/carbon/appfactory/events/notification/service/EventNotificationService.java
@@ -17,11 +17,9 @@
 package org.wso2.carbon.appfactory.events.notification.service;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonObject;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.wso2.carbon.appfactory.application.mgt.service.ApplicationManagementException;
-import org.wso2.carbon.appfactory.events.notification.Constants;
 import org.wso2.carbon.appfactory.events.notification.internal.AppFactoryEventNotificationComponent;
 import org.wso2.carbon.appfactory.events.notification.internal.EventRepository;
 import org.wso2.carbon.core.AbstractAdmin;
@@ -34,19 +32,31 @@ public class EventNotificationService extends AbstractAdmin{
     private static Log log = LogFactory.getLog(EventNotificationService.class);
     private Gson gson = new Gson();
     
-    public void publishEvent(JsonObject eventJson) {
-        EventRepository.getInstance().addEvent(gson.fromJson(eventJson,EventBean.class));
+    public void publishEvent(EventBean event) {
+        EventRepository.getInstance().addEvent(event);
     }
-    
-    public JsonObject[] getEventsForApplications(String[] appIDs, String userName) {
-        ArrayList<JsonObject> events = new ArrayList<JsonObject>();
+
+    public String[] getEventsForApplications(String[] appIDs, String userName) {
+        ArrayList<String> events = new ArrayList<String>();
         ArrayList userApps = getAppsOfUser(userName);
         for(String appID : appIDs) {
             if(userApps.contains(appID)) {
                 events.addAll(getEventsForID(appID));
             }
         }
-        return events.toArray(new JsonObject[events.size()]);
+        return events.toArray(new String[events.size()]);
+    }
+
+    private ArrayList<String> getEventsForID(String appID) {
+        ArrayList<String> events = new ArrayList<String>();
+        Iterator iterator = EventRepository.getInstance().getEventBuffer().iterator();
+        while(iterator.hasNext()) {
+            EventBean eventBean = (EventBean)iterator.next();
+            if(appID.equals(eventBean.getApplicationId())) {
+                events.add(gson.toJson(eventBean));
+            }
+        }
+        return events;
     }
 
     private ArrayList getAppsOfUser(String userName) {
@@ -60,25 +70,5 @@ public class EventNotificationService extends AbstractAdmin{
             log.error("Error while retrieving the application of user "+userName);
         }
         return appList;
-    }
-
-    private ArrayList<JsonObject> getEventsForID(String appID) {
-        ArrayList<JsonObject> events = new ArrayList<JsonObject>();
-        Iterator iterator = EventRepository.getInstance().getEventBuffer().iterator();
-        while(iterator.hasNext()) {
-            EventBean eventBean = (EventBean)iterator.next();
-            if(appID.equals(eventBean.getApplicationId())) {
-                events.add(getJsonObject(eventBean));
-            }
-        }
-        return events;
-    }
-
-    private JsonObject getJsonObject(EventBean eventBean) {
-        JsonObject eventJson = new JsonObject();
-        eventJson.addProperty(Constants.APPLICATION_ID,eventBean.getApplicationId());
-        eventJson.addProperty(Constants.EVENT,eventBean.getEvent());
-        eventJson.addProperty(Constants.RESULT,eventBean.getResult());
-        return eventJson;
     }
 }

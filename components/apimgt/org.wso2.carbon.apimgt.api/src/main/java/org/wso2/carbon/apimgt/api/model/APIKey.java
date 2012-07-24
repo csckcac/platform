282bd19a752eccc4762166069bd7fd546a592b77
hiranya <hiranya@a5903396-d722-0410-b921-86c7d4935375>
2012-07-24 06:34:15 +0000

    API updated to support consumer keys and secrets
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@134430 a5903396-d722-0410-b921-86c7d4935375

--- components/apimgt/org.wso2.carbon.apimgt.api/src/main/java/org/wso2/carbon/apimgt/api/model/APIKey.java
+++ components/apimgt/org.wso2.carbon.apimgt.api/src/main/java/org/wso2/carbon/apimgt/api/model/APIKey.java
@@ -18,15 +18,33 @@ package org.wso2.carbon.apimgt.api.model;
 
 public class APIKey {
     
-    private String key;
+    private String consumerKey;
+    private String consumerSecret;
+    private String accessToken;
     private String type;
 
-    public String getKey() {
-        return key;
+    public String getConsumerKey() {
+        return consumerKey;
     }
 
-    public void setKey(String key) {
-        this.key = key;
+    public void setConsumerKey(String consumerKey) {
+        this.consumerKey = consumerKey;
+    }
+
+    public String getConsumerSecret() {
+        return consumerSecret;
+    }
+
+    public void setConsumerSecret(String consumerSecret) {
+        this.consumerSecret = consumerSecret;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
     }
 
     public String getType() {

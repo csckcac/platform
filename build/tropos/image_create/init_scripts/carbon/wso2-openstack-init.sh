c8c8e9fc14e07193bb6ca8cd3269a267c891106a
damitha <damitha@a5903396-d722-0410-b921-86c7d4935375>
2012-07-24 04:23:27 +0000

    deleting the zip file after extracting
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@134397 a5903396-d722-0410-b921-86c7d4935375

--- build/tropos/image_create/init_scripts/carbon/wso2-openstack-init.sh
+++ build/tropos/image_create/init_scripts/carbon/wso2-openstack-init.sh
@@ -91,6 +91,7 @@ done
 echo "scp -i ${instance_path}/payload/wso2-key root@${CONTROLLER_IP}:/opt/${PRODUCT_NAME}.zip /opt/" >> $LOG
 scp -i ${instance_path}/payload/wso2-key root@${CONTROLLER_IP}:/opt/${PRODUCT_NAME}.zip /opt/
 unzip /opt/${PRODUCT_NAME}.zip -d /opt/
+rm -f /opt/${PRODUCT_NAME}.zip
 
 if [ "$ADMIN_USERNAME" = "" ]; then
 	echo Launching with default admin username >> $LOG

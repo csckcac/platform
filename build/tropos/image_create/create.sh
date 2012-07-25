9ec6b66c86144466ea603ab3fb38152499d276ae
damitha <damitha@a5903396-d722-0410-b921-86c7d4935375>
2012-07-25 05:50:35 +0000

    copy the jdk into image as /opt/java instead of a versioned name
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@134706 a5903396-d722-0410-b921-86c7d4935375

--- build/tropos/image_create/create.sh
+++ build/tropos/image_create/create.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-./customize_image.sh image-id=php1 image-root=/opt/lxc template=lamp original-image=ubuntu-12.04-server-cloudimg-amd64.tar.gz 
+./customize_image.sh image-id=php2 image-root=/opt/lxc template=lamp original-image=ubuntu-12.04-server-cloudimg-amd64.tar.gz 
 
-#./customize_image.sh image-id=carbon1 image-root=/opt/lxc template=wso2_carbon_server original-image=ubuntu-12.04-server-cloudimg-amd64.tar.gz software=/opt/jdk1.6.0_24/
+#./customize_image.sh image-id=carbon3 image-root=/opt/lxc template=wso2_carbon_server original-image=ubuntu-12.04-server-cloudimg-amd64.tar.gz software=/opt/java
 

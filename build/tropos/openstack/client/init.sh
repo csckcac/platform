cd6fb52b1085810ecdf98acac590b61d02739b04
damitha <damitha@a5903396-d722-0410-b921-86c7d4935375>
2012-07-25 06:27:04 +0000

    Adding carbon server ports
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@134730 a5903396-d722-0410-b921-86c7d4935375

--- build/tropos/openstack/client/init.sh
+++ build/tropos/openstack/client/init.sh
@@ -9,4 +9,10 @@ chmod 0600 demo.pem
 euca-authorize default -P tcp -p 22 -s 0.0.0.0/0
 euca-authorize default -P tcp -p 80 -s 0.0.0.0/0
 euca-authorize default -P tcp -p 8080 -s 0.0.0.0/0
+euca-authorize default -P tcp -p 9763 -s 0.0.0.0/0
+euca-authorize default -P tcp -p 9764 -s 0.0.0.0/0
+euca-authorize default -P tcp -p 9765 -s 0.0.0.0/0
+euca-authorize default -P tcp -p 9443 -s 0.0.0.0/0
+euca-authorize default -P tcp -p 9444 -s 0.0.0.0/0
+euca-authorize default -P tcp -p 9445 -s 0.0.0.0/0
 euca-authorize default -P icmp -t -1:-1

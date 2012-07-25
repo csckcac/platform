cc5e54c7bf4e09ec74ed029754256acf86419c35
dushan <dushan@a5903396-d722-0410-b921-86c7d4935375>
2012-07-25 04:43:22 +0000

    revert back SOAP 11  fault actor handle logic
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@134667 a5903396-d722-0410-b921-86c7d4935375

--- components/mediators/fault/org.wso2.carbon.mediator.fault.ui/src/main/java/org/wso2/carbon/mediator/fault/FaultMediator.java
+++ components/mediators/fault/org.wso2.carbon.mediator.fault.ui/src/main/java/org/wso2/carbon/mediator/fault/FaultMediator.java
@@ -271,7 +271,7 @@ public class FaultMediator extends AbstractMediator {
             node.setText(faultNode.toString());
         }
 
-        if (faultRole != null && soapVersion != POX && soapVersion !=SOAP11) {
+        if (faultRole != null && soapVersion != POX) {
             OMElement role = fac.createOMElement("role", synNS, fault);
             role.setText(faultRole.toString());
         }

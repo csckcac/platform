328126c657fd7e61bb6b7b2b4c1d40338884823a
krishantha <krishantha@a5903396-d722-0410-b921-86c7d4935375>
2012-07-25 16:13:44 +0000

    Malinga's patch for service filtering
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@134894 a5903396-d722-0410-b921-86c7d4935375

---
+++ products/greg/modules/integration/registry/tests-new/src/test/java/org/wso2/carbon/registry/governance/api/test/FilterServicesWithAdminTestCase.java
@@ -0,0 +1,262 @@
+/*
+ * Copyright 2004,2005 The Apache Software Foundation.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.wso2.carbon.registry.governance.api.test;
+
+import java.rmi.RemoteException;
+
+import javax.xml.namespace.QName;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import org.wso2.carbon.authenticator.stub.LoginAuthenticationExceptionException;
+import org.wso2.carbon.automation.api.clients.registry.ResourceAdminServiceClient;
+import org.wso2.carbon.automation.core.ProductConstant;
+import org.wso2.carbon.automation.core.utils.UserInfo;
+import org.wso2.carbon.automation.core.utils.UserListCsvReader;
+import org.wso2.carbon.automation.core.utils.environmentutils.EnvironmentBuilder;
+import org.wso2.carbon.automation.core.utils.environmentutils.ManageEnvironment;
+import org.wso2.carbon.automation.utils.registry.RegistryProviderUtil;
+import org.wso2.carbon.governance.api.exception.GovernanceException;
+import org.wso2.carbon.governance.api.services.ServiceFilter;
+import org.wso2.carbon.governance.api.services.ServiceManager;
+import org.wso2.carbon.governance.api.services.dataobjects.Service;
+import org.wso2.carbon.registry.core.Registry;
+import org.wso2.carbon.registry.core.exceptions.RegistryException;
+import org.wso2.carbon.registry.ws.client.registry.WSRegistryServiceClient;
+
+public class FilterServicesWithAdminTestCase {
+    private Registry governance;
+    int userId = 0;
+    Service newService;
+    private final static String WSDL_URL =
+            "https://svn.wso2.org/repos/wso2/carbon/platform/trunk/"
+            + "platform-integration/clarity-tests/org.wso2.carbon.automation.test.repo/"
+            + "src/main/resources/artifacts/GREG/wsdl/info.wsdl";
+    ServiceManager serviceManager;
+    ResourceAdminServiceClient resourceAdminServiceClient;
+    Service serviceForSearching1, serviceForSearching2, serviceForSearching3, serviceForSearching4,
+            serviceForSearching5;
+
+
+    @BeforeClass()
+    public void initialize() throws RemoteException, LoginAuthenticationExceptionException,
+                                    RegistryException {
+        UserInfo userInfo = UserListCsvReader.getUserInfo(userId);
+        WSRegistryServiceClient wsRegistry =
+                new RegistryProviderUtil().getWSRegistry(userId,
+                                                         ProductConstant.GREG_SERVER_NAME);
+        governance = new RegistryProviderUtil().getGovernanceRegistry(wsRegistry, userId);
+        serviceManager = new ServiceManager(governance);
+        EnvironmentBuilder builder = new EnvironmentBuilder().greg(userId);
+        ManageEnvironment environment = builder.build();
+
+        resourceAdminServiceClient =
+                new ResourceAdminServiceClient(environment.getGreg()
+                                                       .getBackEndUrl(),
+                                               userInfo.getUserName(),
+                                               userInfo.getPassword());
+        serviceForSearching1 =
+                serviceManager.newService(new QName(
+                        "http://service.for.searching1/mnm/",
+                        "serviceForSearching1"));
+        serviceForSearching1.addAttribute("overview_version", "3.0.0");
+        serviceForSearching1.addAttribute("overview_description", "Test");
+        serviceForSearching1.addAttribute("interface_wsdlUrl", WSDL_URL);
+        serviceForSearching1.addAttribute("docLinks_documentType", "test");
+        serviceForSearching1.addAttribute("interface_messageFormats", "SOAP 1.2");
+        serviceForSearching1.addAttribute("interface_messageExchangePatterns", "Request Response");
+        serviceForSearching1.addAttribute("security_authenticationPlatform ", "XTS-WS TRUST");
+        serviceForSearching1.addAttribute("security_authenticationMechanism", "InfoCard");
+        serviceForSearching1.addAttribute("security_messageIntegrity", "WS-Security");
+        serviceForSearching1.addAttribute("security_messageEncryption", "WS-Security");
+        serviceManager.addService(serviceForSearching1);
+
+        serviceForSearching2 =
+                serviceManager.newService(new QName(
+                        "http://service.for.searching2/mnm/",
+                        "serviceForSearching2"));
+        serviceForSearching2.addAttribute("overview_version", "4.0.0");
+        serviceForSearching2.addAttribute("overview_description", "Test");
+        serviceForSearching2.addAttribute("interface_wsdlUrl", WSDL_URL);
+        serviceForSearching2.addAttribute("docLinks_documentType", "test");
+        serviceForSearching2.addAttribute("interface_messageFormats", "SOAP 1.2");
+        serviceForSearching2.addAttribute("interface_messageExchangePatterns", "Request Response");
+        serviceForSearching2.addAttribute("security_authenticationPlatform ", "XTS-WS TRUST");
+        serviceForSearching2.addAttribute("security_authenticationMechanism", "InfoCard");
+        serviceForSearching2.addAttribute("security_messageIntegrity", "WS-Security");
+        serviceForSearching2.addAttribute("security_messageEncryption", "WS-Security");
+        serviceManager.addService(serviceForSearching2);
+
+        serviceForSearching3 =
+                serviceManager.newService(new QName(
+                        "http://service.for.searching3/mnm/",
+                        "serviceForSearching3"));
+        serviceForSearching3.addAttribute("overview_version", "5.0.0");
+        serviceForSearching3.addAttribute("overview_description", "Test");
+        serviceForSearching3.addAttribute("interface_wsdlUrl", WSDL_URL);
+        serviceForSearching3.addAttribute("docLinks_documentType", "test");
+        serviceForSearching3.addAttribute("interface_messageFormats", "SOAP 1.2");
+        serviceForSearching3.addAttribute("interface_messageExchangePatterns", "Request Response");
+        serviceForSearching3.addAttribute("security_authenticationPlatform ", "XTS-WS TRUST");
+        serviceForSearching3.addAttribute("security_authenticationMechanism", "InfoCard");
+        serviceForSearching3.addAttribute("security_messageIntegrity", "WS-Security");
+        serviceForSearching3.addAttribute("security_messageEncryption", "WS-Security");
+        serviceManager.addService(serviceForSearching3);
+
+        serviceForSearching4 =
+                serviceManager.newService(new QName(
+                        "http://service.for.searching4/mnm/",
+                        "serviceForSearching4"));
+        serviceForSearching4.addAttribute("overview_version", "5.1.0");
+        serviceForSearching4.addAttribute("overview_description", "Test");
+        serviceForSearching4.addAttribute("interface_wsdlUrl", WSDL_URL);
+        serviceForSearching4.addAttribute("docLinks_documentType", "test");
+        serviceForSearching4.addAttribute("interface_messageFormats", "SOAP 1.2");
+        serviceForSearching4.addAttribute("interface_messageExchangePatterns", "Request Response");
+        serviceForSearching4.addAttribute("security_authenticationPlatform ", "XTS-WS TRUST");
+        serviceForSearching4.addAttribute("security_authenticationMechanism", "InfoCard");
+        serviceForSearching4.addAttribute("security_messageIntegrity", "WS-Security");
+        serviceForSearching4.addAttribute("security_messageEncryption", "WS-Security");
+        serviceManager.addService(serviceForSearching4);
+
+        serviceForSearching5 =
+                serviceManager.newService(new QName(
+                        "http://service.for.searching5/mnm/",
+                        "serviceForSearching5"));
+        serviceForSearching5.addAttribute("overview_version", "5.1.1");
+        serviceForSearching5.addAttribute("overview_description", "Test");
+        serviceForSearching5.addAttribute("interface_wsdlUrl", WSDL_URL);
+        serviceForSearching5.addAttribute("docLinks_documentType", "test");
+        serviceForSearching5.addAttribute("interface_messageFormats", "SOAP 1.2");
+        serviceForSearching5.addAttribute("interface_messageExchangePatterns", "Request Response");
+        serviceForSearching5.addAttribute("security_authenticationPlatform ", "XTS-WS TRUST");
+        serviceForSearching5.addAttribute("security_authenticationMechanism", "InfoCard");
+        serviceForSearching5.addAttribute("security_messageIntegrity", "WS-Security");
+        serviceForSearching5.addAttribute("security_messageEncryption", "WS-Security");
+        serviceManager.addService(serviceForSearching5);
+    }
+
+    /**
+     * Do filtering from the basic filter available on the 'Service List' page.
+     * Here I will use the service filter to mimic the process
+     * <p/>
+     * Search by giving single search criteria
+     * <p/>
+     * Add a new service, then search for the services through basic and
+     * advanced search
+     *
+     * @throws GovernanceException
+     */
+    @Test(groups = {"wso2.greg"}, description = "Filter Services")
+    public void filterServicesTestCase() throws GovernanceException {
+        Service searchResult = serviceManager.findServices(new ServiceFilter() {
+
+            public boolean matches(Service service) throws GovernanceException {
+                String attributeVal = service.getAttribute("overview_name");
+                if (attributeVal != null && attributeVal.startsWith("serviceForSearching1")) {
+                    return true;
+                }
+                return false;
+            }
+        })[0];
+
+        Assert.assertEquals(searchResult.getAttribute("overview_name"), "serviceForSearching1");
+    }
+
+    /**
+     * Filter from all the available fields in the 'Filter Services' advance
+     * search page. Here I will use the service filter to mimic the process
+     * <p/>
+     * Search by giving multiple search criteria
+     *
+     * @throws GovernanceException
+     */
+    @Test(groups = {"wso2.greg"}, description = "Filter Services", dependsOnMethods = "filterServicesTestCase")
+    public void advFilterServicesTestCase() throws GovernanceException {
+        Service searchResult = serviceManager.findServices(new ServiceFilter() {
+
+            public boolean matches(Service service) throws GovernanceException {
+                String attributeVal1 = service.getAttribute("overview_name");
+                String attributeVal2 = service.getAttribute("overview_version");
+                String attributeVal3 = service.getAttribute("overview_description");
+                String attributeVal4 = service.getAttribute("interface_wsdlUrl");
+                String attributeVal5 = service.getAttribute("docLinks_documentType");
+                String attributeVal6 = service.getAttribute("interface_messageFormats");
+                String attributeVal7 = service.getAttribute("interface_messageExchangePatterns");
+                String attributeVal8 = service.getAttribute("security_authenticationPlatform");
+                String attributeVal9 = service.getAttribute("security_authenticationMechanism");
+                String attributeVal10 = service.getAttribute("security_messageIntegrity");
+                String attributeVal11 = service.getAttribute("security_messageEncryption");
+                if (attributeVal1 != null && attributeVal1.startsWith("serviceForSearching4") &&
+                    attributeVal2 != null && attributeVal2.startsWith("5.1.0") &&
+                    attributeVal3 != null && attributeVal3.startsWith("Test") &&
+                    attributeVal4 != null && attributeVal4.startsWith(WSDL_URL) &&
+                    attributeVal5 != null && attributeVal5.startsWith("test") &&
+                    attributeVal6 != null && attributeVal6.startsWith("SOAP 1.2") &&
+                    attributeVal7 != null && attributeVal7.startsWith("Request Response") &&
+                    attributeVal8 != null && attributeVal8.startsWith("XTS-WS TRUST") &&
+                    attributeVal9 != null && attributeVal9.startsWith("InfoCard") &&
+                    attributeVal10 != null && attributeVal10.startsWith("WS-Security") &&
+                    attributeVal11 != null && attributeVal11.startsWith("WS-Security")) {
+                    return true;
+                }
+                return false;
+            }
+        })[0];
+
+        Assert.assertEquals(searchResult.getAttribute("overview_name"), "serviceForSearching4");
+        Assert.assertEquals(searchResult.getAttribute("overview_version"), "5.1.0");
+        Assert.assertEquals(searchResult.getAttribute("overview_description"), "Test");
+        Assert.assertEquals(searchResult.getAttribute("interface_wsdlUrl"), WSDL_URL);
+        Assert.assertEquals(searchResult.getAttribute("docLinks_documentType"), "test");
+        Assert.assertEquals(searchResult.getAttribute("interface_messageFormats"), "SOAP 1.2");
+        Assert.assertEquals(searchResult.getAttribute("interface_messageExchangePatterns"),
+                            "Request Response");
+        Assert.assertEquals(searchResult.getAttribute("security_authenticationPlatform"),
+                            "XTS-WS TRUST");
+        Assert.assertEquals(searchResult.getAttribute("security_authenticationMechanism"),
+                            "InfoCard");
+        Assert.assertEquals(searchResult.getAttribute("security_messageIntegrity"), "WS-Security");
+        Assert.assertEquals(searchResult.getAttribute("security_messageEncryption"), "WS-Security");
+    }
+
+    /**
+     * Edit the content of the service and then search for the services through
+     * basic and advanced search
+     *
+     * @throws GovernanceException
+     */
+    @Test(groups = {"wso2.greg"}, description = "Filter Edited Services", dependsOnMethods = "advFilterServicesTestCase")
+    public void filterEditedServices() throws GovernanceException {
+        serviceForSearching2.addAttribute("test-att", "test-val");
+        serviceManager.updateService(serviceForSearching2);
+        Service searchResult = serviceManager.findServices(new ServiceFilter() {
+
+            public boolean matches(Service service) throws GovernanceException {
+                String attributeVal = service.getAttribute("test-att");
+                if (attributeVal != null && attributeVal.startsWith("test-val")) {
+                    return true;
+                }
+                return false;
+            }
+        })[0];
+
+        Assert.assertEquals(searchResult.getAttribute("overview_name"), "serviceForSearching2");
+    }
+
+}

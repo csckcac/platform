04239a3d758f4f74cce25e2c3b68e092c098ad6b
johann <johann@a5903396-d722-0410-b921-86c7d4935375>
2012-07-26 04:37:57 +0000

    Adding EntitlementFilter callback package and removing unused packages
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@134975 a5903396-d722-0410-b921-86c7d4935375

---
+++ components/identity/org.wso2.carbon.identity.entitlement.filter/src/main/java/org/wso2/carbon/identity/entitlement/filter/callback/EntitlementFilterCallBackHandler.java
@@ -0,0 +1,22 @@
+package org.wso2.carbon.identity.entitlement.filter.callback;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: andunslg
+ * Date: 7/18/12
+ * Time: 5:15 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class EntitlementFilterCallBackHandler {
+    private String userName;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+
+}

3a1137485c3a9de0bd240cc6ef622d968db3f6b1
thilinab <thilinab@a5903396-d722-0410-b921-86c7d4935375>
2012-07-26 06:17:43 +0000

    Adding the option of skipping identity db schema initialization. This was added to support a single db script for the API Manager.
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@135001 a5903396-d722-0410-b921-86c7d4935375

--- components/identity/org.wso2.carbon.identity.base/src/main/java/org/wso2/carbon/identity/base/IdentityConstants.java
+++ components/identity/org.wso2.carbon.identity.base/src/main/java/org/wso2/carbon/identity/base/IdentityConstants.java
@@ -171,6 +171,8 @@ public class IdentityConstants {
         public static final String SINGLE_LOGOUT_RETRY_COUNT = "SSOService.SingleLogoutRetryCount";
         public static final String SINGLE_LOGOUT_RETRY_INTERVAL = "SSOService.SingleLogoutRetryInterval";
 
+        //Identity Persistence Manager
+        public static final String SKIP_DB_SCHEMA_CREATION = "JDBCPersistenceManager.SkipDBSchemaCreation";
     }
 
 	/**

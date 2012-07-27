8d462477a1d0f18fa5f160917488b11ea2bf959d
dharshanaw <dharshanaw@a5903396-d722-0410-b921-86c7d4935375>
2012-07-27 08:24:14 +0000

    Adding ) Xpath Xpath SourceXpathTargetXpath test -amila
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@135329 a5903396-d722-0410-b921-86c7d4935375

--- products/esb/modules/integration/tests/src/test/java/org/wso2/carbon/mediator/test/callOut/SourceXpathTargetXpath.java
+++ products/esb/modules/integration/tests/src/test/java/org/wso2/carbon/mediator/test/callOut/SourceXpathTargetXpath.java
@@ -1,21 +1,3 @@
-/*
-*Copyright (c) 2005-2010, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
-*
-*WSO2 Inc. licenses this file to you under the Apache License,
-*Version 2.0 (the "License"); you may not use this file except
-*in compliance with the License.
-*You may obtain a copy of the License at
-*
-*http://www.apache.org/licenses/LICENSE-2.0
-*
-*Unless required by applicable law or agreed to in writing,
-*software distributed under the License is distributed on an
-*"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-*KIND, either express or implied.  See the License for the
-*specific language governing permissions and limitations
-*under the License.
-*/
-
 package org.wso2.carbon.mediator.test.callOut;
 
 import org.apache.axiom.om.OMElement;
@@ -39,18 +21,30 @@ import java.io.IOException;
 
 import static org.testng.Assert.assertTrue;
 
-
+/**
+ * Created with IntelliJ IDEA.
+ * User: amila
+ * Date: 7/18/12
+ * Time: 10:12 AM
+ * To change this template use File | Settings | File Templates.
+ */
 public class SourceXpathTargetXpath {
 
-    private static final Log log = LogFactory.getLog(LogMediatorLevelTest.class);
+
+
     StockQuoteClient axis2Client;
     LogViewerClient logViewer;
     LoggingAdminClient logAdmin;
+
     String sessionCookie = null;
+    private static final Log log = LogFactory.getLog(LogMediatorLevelTest.class);
     String backEndUrl = null;
+
     private OMElement response;
+
     private EnvironmentVariables esbServer;
     UserInfo userInfo;
+
 
     @BeforeTest(alwaysRun = true)
     public void setEnvironment() throws Exception, IOException {
@@ -61,22 +55,24 @@ public class SourceXpathTargetXpath {
         ESBTestCaseUtils caseUtils=new ESBTestCaseUtils();
         backEndUrl = esbServer.getBackEndUrl();
         sessionCookie = esbServer.getSessionCookie();
-
-        caseUtils.loadESBConfigurationFromClasspath
-                ("/artifacts/ESB/mediatorconfig/callout/synapse_sample_430.xml", backEndUrl, sessionCookie);
+        caseUtils.loadESBConfigurationFromClasspath("/artifacts/ESB/mediatorconfig/callout/synapse_sample_430.xml", backEndUrl, sessionCookie);
     }
 
 
     @Test (alwaysRun = true)
 
         public void test_sourceXpathTargetXpath() throws AxisFault {
-        response = axis2Client.sendSimpleStockQuoteRequest("http://localhost:8280/","","IBM");
-         // send the simplestockquote request. service url is set at the synapse
-        boolean ResponseContainsIBM = response.getFirstElement().toString().contains("IBM");
-         //checks whether the  response contains IBM
-        assertTrue(ResponseContainsIBM);
-       }
 
+
+      response = axis2Client.sendSimpleStockQuoteRequest("http://localhost:8280/","","IBM");    // send the simplestockquote request. service url is set at the synapse
+
+
+        boolean ResponseContainsIBM = response.getFirstElement().toString().contains("IBM");      //checks whether the  response contains IBM
+
+        assertTrue(ResponseContainsIBM);
+
+
+       }
 
     @AfterClass
     public void cleanup() {
@@ -84,6 +80,11 @@ public class SourceXpathTargetXpath {
         userInfo = null;
         esbServer = null;
     }
+
+
+
+
+
 
     protected void loadSampleESBConfiguration(int sampleNo) throws Exception {
         ESBTestCaseUtils esbUtils = new ESBTestCaseUtils();

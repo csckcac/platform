f0350a43bbe36543fc7748013fdad58c9f3baf49
dharshanaw <dharshanaw@a5903396-d722-0410-b921-86c7d4935375>
2012-07-27 14:17:55 +0000

    call template parameters with value - harsha kumara
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@135420 a5903396-d722-0410-b921-86c7d4935375

---
+++ products/esb/modules/integration/tests/src/test/java/org/wso2/carbon/mediator/test/call/CallTemplateMediatorParamsWithValuesTestCase.java
@@ -0,0 +1,59 @@
+/*
+*Copyright (c) 2005-2010, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+*WSO2 Inc. licenses this file to you under the Apache License,
+*Version 2.0 (the "License"); you may not use this file except
+*in compliance with the License.
+*You may obtain a copy of the License at
+*
+*http://www.apache.org/licenses/LICENSE-2.0
+*
+*Unless required by applicable law or agreed to in writing,
+*software distributed under the License is distributed on an
+*"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+*KIND, either express or implied.  See the License for the
+*specific language governing permissions and limitations
+*under the License.
+*/
+
+package org.wso2.carbon.mediator.test.call;
+
+import org.apache.axiom.om.OMElement;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import org.wso2.carbon.automation.api.clients.logging.LogViewerClient;
+import org.wso2.carbon.automation.api.clients.logging.LoggingAdminClient;
+import org.wso2.carbon.mediator.test.ESBMediatorTest;
+
+import javax.xml.stream.XMLStreamException;
+import java.io.IOException;
+
+public class CallTemplateMediatorParamsWithValuesTestCase extends ESBMediatorTest {
+    private LogViewerClient logViewer;
+    private LoggingAdminClient logAdmin;
+
+    @BeforeClass(alwaysRun = true)
+    public void setEnvironment() throws Exception, IOException {
+        super.init();
+        loadESBConfigurationFromClasspath("/artifacts/ESB/mediatorconfig/call/synapse_param_with_values.xml");
+    }
+
+    @Test(groups = {"wso2.esb"}, description = "Call Template Mediator Sample Parameters with" +
+                                               " values assigned test")
+    public void testXSLTTransformationWithTemplates() throws IOException, XMLStreamException {
+        OMElement response=axis2Client.sendSimpleStockQuoteRequest(getProxyServiceURL
+                                                                           ("StockQuoteProxy"), null, "IBM");
+        logAdmin = new LoggingAdminClient(esbServer.getBackEndUrl(),esbServer.getSessionCookie());
+        //TODO - Asserting the response from the log
+        /*logViewer=new LogViewerClient(esbServer.getBackEndUrl(),esbServer.getSessionCookie());
+        LogEvent[] getLogsDebug = logViewer.getLogs("PARAM", "LogMediator");*/
+        /*Assert "RESPONSE PARAM VALUE" and "REQUEST PARAM VALUE" is in logs */
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void cleanUp()
+    {
+        cleanup();
+    }
+}

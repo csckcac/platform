b3491df10c2213440375e9086f18a6872098080c
kishanthan <kishanthan@a5903396-d722-0410-b921-86c7d4935375>
2012-08-28 09:37:21 +0000

    removing valve addintion in JaggeryappManagementServiceComponent, this is already added in WebappManagementServiceComponent
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@140112 a5903396-d722-0410-b921-86c7d4935375

--- products/jaggery/components/jaggery-core/org.jaggeryjs.jaggery.app.mgt/src/main/java/org/jaggeryjs/jaggery/app/mgt/internal/JaggeryappManagementServiceComponent.java
+++ products/jaggery/components/jaggery-core/org.jaggeryjs.jaggery.app.mgt/src/main/java/org/jaggeryjs/jaggery/app/mgt/internal/JaggeryappManagementServiceComponent.java
@@ -46,15 +46,6 @@ public class JaggeryappManagementServiceComponent {
 
 
     protected void activate(ComponentContext ctx) {
-        try {
-            // Register the valves with Tomcat
-            ArrayList<CarbonTomcatValve> valves = new ArrayList<CarbonTomcatValve>();
-            valves.add(new TenantLazyLoaderValve());
-            valves.add(new GhostWebappDeployerValve());
-            TomcatValveContainer.addValves(valves);
-        } catch (Exception e) {
-            log.error("Error occurred while activating JaggeryappManagementServiceComponent", e);
-        }
     }
 
     protected void deactivate(ComponentContext ctx) {

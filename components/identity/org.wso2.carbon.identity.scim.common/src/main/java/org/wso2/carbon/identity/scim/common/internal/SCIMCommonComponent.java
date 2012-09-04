89d11eda5d4de6a0741e5a462c846e307925ac85
hasini <hasini@a5903396-d722-0410-b921-86c7d4935375>
2012-09-04 00:00:42 +0000

    merging a commit made to branch
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@140999 a5903396-d722-0410-b921-86c7d4935375

--- components/identity/org.wso2.carbon.identity.scim.common/src/main/java/org/wso2/carbon/identity/scim/common/internal/SCIMCommonComponent.java
+++ components/identity/org.wso2.carbon.identity.scim.common/src/main/java/org/wso2/carbon/identity/scim/common/internal/SCIMCommonComponent.java
@@ -4,6 +4,8 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.osgi.service.component.ComponentContext;
 import org.wso2.carbon.identity.scim.common.config.SCIMProvisioningConfigManager;
+import org.wso2.carbon.identity.scim.common.listener.SCIMUserOperationListener;
+import org.wso2.carbon.user.core.listener.UserOperationEventListener;
 import org.wso2.carbon.utils.CarbonUtils;
 import org.wso2.charon.core.config.SCIMConfig;
 import org.wso2.charon.core.config.SCIMConfigConstants;
@@ -30,6 +32,10 @@ public class SCIMCommonComponent {
 
             configManager.setSCIMConfig(scimConfig);
 
+            //register UserOperationEventListener implementation
+            SCIMUserOperationListener scimUserOperationListener = new SCIMUserOperationListener();
+            ctx.getBundleContext().registerService(UserOperationEventListener.class.getName(),
+                                                   scimUserOperationListener, null);
             if (logger.isDebugEnabled()) {
                 logger.debug("SCIM Common component activated successfully.");
             }

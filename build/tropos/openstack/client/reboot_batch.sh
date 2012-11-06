e46d225b10b2d7d48536fcce991cbecf1e91c43e
damitha <damitha@a5903396-d722-0410-b921-86c7d4935375>
2012-11-06 06:04:52 +0000

    Adding reboot helper script.
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@147363 a5903396-d722-0410-b921-86c7d4935375

---
+++ build/tropos/openstack/client/reboot_batch.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+source ./demorc
+image=$1
+fromCount=$2
+toCount=$3
+
+if [ -z $fromCount ]; then
+    fromCount=1
+fi
+if [ -z $toCount ]; then
+    toCount=1
+fi
+
+
+for I in `seq $fromCount $toCount`; do nova reboot test$I; done

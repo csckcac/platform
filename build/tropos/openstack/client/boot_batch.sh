95dc18d30dc01d8a5d2e0671a8ae35feb7c9c59e
damitha <damitha@a5903396-d722-0410-b921-86c7d4935375>
2012-11-06 05:27:45 +0000

    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@147350 a5903396-d722-0410-b921-86c7d4935375

--- build/tropos/openstack/client/boot_batch.sh
+++ build/tropos/openstack/client/boot_batch.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 source ./demorc
 image=$1
-count=$2
+fromCount=$2
+toCount=$3
 
-for I in `seq 1 $count`; do ./boot_instance.sh $image test$I; done
+if [ -z $fromCount ]; then
+    fromCount=1
+fi
+if [ -z $toCount ]; then
+    toCount=1
+fi
+
+
+for I in `seq $fromCount $toCount`; do ./boot_instance.sh $image test$I; done

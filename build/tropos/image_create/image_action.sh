86cfa9fcd934933518247e80dbec3b8d996414c1
damitha <damitha@a5903396-d722-0410-b921-86c7d4935375>
2012-11-21 16:28:38 +0000

    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@148801 a5903396-d722-0410-b921-86c7d4935375

--- build/tropos/image_create/image_action.sh
+++ build/tropos/image_create/image_action.sh
@@ -9,8 +9,8 @@ image_root=""
 software=""
 image_type=""
 loopdevice="/dev/loop3"
-nbddevice="/dev/nbd4"
-nbdmount="/dev/nbd4p1"
+nbddevice="/dev/nbd2"
+nbdmount="/dev/nbd2p1"
 
 function image_validate {
     if [ -z $action ]; then
